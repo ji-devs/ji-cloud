661b78a93abf34506acc97f111e0d45af501f9b3
David Komer <david.komer@gmail.com>
2020-06-15 23:45:20 +0300

    ci/cd

--- backend/pages/src/logger.rs
+++ backend/pages/src/logger.rs
@@ -1,11 +1,24 @@
 use crate::settings::SETTINGS;
 use simplelog::*;
+use cfg_if::cfg_if;
 
-pub fn init_logger() {
-    CombinedLogger::init(vec![
-        TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed).unwrap(),
-        //WriteLogger::new(LevelFilter::Info, Config::default(), File::create("my_rust_binary.log").unwrap()),
-    ])
-    .unwrap();
-    //log::info!("{:?}", &*SETTINGS);
+cfg_if! {
+    if #[cfg(feature = "local")] {
+        pub fn init_logger() { 
+            CombinedLogger::init(vec![
+                TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed),
+                //WriteLogger::new(LevelFilter::Info, Config::default(), File::create("my_rust_binary.log").unwrap()),
+            ])
+            .unwrap();
+        }
+    } else { 
+        pub fn init_logger() { 
+            CombinedLogger::init(vec![
+                SimpleLogger::new(LevelFilter::Info, Config::default()),
+                //WriteLogger::new(LevelFilter::Info, Config::default(), File::create("my_rust_binary.log").unwrap()),
+            ])
+            .unwrap();
+        }
+        
+    } 
 }
