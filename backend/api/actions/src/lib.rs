a4a458c5291aa128a8110a446b0bea578742edc5
David Komer <david.komer@gmail.com>
2020-07-08 11:58:52 +0300

    openssl

--- backend/api/actions/src/lib.rs
+++ backend/api/actions/src/lib.rs
@@ -1,6 +1,8 @@
 //see: https://github.com/rust-lang/cargo/issues/8010
 #![cfg_attr(feature = "quiet", allow(warnings))]
 
+extern crate openssl;
+
 pub mod settings;
 pub mod user;
 pub mod auth;
--- backend/api/app/src/main.rs
+++ backend/api/app/src/main.rs
@@ -10,6 +10,7 @@ use dotenv::dotenv;
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+    openssl_probe::init_ssl_cert_env_vars();
     logger::init_logger();
     server::start().await;
     log::info!("app started!");
