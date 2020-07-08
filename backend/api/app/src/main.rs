320b9f784451dd79e63f81209e4776e9c69c5f67
David Komer <david.komer@gmail.com>
2020-07-08 14:14:34 +0300

    updates

--- backend/api/app/src/main.rs
+++ backend/api/app/src/main.rs
@@ -1,8 +1,6 @@
 //see: https://github.com/rust-lang/cargo/issues/8010
 #![cfg_attr(feature = "quiet", allow(warnings))]
 
-extern crate openssl;
-
 mod logger;
 
 use dotenv::dotenv;
@@ -10,7 +8,6 @@ use dotenv::dotenv;
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    openssl_probe::init_ssl_cert_env_vars();
     logger::init_logger();
     server::start().await;
     log::info!("app started!");
--- backend/api/server/src/lib.rs
+++ backend/api/server/src/lib.rs
@@ -1,8 +1,6 @@
 //see: https://github.com/rust-lang/cargo/issues/8010
 #![cfg_attr(feature = "quiet", allow(warnings))]
 
-extern crate openssl;
-
 pub mod settings;
 mod db;
 mod logger;
