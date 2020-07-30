3e42b1d93bece7d1b363791e4c196087c5501f09
Chloe Ross <orangesnowfox@gmail.com>
2020-07-30 14:49:05 -0700

    refactor: deduplicate server::_start

--- backend/api/crates/server/src/lib.rs
+++ backend/api/crates/server/src/lib.rs
@@ -15,9 +15,19 @@ pub async fn start() {
     _start(db_pool).await;
 }
 
+#[cfg(feature = "local")]
+fn get_tcp_fd() -> Option<std::net::TcpListener> {
+    listenfd::ListenFd::from_env().take_tcp_listener(0).unwrap()
+}
+
+#[cfg(not(feature = "local"))]
+fn get_tcp_fd() -> Option<std::net::TcpListener> {
+    // we don't have listenfd here.
+    None
+}
+
 //auto reload on code change
 //see: https://github.com/seanmonstar/warp/blob/master/examples/autoreload.rs
-#[cfg(feature = "local")]
 pub async fn _start(pool: PgPool) {
     // todo: de-duplicate this somehow.
     let server = actix_web::HttpServer::new(move || {
@@ -32,31 +42,13 @@ pub async fn _start(pool: PgPool) {
     // if listenfd doesn't take a TcpListener (i.e. we're not running via
     // the command above), we fall back to explicitly binding to a given
     // host:port.
-    let server = if let Some(l) = listenfd::ListenFd::from_env().take_tcp_listener(0).unwrap() {
+    let server: _ = if let Some(l) = get_tcp_fd() {
         server.listen(l).unwrap()
     } else {
         server.bind(get_addr()).unwrap()
     };
 
     server.run().await.unwrap();
-}
-
-#[cfg(not(feature = "local"))]
-pub async fn _start(pool: PgPool) {
-    // todo: de-duplicate this somehow.
-    actix_web::HttpServer::new(move || {
-        actix_web::App::new()
-            .app_data(pool.clone())
-            .wrap(actix_web::middleware::Logger::default())
-            .wrap(cors::get_cors_actix().finish())
-            .app_data(actix_web::web::JsonConfig::default().limit(JSON_BODY_LIMIT as usize))
-            .configure(endpoints::user::configure)
-    })
-    .bind(get_addr())
-    .unwrap()
-    .run()
-    .await
-    .unwrap();
 }
 
 fn get_addr() -> SocketAddr {
