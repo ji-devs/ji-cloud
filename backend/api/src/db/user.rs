3161861113252e564543921b84ebb0faa26a81b9
Chloe Ross <orangesnowfox@gmail.com>
2020-07-30 17:07:48 -0700

    refactor: allow `shared` to impl stuff for the DB for the backend while leaving the frontend alone

--- backend/api/src/db/user.rs
+++ backend/api/src/db/user.rs
@@ -1,46 +1,17 @@
 use shared::{auth::RegisterRequest, user::User};
 
-impl From<UserQuery> for User {
-    fn from(u: UserQuery) -> Self {
-        Self {
-            id: u.id,
-            display_name: u.display_name,
-            first_name: u.first_name,
-            last_name: u.last_name,
-            email: u.email,
-            roles: u.roles.into_iter().map(|r| r.into()).collect(),
-        }
-    }
-}
-
-#[derive(sqlx::FromRow)]
-pub struct UserQuery {
-    pub id: String,
-    pub first_name: String,
-    pub last_name: String,
-    pub roles: Vec<i32>,
-    pub email: String,
-    pub display_name: String,
-}
-
 pub async fn by_email(db: &sqlx::PgPool, email_addr: &str) -> sqlx::Result<Option<User>> {
-    let user = sqlx::query_as::<_, UserQuery>("SELECT * FROM users WHERE email = $1")
+    sqlx::query_as("SELECT * FROM users WHERE email = $1")
         .bind(email_addr)
         .fetch_optional(db)
-        .await?
-        .map(User::from);
-
-    Ok(user)
+        .await
 }
 
 pub async fn by_id(db: &sqlx::PgPool, user_id: &str) -> sqlx::Result<Option<User>> {
-    let user = sqlx::query_as::<_, UserQuery>("SELECT * FROM users WHERE id = $1")
+    sqlx::query_as("SELECT * FROM users WHERE id = $1")
         .bind(user_id)
         .fetch_optional(db)
-        .await?
-        .map(|u: UserQuery| u.into());
-
-    Ok(user)
+        .await
 }
 
 pub async fn register(db: &sqlx::PgPool, user_id: &str, req: &RegisterRequest) -> sqlx::Result<()> {
--- shared/rust/src/user.rs
+++ shared/rust/src/user.rs
@@ -1,20 +1,13 @@
 use serde::{Deserialize, Serialize};
 use serde_repr::{Deserialize_repr, Serialize_repr};
+#[cfg(feature = "backend")]
+use sqlx::postgres::PgRow;
 
 #[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug, Clone)]
-#[repr(u8)]
+#[repr(i8)]
+#[cfg_attr(feature = "backend", derive(sqlx::Type))]
 pub enum UserRole {
     Admin = 1,
-}
-
-impl From<i32> for UserRole {
-    fn from(x: i32) -> Self {
-        if x == UserRole::Admin as i32 {
-            UserRole::Admin
-        } else {
-            panic!("Couldn't get user role from i32!");
-        }
-    }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -29,3 +22,38 @@ pub struct User {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct NoSuchUserError {}
+
+// HACK: we can't get `Vec<UserRole>` directly from the DB, so we have to work around it for now.
+// see: https://github.com/launchbadge/sqlx/issues/298
+#[cfg(feature = "backend")]
+impl<'r> sqlx::FromRow<'r, PgRow> for User {
+    fn from_row(row: &'r PgRow) -> Result<Self, sqlx::Error> {
+        let DbUser {
+            id,
+            first_name,
+            last_name,
+            roles,
+            email,
+            display_name,
+        } = DbUser::from_row(row)?;
+
+        Ok(Self {
+            id,
+            first_name,
+            last_name,
+            roles: roles.into_iter().map(|(it,)| it).collect(),
+            email,
+            display_name,
+        })
+    }
+}
+
+#[cfg_attr(feature = "backend", derive(sqlx::FromRow))]
+struct DbUser {
+    pub id: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub roles: Vec<(UserRole,)>,
+    pub email: String,
+    pub display_name: String,
+}
