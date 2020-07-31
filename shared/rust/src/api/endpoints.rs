bc565c0adbd203e26130d8a126e26bcf5074179b
Chloe Ross <orangesnowfox@gmail.com>
2020-07-30 17:28:32 -0700

    feat: give ApiEndpoint a PATH

--- shared/rust/src/api/endpoints.rs
+++ shared/rust/src/api/endpoints.rs
@@ -1,9 +1,11 @@
 use serde::{de::DeserializeOwned, Serialize};
 
+// todo: add some way of getting the method.
 pub trait ApiEndpoint {
     type Req: Serialize;
     type Res: DeserializeOwned + Serialize;
     type Err: DeserializeOwned + Serialize;
+    const PATH: &'static str;
 }
 
 pub mod user {
@@ -16,31 +18,35 @@ pub mod user {
         user::{NoSuchUserError, User},
     };
 
-    pub struct Signin {}
+    pub struct Signin;
     impl ApiEndpoint for Signin {
         type Req = ();
         type Res = SigninSuccess;
         type Err = NoSuchUserError;
+        const PATH: &'static str = "/v1/login";
     }
 
-    pub struct SingleSignOn {}
+    pub struct SingleSignOn;
     impl ApiEndpoint for SingleSignOn {
         type Req = ();
         type Res = SingleSignOnSuccess;
         type Err = ();
+        const PATH: &'static str = "/v1/authorize";
     }
 
-    pub struct Register {}
+    pub struct Register;
     impl ApiEndpoint for Register {
         type Req = RegisterRequest;
         type Res = RegisterSuccess;
         type Err = RegisterError;
+        const PATH: &'static str = "/v1/user";
     }
 
-    pub struct Profile {}
+    pub struct Profile;
     impl ApiEndpoint for Profile {
         type Req = ();
         type Res = User;
         type Err = NoSuchUserError;
+        const PATH: &'static str = "/v1/user/me/profile";
     }
 }
