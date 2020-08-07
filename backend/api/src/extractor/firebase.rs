905e318b3f4229bf98c01cf335c5da3c823a2e0b
Chloe Ross <orangesnowfox@gmail.com>
2020-08-06 17:21:36 -0700

    test: add a (partial) user register test

--- backend/api/src/extractor/firebase.rs
+++ backend/api/src/extractor/firebase.rs
@@ -16,11 +16,21 @@ pub async fn get_firebase_id(
     token: &str,
     js_api_url: &str,
     inter_server_secret: &str,
+    insecure_decode: bool,
 ) -> anyhow::Result<Option<String>> {
     //use the js server to handle this, since it has the official firebase admin sdk
     //it could be done natively in Rust, but depends on:
     //1. https://github.com/Keats/jsonwebtoken/issues/127
     //2. all the specific claim checks (e.g. timestamp comparisons)
+
+    // HACK for testing don't verify external auth, just pretend they made something.
+    if insecure_decode {
+        let claims: FirebaseClaims = jwt::dangerous_insecure_decode(&token)?.claims;
+
+        let user_id = claims.sub;
+
+        return Ok(Some(user_id));
+    }
 
     let response: JsApiResponse = reqwest::Client::new()
         .get(&format!("{}/validate-firebase-token/{}", js_api_url, token))
