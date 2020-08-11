bc8df97d075007d3be3dc66dc9f11cab231dd232
Chloe Ross <orangesnowfox@gmail.com>
2020-08-11 16:22:53 -0700

    test: make tests ~3x as fast

--- backend/api/integration-testing/test.js
+++ backend/api/integration-testing/test.js
@@ -1,36 +1,36 @@
 const { default: test } = require('ava');
 
 const got = require('got');
-getPort = require('get-port');
-const { execSync, exec, spawnSync } = require('child_process');
-const { argv, stdout } = require('process');
-const { copyFile, copyFileSync, mkdirSync } = require('fs');
+const getPort = require('get-port');
+const { mkdir, copyFile } = require('fs').promises;
 const path = require('path');
 const tough = require('tough-cookie');
-const { TLSSocket } = require('tls');
+const spawnAsync = require('@expo/spawn-async');
+
 function hookServerStarted(server) {
     return new Promise(resolve => {
+        resolved = false;
         server.stdout.on('data', function (data) {
-            if (data.toString().includes(`Starting "actix-web-service-`)) {
+            if (data.toString().includes('Starting "actix-web-service-')) {
                 resolve();
             }
         });
     })
 }
 
-function hookProcEnd(proc) {
-    return new Promise(resolve => {
-        proc.on('close', () => resolve())
-    })
+async function runFixtures(files, dbUrl, parentDir) {
+    for (let file of files) {
+        await spawnAsync("/usr/bin/psql", ["-f", file, dbUrl], { cwd: parentDir, env: { PGUSER: "postgres" }, encoding: 'utf8' });
+    }
 }
 
 test.before(async t => {
     t.context.parentDir = path.resolve(process.cwd(), '..');
 
-    execSync("cargo build --manifest-path ../Cargo.toml");
+    await spawnAsync("cargo", ["build", "--manifest-path", "../Cargo.toml"]);
 
     try {
-        mkdirSync("bin")
+        await mkdir("bin");
     }
     catch (e) {
         if (e && e.code === 'EEXIST') {
@@ -39,22 +39,25 @@ test.before(async t => {
             throw e
         }
     }
-    copyFileSync("../target/debug/ji-cloud-api", "bin/ji-cloud-api");
+    await copyFile("../target/debug/ji-cloud-api", "bin/ji-cloud-api");
 })
 
 test.beforeEach(async t => {
-    const port = await getPort();
+    let port = getPort();
 
     var parentDir = t.context.parentDir;
-    const dbUrl = execSync("../../script/ephemeralpg/pg_tmp.sh", { encoding: 'utf8' });
+    const dbUrl = await spawnAsync("../../script/ephemeralpg/pg_tmp.sh", { encoding: 'utf8' }).then(it => it.stdout);
 
     t.context.dbUrl = dbUrl;
 
-    execSync("cargo sqlx migrate run", { cwd: parentDir, env: { DATABASE_URL: `${dbUrl}`, PGUSER: "postgres" }, encoding: 'utf8' });
+    let id = Math.random();
+
+    await spawnAsync("cargo", ["sqlx", "migrate", "run"], { cwd: parentDir, env: { DATABASE_URL: dbUrl, PGUSER: "postgres" }, encoding: 'utf8' });
+    port = await port;
 
     const env = {
         LOCAL_API_PORT: port,
-        DATABASE_URL: `${dbUrl}`,
+        DATABASE_URL: dbUrl,
         PGUSER: "postgres",
         JWT_SECRET: "abc123",
         INTER_SERVER_SECRET: "aaa",
@@ -63,16 +66,23 @@ test.beforeEach(async t => {
     };
 
     t.context.port = port;
-    t.context.server = exec("bin/ji-cloud-api", { env: env, encoding: 'utf8' });
+    t.context.server = spawnAsync("bin/ji-cloud-api", { env: env, encoding: 'utf8' });
 
-    await hookServerStarted(t.context.server);
+    await hookServerStarted(t.context.server.child);
 })
 
 test.afterEach.always(async t => {
     if (t.context.server) {
-        let exitHook = hookProcEnd(t.context.server);
-        t.context.server.kill('SIGTERM');
-        await exitHook;
+        try {
+            t.context.server.child.kill('SIGKILL');
+            await t.context.server;
+        } catch (e) {
+            if (e && e.signal === 'SIGKILL') {
+                // ignore it.
+            } else {
+                throw e
+            }
+        }
     }
 })
 
@@ -86,6 +96,14 @@ test("pass", async t => {
 // {"sub": "SGkgdGhpcyBpcyBhIHRlc3QgdG9rZW4K", "iat": 1597096685, "auth_time": 1597096686 }
 // The secret used is `aaaaa`
 const TEST_JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTR2tnZEdocGN5QnBjeUJoSUhSbGMzUWdkRzlyWlc0SyIsImlhdCI6MTU5NzA5NjY4NSwiYXV0aF90aW1lIjoxNTk3MDk2Njg2fQ.BNpCIBuNq0bhgXuAEqrAfPpIein0Y54hj352d2ke1sI";
+
+// login cookie
+const COOKIE = "X-JWT=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9." +
+    "eyJpZCI6IjFmMjQxZTFiLWI1MzctNDkzZi1hMjMwLTA3NWNiMTYzMTViZSIsImNzcmYiOiJSdVF1WmI1QW9HU2R4SUdBIn0.osvyaIW4Mt-3Em4kkuvO4wXAsCVA9gZwkqXlQvQETAs; " +
+    "Max-Age=1209600; Path=/v1; HttpOnly; SameSite=Lax; hostOnly=true; aAge=2ms; cAge=6ms; hostOnly=true; aAge=1ms; cAge=1ms";
+
+// login csrf
+const CSRF = "RuQuZb5AoGSdxIGA";
 
 test("missing auth (firebase)", async t => {
     const e = await t.throwsAsync(got.post('http://0.0.0.0/v1/user', {
@@ -125,10 +143,7 @@ test("register user", async t => {
 })
 
 test("login user", async t => {
-    const parentDir = t.context.parentDir;
-    const dbUrl = t.context.dbUrl;
-
-    execSync(`/usr/bin/psql -f fixtures/1_user.sql ${dbUrl}`, { cwd: parentDir, env: { PGUSER: "postgres" }, encoding: 'utf8' });
+    await runFixtures(['fixtures/1_user.sql'], t.context.dbUrl, t.context.parentDir);
 
     const cookieJar = new tough.CookieJar();
 
@@ -145,28 +160,17 @@ test("login user", async t => {
 })
 
 test("user profile", async t => {
-    const parentDir = t.context.parentDir;
-    const dbUrl = t.context.dbUrl;
-
-    execSync(`/usr/bin/psql -f fixtures/1_user.sql ${dbUrl}`, { cwd: parentDir, env: { PGUSER: "postgres" }, encoding: 'utf8' });
+    await runFixtures(['fixtures/1_user.sql'], t.context.dbUrl, t.context.parentDir);
 
     const cookieJar = new tough.CookieJar();
-
-    const login = await got.post('http://0.0.0.0/v1/login', {
-        cookieJar,
-        port: t.context.port,
-        responseType: 'json',
-        headers: {
-            authorization: "Bearer " + TEST_JWT,
-        }
-    });
+    await cookieJar.setCookie(COOKIE, 'http://0.0.0.0/v1/login')
 
     const profile = await got.get('http://0.0.0.0/v1/user/me/profile', {
         cookieJar,
         port: t.context.port,
         responseType: 'json',
         headers: {
-            "X-CSRF": login.body.csrf,
+            "X-CSRF": CSRF,
         }
     });
 
@@ -183,28 +187,17 @@ test.todo("metadata/age-range");
 test.todo("metadata/affliations");
 
 test("create image meta", async t => {
-    const parentDir = t.context.parentDir;
-    const dbUrl = t.context.dbUrl;
-
-    execSync(`/usr/bin/psql -f fixtures/1_user.sql ${dbUrl}`, { cwd: parentDir, env: { PGUSER: "postgres" }, encoding: 'utf8' });
+    await runFixtures(['fixtures/1_user.sql'], t.context.dbUrl, t.context.parentDir);
 
     const cookieJar = new tough.CookieJar();
-
-    const login = await got.post('http://0.0.0.0/v1/login', {
-        cookieJar,
-        port: t.context.port,
-        responseType: 'json',
-        headers: {
-            authorization: "Bearer " + TEST_JWT,
-        }
-    });
+    await cookieJar.setCookie(COOKIE, 'http://0.0.0.0/v1/login')
 
     const image = await got.post('http://0.0.0.0/v1/image', {
         cookieJar,
         port: t.context.port,
         responseType: 'json',
         headers: {
-            "X-CSRF": login.body.csrf,
+            "X-CSRF": CSRF,
         },
         json: {
             name: "test",
