0de68578a937079bf76b575068058f456b658618
David Komer <david.komer@gmail.com>
2020-08-11 11:01:06 +0300

    fastly cache

--- backend/fastly-purge/index.js
+++ backend/fastly-purge/index.js
@@ -26,9 +26,9 @@ const getMetadata = filename => {
     const noBrowserCache = ["wasm", "html", "css"].some(ext => hasExtension(ext) (filename));
 
     if(noBrowserCache) {
-        console.log(`not caching ${fileName} in browser`);
+        console.log(`not caching ${filename} in browser`);
     } else {
-        console.log(`caching ${fileName} in browser`);
+        console.log(`caching ${filename} in browser`);
     }
 
     let metaData = noBrowserCache
@@ -43,8 +43,8 @@ const getMetadata = filename => {
                 //surrogateControl: "max-age=2628000",
             };
 
-    if(hasWasmExtension(fileName)) {
-        console.log(`${fileName} is wasm, so changing contentType`);
+    if(hasWasmExtension(filename)) {
+        console.log(`${filename} is wasm, so changing contentType`);
         metaData.contentType = 'application/wasm';
     }
 
@@ -53,10 +53,10 @@ const getMetadata = filename => {
 }
 const makePurger = FASTLY_PUBLIC_BASEURL => async (obj, context) => {
     const baseUrl = FASTLY_PUBLIC_BASEURL.replace(/\/+$/, '');
-    const fileName = obj.name.replace(/^\/+/, '');
-    const completeObjectUrl = `${baseUrl}/${fileName}`;
+    const filename = obj.name.replace(/^\/+/, '');
+    const completeObjectUrl = `${baseUrl}/${filename}`;
 
-    console.log(`got purge request for object: ${obj.name} in bucket ${obj.bucket} filename: ${fileName}`);
+    console.log(`got purge request for object: ${obj.name} in bucket ${obj.bucket} filename: ${filename}`);
     const file = storage.bucket(obj.bucket).file(obj.name);
 
     try {
@@ -64,10 +64,10 @@ const makePurger = FASTLY_PUBLIC_BASEURL => async (obj, context) => {
         const exists = existsData[0];
 
         if(!exists) {
-            console.warn(`${fileName} doesn't exist in storage (kinda weird), so not setting metadata`);
+            console.warn(`${filename} doesn't exist in storage (kinda weird), so not setting metadata`);
         } else {
-            console.log(`${fileName} exists, so setting metadata`);
-            await storage.bucket(obj.bucket).file(obj.name).setMetadata(getMetadata(fileName));
+            console.log(`${filename} exists, so setting metadata`);
+            await storage.bucket(obj.bucket).file(obj.name).setMetadata(getMetadata(filename));
         }
         console.log(`making purge request for ${completeObjectUrl}`);
 
@@ -79,7 +79,7 @@ const makePurger = FASTLY_PUBLIC_BASEURL => async (obj, context) => {
          
         const data = await resp.json();
 
-        console.log(`Purged ${fileName}, ID ${data.id}`);
+        console.log(`Purged ${filename}, ID ${data.id}`);
     } catch(err) {
             console.error("got error in purge!");
             console.error(err);
