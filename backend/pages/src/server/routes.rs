3999bf506ce6d9603e1b193d7cd897fdc1c36611
David Komer <david.komer@gmail.com>
2020-09-03 13:13:12 +0300

    pages template
    * template loads CSS per SPA
    * routes support admin/*

--- backend/pages/src/server/routes.rs
+++ backend/pages/src/server/routes.rs
@@ -2,7 +2,7 @@ use crate::templates::{
     direct::{direct_template_home, direct_template_no_auth},
     epoch::epoch_page,
     info::info_template,
-    spa::spa_user_template,
+    spa::{spa_user_template, spa_admin_template}
 };
 use actix_web::web::{self, ServiceConfig};
 
@@ -10,6 +10,8 @@ pub fn configure(config: &mut ServiceConfig) {
     config
         .route("/user.*", web::get().to(spa_user_template))
         .route("/user", web::get().to(spa_user_template))
+        .route("/admin.*", web::get().to(spa_admin_template))
+        .route("/admin", web::get().to(spa_admin_template))
         .route("/", web::get().to(direct_template_home))
         .route("/no-auth", web::get().to(direct_template_no_auth))
         .route("/info", web::get().to(info_template))
