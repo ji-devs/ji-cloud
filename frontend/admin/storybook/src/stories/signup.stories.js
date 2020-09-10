b646910e7ecd3c8cecd7040bddeb8e3bc26facfb
Naomi Avraham <naomikudren@gmail.com>
2020-09-10 14:54:46 +0300

    signup pages

--- frontend/admin/storybook/src/stories/signup.stories.js
+++ frontend/admin/storybook/src/stories/signup.stories.js
@@ -1,3 +1,12 @@
+import {renderTemplate as tmpl} from "@utils/template";
+import {appendId, toggleClassesId} from "@utils/dom";
+import login from "@templates/login/login.html";
+
 export default {
   title: 'Sign up',
 }
+
+export const Login = () =>
+    tmpl(login, {
+
+});
