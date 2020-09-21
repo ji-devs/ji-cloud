ac4ea8ce761316b8321dc95e7e844c199c9a310e
David Komer <david.komer@gmail.com>
2020-09-21 19:15:07 +0300

    register page

--- frontend/user/app/src/pages/register/actions.rs
+++ frontend/user/app/src/pages/register/actions.rs
@@ -11,12 +11,11 @@ use core::{
         FetchResult,
         user::fetch_register,
     },
-    storage,
 };
 use serde::{Serialize, Deserialize};
 use wasm_bindgen::UnwrapThrowExt;
 use wasm_bindgen_futures::{JsFuture, spawn_local, future_to_promise};
-use crate::utils::firebase::get_firebase_register_google;
+use crate::utils::firebase::*;
 use futures_signals::signal::{Mutable, Signal, SignalExt};
 use dominator::clone;
 use std::rc::Rc;
@@ -31,87 +30,132 @@ pub enum RegisterStatus {
     Busy,
     Failure,
     ConfirmEmail,
+    EmptyPw,
+    PwMismatch,
+    PwWeak,
+    EmptyGivenName,
+    EmptyLastName,
+    EmptyUserName,
+    EmptyEmail,
+    EmailExists,
+    UsernameExists,
+    Over18,
+    UnknownFirebase,
+    Technical 
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum PwInvalid {
+    Empty,
+    Mismatch
 }
 
 impl RegisterStatus {
     pub fn to_string(&self) -> String {
         match self {
-            Self::Busy => "registering...".to_string(),
-            Self::Failure => "failed to register!".to_string(),
-            Self::ConfirmEmail => "confirm your email!".to_string(),
+            Self::Busy => "registering...",
+            Self::Failure => "failed to register!",
+            Self::ConfirmEmail => "confirm your email!",
+            Self::EmptyPw => "supply a password!",
+            Self::PwMismatch => "passwords don't match!",
+            Self::PwWeak => "weak password!",
+            Self::EmptyGivenName => "supply a first name!",
+            Self::EmptyLastName => "supply a last name!",
+            Self::EmptyUserName => "supply a user name!",
+            Self::EmptyEmail => "supply an email address!",
+            Self::Over18 => "Check the age restriction!",
+            Self::UsernameExists => "Username in use!",
+            Self::EmailExists => "Email in use!",
+            Self::UnknownFirebase => "firebase error!",
+            Self::Technical => "technical error!",
+        }.to_string()
+    }
+}
 
+
+
+pub async fn register_email(email: &str, pw: &str) -> Result<String, RegisterStatus> {
+    let token_promise = unsafe { get_firebase_register_email(email, pw) };
+
+    JsFuture::from(token_promise).await
+        .map(|info| {
+            let user:EmailRegisterInfo = serde_wasm_bindgen::from_value(info).unwrap_throw();
+            user.token
+        })
+        .map_err(|err| {
+            match serde_wasm_bindgen::from_value::<FirebaseError>(err) {
+                Ok(err) => {
+                    match err.code.as_ref() {
+                        "auth/email-already-in-use" => RegisterStatus::EmailExists,
+                        "auth/weak-password" => RegisterStatus::PwWeak,
+                        _ => RegisterStatus::UnknownFirebase
+                    }
+                },
+                Err(uhh) => {
+                    RegisterStatus::Technical
+                }
+            }
+        })
+}
+
+pub async fn register_google() -> Result<GoogleRegisterInfo, Option<RegisterStatus>> {
+    let token_promise = unsafe { get_firebase_register_google() };
+
+    JsFuture::from(token_promise).await
+        .map(|info| {
+            serde_wasm_bindgen::from_value::<GoogleRegisterInfo>(info).unwrap_throw()
+        })
+        .map_err(|err| {
+            None
+            /*
+            match serde_wasm_bindgen::from_value::<FirebaseError>(err) {
+                Ok(err) => {
+                    match err.code.as_ref() {
+                        "auth/email-already-in-use" => RegisterStatus::EmailExists,
+                        "auth/weak-password" => RegisterStatus::PwWeak,
+                        _ => RegisterStatus::UnknownFirebase
+                    }
+                },
+                Err(uhh) => {
+                    RegisterStatus::Technical
+                }
+            }
+            */
+        })
+}
+pub async fn create_user(
+    token: String,
+    username: String,
+    given_name: String,
+    family_name: String,
+    email: String,
+) -> Result<String, RegisterStatus> {
+    let req = RegisterRequest {
+        username,
+        email,
+        given_name,
+        family_name,
+        over_18: true,
+        language: "en".to_string(),
+        locale: "en".to_string(),
+        timezone: chrono_tz::Tz::Asia__Jerusalem,
+        opt_into_edu_resources: true,
+        organization: "ji".to_string()
+    };
+
+    let resp:FetchResult<RegisterSuccess, RegisterError> = fetch_register(&token, &req).await;
+    match resp {
+        Ok(resp) => match resp {
+            RegisterSuccess::Signin(csrf) => Ok(csrf),
+            RegisterSuccess::ConfirmEmail => Err(RegisterStatus::ConfirmEmail)
+        }, 
+        Err(_) => {
+            Err(RegisterStatus::Technical)
         }
     }
 }
+
 /*
-
-#[derive(Deserialize)]
-struct GoogleRegisterInfo {
-    avatar: String,
-    email: String,
-    name: String,
-    token: String
-}
-
-pub async fn register_google(page:Rc<RegisterPage>) {
-    let token_promise = unsafe { get_firebase_register_google() };
-
-    let refs = page.refs.borrow();
-    let refs = refs.as_ref().unwrap_throw();
-
-    let username = refs.username();
-    let family_name = refs.family_name();
-    let given_name = refs.given_name();
-    match JsFuture::from(token_promise).await {
-        Ok(info) => {
-            let user:GoogleRegisterInfo = serde_wasm_bindgen::from_value(info).unwrap_throw();
-            //let (first_name, last_name) = parse_name(&user.name);
-            let req = RegisterRequest {
-                username,
-                email: user.email,
-                over_18: true,
-                given_name,
-                family_name,
-                language: "en".to_string(),
-                locale: "en".to_string(),
-                timezone: chrono_tz::Tz::Asia__Jerusalem,
-                opt_into_edu_resources: true,
-                organization: "ji".to_string()
-            };
-
-            let resp:FetchResult<RegisterSuccess, RegisterError> = fetch_register(&user.token, &req).await;
-            match resp {
-                Ok(resp) => match resp {
-                    RegisterSuccess::Signin(csrf) => do_success(&page, csrf),
-                    RegisterSuccess::ConfirmEmail => page.status.set(Some(RegisterStatus::ConfirmEmail)),
-                }, 
-                Err(_) => {
-                    page.status.set(Some(RegisterStatus::Failure))
-                }
-            }
-        },
-        Err(_) => {
-            //not really an error, probably a cancel
-            page.status.set(None);
-        }
-    };
-}
-fn do_success(page:&RegisterPage, csrf:String) {
-    storage::save_csrf_token(&csrf);
-
-    let route:String = Route::User(UserRoute::Profile).into();
-    dominator::routing::go_to_url(&route);
-
-    ///generally speaking this kind of thing isn't necessary
-    ///futures will just resolve and be dropped as part of the flow
-    ///but because the oauth flow here opens a separate window
-    ///it's more at risk to leave dangling Futures
-    ///specifically, here, dangling futures which hold the Rc that holds it
-    ///thereby creating a cycle, we need to break by cancelling that future
-    ///see: https://github.com/jewish-interactive/ji-cloud/issues/78
-    page.loader.cancel();
-}
-
 fn parse_name(name:&str) -> (String, String) {
     let names_split:Vec<&str> = 
         name
