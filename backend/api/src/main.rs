5667b9f67b0ddafad8856e4f5f4de0e5370191fc
Chloe Ross <orangesnowfox@gmail.com>
2020-10-14 16:10:15 -0700

    refactor: replace most of `core`'s features with runtime config

--- backend/api/src/main.rs
+++ backend/api/src/main.rs
@@ -1,4 +1,4 @@
-use core::settings::SettingsManager;
+use core::settings::{self, SettingsManager};
 use ji_cloud_api::{algolia, db, http, jwkkeys, logger, s3};
 use std::thread;
 
@@ -8,19 +8,30 @@ async fn main() -> anyhow::Result<()> {
 
     logger::init()?;
 
-    let settings: SettingsManager = SettingsManager::new().await?;
+    let (runtime_settings, jwk_verifier, s3, algolia, db_pool) = {
+        let remote_target = settings::read_remote_target()?;
 
-    let runtime_settings = settings.runtime_settings().await?;
+        let settings: SettingsManager = SettingsManager::new(remote_target).await?;
 
-    let jwk_verifier = jwkkeys::create_verifier(settings.jwk_settings().await?);
+        let runtime_settings = settings.runtime_settings().await?;
 
-    let _ = jwkkeys::run_task(jwk_verifier.clone());
+        let jwk_verifier = jwkkeys::create_verifier(settings.jwk_settings().await?);
 
-    let s3 = s3::S3Client::new(settings.s3_settings().await?)?;
+        let _ = jwkkeys::run_task(jwk_verifier.clone());
 
-    let algolia = algolia::AlgoliaClient::new(settings.algolia_settings().await?)?;
+        let s3 = s3::S3Client::new(settings.s3_settings().await?)?;
 
-    let db_pool = db::get_pool(settings.db_connect_options().await?).await?;
+        let algolia = algolia::AlgoliaClient::new(settings.algolia_settings().await?)?;
+
+        let db_pool = db::get_pool(
+            settings
+                .db_connect_options(settings::read_sql_proxy())
+                .await?,
+        )
+        .await?;
+
+        (runtime_settings, jwk_verifier, s3, algolia, db_pool)
+    };
 
     let algolia_syncer = algolia::Updater {
         db: db_pool.clone(),
