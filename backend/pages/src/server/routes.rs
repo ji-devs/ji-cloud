7aa798d33ebf479cba79ec0790f708c00d68bc39
Chloe Ross <orangesnowfox@gmail.com>
2020-10-23 12:57:35 -0700

    feat(pages): Spa Pages for `jig`

--- backend/pages/src/server/routes.rs
+++ backend/pages/src/server/routes.rs
@@ -2,16 +2,18 @@ use crate::templates::{
     direct::{direct_template_home, direct_template_no_auth},
     epoch::epoch_page,
     info::info_template,
-    spa::{spa_admin_template, spa_user_template},
+    spa,
 };
 use actix_web::web::{self, ServiceConfig};
 
 pub fn configure(config: &mut ServiceConfig) {
     config
-        .route("/user.*", web::get().to(spa_user_template))
-        .route("/user", web::get().to(spa_user_template))
-        .route("/admin.*", web::get().to(spa_admin_template))
-        .route("/admin", web::get().to(spa_admin_template))
+        .route("/user.*", web::get().to(spa::user_template))
+        .route("/user", web::get().to(spa::user_template))
+        .route("/admin.*", web::get().to(spa::admin_template))
+        .route("/admin", web::get().to(spa::admin_template))
+        .route("/jig.*", web::get().to(spa::jig_template))
+        .route("/jig", web::get().to(spa::jig_template))
         .route("/", web::get().to(direct_template_home))
         .route("/no-auth", web::get().to(direct_template_no_auth))
         .route("/info", web::get().to(info_template))
