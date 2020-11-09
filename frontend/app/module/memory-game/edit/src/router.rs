2d7603cc43161e45e793353135d9d17528cda202
David Komer <david.komer@gmail.com>
2020-11-09 18:42:26 +0200

    initial memory game

---
+++ frontend/app/module/memory-game/edit/src/router.rs
@@ -0,0 +1,51 @@
+use core::routes::{Route, ModuleRoute};
+use shared::domain::jig::ModuleKind;
+use std::rc::Rc;
+use wasm_bindgen::UnwrapThrowExt;
+use web_sys::Url;
+use futures_signals::{
+    map_ref,
+    signal::{Mutable, SignalExt, Signal}
+};
+use dominator::{Dom, html};
+use crate::pages::{
+    mode::ModePage,
+};
+
+pub struct Router {
+}
+
+impl Router {
+    pub fn new() -> Self {
+        Self { }
+    }
+
+    fn signal() -> impl Signal<Item = Route> {
+        dominator::routing::url()
+            .signal_ref(|url| Route::from_url(&url))
+    }
+
+    fn dom_signal() -> impl Signal<Item = Option<Dom>> {
+        Self::signal()
+            .map(|route| {
+                match route {
+                    Route::Module(route) => {
+                        match route {
+                            ModuleRoute::Edit(kind, jig_id, module_id) => {
+                                match kind {
+                                    ModuleKind::MemoryGame => Some(ModePage::render(ModePage::new())),
+                                    _ => None
+                                }
+                            }
+                            _ => None
+                        }
+                    },
+                    _ => None
+                }
+            })
+    }
+    
+    pub fn render(&self) -> Dom {
+        html!("main", { .child_signal(Self::dom_signal()) } )
+    }
+}
---
+++ frontend/app/module/memory-game/edit/src/utils/templates.rs
@@ -0,0 +1,48 @@
+use simple_html_template::{TemplateCache, html_map};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::HtmlElement;
+use core::settings::SETTINGS;
+use std::fmt;
+
+thread_local! {
+    pub static TEMPLATES: Templates = Templates::new(); 
+}
+
+
+const MODE_PAGE:&'static str = "mode-page";
+
+pub fn mode_page() -> HtmlElement {
+    TEMPLATES.with(|t| t.cache.render_elem_plain(MODE_PAGE))
+}
+
+pub struct Templates {
+    pub cache: TemplateCache<'static>
+}
+
+impl fmt::Debug for Templates {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        
+        f.debug_list()
+            .entries(self.cache.templates.keys())
+         .finish()
+    }
+}
+impl Templates {
+    pub fn new() -> Self {
+        let cache = TemplateCache::new(&vec![
+            (MODE_PAGE, get_template_str(include_str!("../../../../../../.template_output/memory/memory-creator.html"))),
+        ]);
+
+        Self { cache }
+    }
+
+}
+
+//replace {{MEDIA_UI}} in the template string
+//this leaks memory - which is okay since templates exist for the lifetime of the app
+fn get_template_str(s:&'static str) -> &'static str {
+    unsafe {
+        Box::leak(SETTINGS.get_unchecked().remote_target.replace_media_ui(s).into_boxed_str())
+    }
+}
