723871d7488fbf948c7a58b7b90912dbcce3e509
David Komer <david.komer@gmail.com>
2020-11-18 14:33:31 +0200

    abstract card to grid config

--- frontend/storybook/src/stories/module/memory-game/memory-common.js
+++ frontend/storybook/src/stories/module/memory-game/memory-common.js
@@ -28,3 +28,20 @@ export const mockThemes = [
       label: 'Chalk',
     }
 ];
+
+export const nCardsToGrid = (nCards) => {
+  switch(nCards) {
+    case 8: return 1;
+    case 10: return 2;
+    case 12: return 1;
+    case 14: return 5;
+    case 16: return 1;
+    case 18: return 6;
+    case 20: return 2;
+    case 22: return 7;
+    case 24: return 3;
+    case 26: return 8;
+    case 28: return 4;
+    default: return -1;
+  }
+};
--- frontend/storybook/src/stories/module/memory-game/memory-play-duplicate.stories.js
+++ frontend/storybook/src/stories/module/memory-game/memory-play-duplicate.stories.js
@@ -1,7 +1,7 @@
 import {renderTemplate as tmpl} from "@utils/template";
 import {appendId, appendValueLineId, getChildId, setValueId, toggleClasses, appendTextLineId, toggleClassesId, setTextId} from "@utils/dom";
 import {startResizerOnElement} from "@utils/resize";
-import {mockWords, mockThemes} from "./memory-common";
+import {mockWords, mockThemes, nCardsToGrid} from "./memory-common";
 import modulePage from "@templates/_common/module/module-page.html";
 import playerTmpl from "@templates/module/memory/play/player.html";
 import cardPlayTmpl from "@templates/module/memory/play/memory-card.html";
@@ -10,24 +10,26 @@ export default {
   title: 'Modules/Memory-Game/Play/Duplicate',
 }
 
-const makePlayer = (nCards, nGrid) => () =>  mockPlayer(0, false, nCards, nGrid);
-export const Play_8_Cards= makePlayer(8, 1);
-export const Play_10_Cards = makePlayer(10, 2);
-export const Play_12_Cards = makePlayer(12, 1);
-export const Play_14_Cards = makePlayer(14, 5);
-export const Play_16_Cards = makePlayer(16, 1);
-export const Play_18_Cards = makePlayer(18, 6);
-export const Play_20_Cards = makePlayer(20, 2);
-export const Play_22_Cards = makePlayer(22, 7);
-export const Play_24_Cards = makePlayer(24, 3);
-export const Play_26_Cards = makePlayer(26, 8);
-export const Play_28_Cards = makePlayer(28, 4);
+const makePlayer = (nCards) => () =>  mockPlayer(0, false, nCards);
+export const Play_8_Cards= makePlayer(8);
+export const Play_10_Cards = makePlayer(10);
+export const Play_12_Cards = makePlayer(12);
+export const Play_14_Cards = makePlayer(14);
+export const Play_16_Cards = makePlayer(16);
+export const Play_18_Cards = makePlayer(18);
+export const Play_20_Cards = makePlayer(20);
+export const Play_22_Cards = makePlayer(22);
+export const Play_24_Cards = makePlayer(24);
+export const Play_26_Cards = makePlayer(26);
+export const Play_28_Cards = makePlayer(28);
 
-export const Player_Theme_1 = () => mockPlayer(1, false, 12, 1);
-export const Player_Theme_1_Flipped = () => mockPlayer(1, true, 12, 1);
+export const Player_Theme_1 = () => mockPlayer(1, false, 12);
+export const Player_Theme_1_Flipped = () => mockPlayer(1, true, 12);
 //Helpers
 
-function mockPlayer(selectedThemeIndex, isFlipped, nCards, nGrid) {
+function mockPlayer(selectedThemeIndex, isFlipped, nCards) {
+    const nGrid = nCardsToGrid(nCards);
+
     const _page = appendId(tmpl(modulePage), "module-content", tmpl(playerTmpl));
     const page = appendMockCards(_page, {isFlipped, nCards});
     if(selectedThemeIndex) {
