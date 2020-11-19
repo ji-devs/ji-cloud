a68207d3068fa3122764e70b19055f4d41d6805a
David Komer <david.komer@gmail.com>
2020-11-19 20:05:24 +0200

    dev setup

--- frontend/storybook/.storybook/main.js
+++ frontend/storybook/.storybook/main.js
@@ -7,12 +7,26 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     {
-		name: '@storybook/addon-essentials',
-		options: {
-			backgrounds: false,
-			actions: false,
-			controls: false
-		}
-	}
-  ]
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false,
+        actions: false,
+        controls: false
+      }
+    }
+  ],
+
+  webpackFinal: makeWebpackFinal
+}
+
+async function makeWebpackFinal(config, {configType}) {
+  [".html", ".css"]
+    .forEach(ext => {
+      if(config.resolve.extensions.indexOf(ext) == -1) {
+        console.warn(`${ext} was not in webpack resolve.extensions! adding...`);
+        config.resolve.extensions.push(ext);
+      }
+    });
+
+  return config
 }
