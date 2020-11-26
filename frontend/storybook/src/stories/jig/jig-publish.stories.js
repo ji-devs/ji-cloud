1358ece385d08a08f7258a941652d3d3600d3692
Naomi Avraham <naomikudren@gmail.com>
2020-11-26 13:38:55 +0200

    Adding to publish pages

--- frontend/storybook/src/stories/jig/jig-publish.stories.js
+++ frontend/storybook/src/stories/jig/jig-publish.stories.js
@@ -4,6 +4,10 @@ import jigpublishone from "@templates/jig/publish/publish-one.html";
 import jigpublishtooltip from "@templates/jig/publish/publish-error-tooltip.html";
 import jiglanguagedropdown from "@templates/jig/publish/publish-language-dropdown.html";
 import jigpublishage from "@templates/jig/publish/publish-age.html";
+import jigpublishcategorydropdown from "@templates/jig/publish/publish-categories-dropdown.html";
+import jigpublishcategories from "@templates/jig/publish/publish-selected-category.html";
+
+
 
 
 export default {
@@ -41,6 +45,26 @@ export const JigPublishAge = () =>  {
     const pageContents = tmpl(jigpublishage);
 
     appendId(pageContainer, "publish-language-dropdown", pageContents);
+
+    return pageContainer;
+}
+
+export const JigPublishCategoryDropdown = () =>  {
+    const pageContainer = tmpl(jigpublishone);
+
+    const pageContents = tmpl(jigpublishcategorydropdown);
+
+    appendId(pageContainer, "publish-category-dropdown", pageContents);
+
+    return pageContainer;
+}
+
+export const JigPublishCategories = () =>  {
+    const pageContainer = tmpl(jigpublishone);
+
+    const pageContents = tmpl(jigpublishcategories);
+
+    appendId(pageContainer, "publish-categories", pageContents);
 
     return pageContainer;
 }
