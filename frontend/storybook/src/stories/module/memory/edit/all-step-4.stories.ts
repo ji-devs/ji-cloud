00d43c75f3b2141de00236efc08abe70bfa6c3f6
David Komer <david.komer@gmail.com>
2020-12-01 17:59:42 +0200

    step 4 is iframe

--- frontend/storybook/src/stories/module/memory/edit/all-step-4.stories.ts
+++ frontend/storybook/src/stories/module/memory/edit/all-step-4.stories.ts
@@ -1,8 +1,10 @@
 import {renderTemplate as tmpl} from "@utils/template";
+import {setIframeContentsId, setIframeContents} from "@utils/dom";
 import { modulePage, ModulePageKind } from "@components/module";
 import { makeMainPairs } from "./_utils/main";
 import headerEmpty from "@templates/module/memory/edit/_common/header/empty.html";
 import footerDefault from "@templates/module/memory/edit/_common/footer/default.html";
+import iframeTmpl from "@templates/module/memory/edit/_common/main/iframe.html";
 
 export default {
   title: 'Modules/Memory/Edit/All Steps/Step 4',
@@ -17,7 +19,10 @@ interface Options {
 function makeStep({}:Options) {
   const pairKind = "text-image";
 
-  const main = makeMainPairs({pairKind, flipSecond: false, isEdit: false, themeIndex: 1}); 
+  const main = tmpl(iframeTmpl);
+  
+  setIframeContents(main, "<h1>Player here!</h1>");
+
   const header = tmpl(headerEmpty);
   const footer = tmpl(footerDefault);
 
