12c6546af771d7404fe87e0a8b4a1df643483b97
Naomi Avraham <naomikudren@gmail.com>
2020-12-15 12:22:47 +0200

    adding input

---
+++ frontend/storybook/src/components/input-text.ts
@@ -0,0 +1,13 @@
+import "@elements/inputs/input-text";
+import "@elements/buttons/rectangle-button";
+import {RectangleButtonBlueMedium} from "~/components/rectangle-button";
+export default {
+  title: 'Input Text',
+}
+
+export const InputTextIdle = () => {
+    return `<input-text label="Title">
+ 
+    </input-text>`
+}
+
---
+++ frontend/storybook/src/components/rectangle-button.ts
@@ -0,0 +1,21 @@
+import "@elements/buttons/rectangle-button";
+export default {
+  title: 'Rectangle Button',
+}
+
+export const RectangleButtonBlueMedium = () => {
+    return `<rectangle-button label="Placeholder" color="blue" size="medium" />`
+}
+
+export const RecButtonBlueLarge = () => {
+  return `<rectangle-button label="Placeholder" color="blue" size="large"/>`
+}
+
+export const RectangleButtonRedMedium = () => {
+  return `<rectangle-button label="Placeholder" color="red" size="medium" />`
+}
+
+export const RecButtonRedLarge = () => {
+  return `<rectangle-button label="Placeholder" color="red" size="large"/>`
+}
+
