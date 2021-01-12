fb2e0c4e979c32858b981ee2a67ec86d7305290f
Naomi Avraham <naomikudren@gmail.com>
2021-01-12 14:03:10 +0200

    adding component

--- frontend/storybook/src/components/full-pages/login/newpassword.ts
+++ frontend/storybook/src/components/full-pages/login/newpassword.ts
@@ -12,47 +12,40 @@ export default {
 }
 
 interface LoginArgs {
-    color: string,
-    passwordtitle:string,
-    noaccount:string,
     helpertext:string,
     errormessage: string,
     instruction: boolean,
     error: boolean,
-    label: string,
-    size:string
-
-  
+ 
   }
 
   const DEFAULT_ARGS:LoginArgs = {
-    
-    label: "Set Password",
-    passwordtitle: "Enter new Password",
-    noaccount: "You’ll be logged in automatically after this",
-    color: "red",
-    helpertext: "",
+ helpertext: "",
     errormessage: "",
     instruction: false,
-    size:"medium",
     error:false,
   }
 
-  const STR_NEWPASSWORD = "Create a New Password"
+  const STR_NEWPASSWORD = "Create a New Password";
+  const STR_LABEL = "Set Password";
+  const STR_MEDIUM = "medium";
+  const STR_RED = "red";
+  const STR_ENTERPASSWORD = "Enter new Password";
+  const STR_LOGGEDOUT = "You’ll be logged in automatically after this"
 
 export const LoginForgotPassword = (props?:LoginArgs) => {
 
-    const {color, size, passwordtitle,label, noaccount, helpertext,errormessage, instruction, error} = props || DEFAULT_ARGS;
+    const {helpertext,errormessage, instruction, error} = props || DEFAULT_ARGS;
 
 
     return `
     <login-full title="${STR_NEWPASSWORD}">
     
        
-        <input-text slot="password" label="${passwordtitle}" helpertext="${helpertext}" error="${errormessage}" ${instruction && "instruction"} ${error && "error"} >
+        <input-text slot="password" label="${STR_ENTERPASSWORD}" helpertext="${helpertext}" error="${errormessage}" ${instruction && "instruction"} ${error && "error"} >
         </input-text>
-        <div slot="submit">${RectangleButton({label:label, color: color,size: size})}</div>
-        <plain-black title="${noaccount}" slot="noaccount"></plain-blue>
+        <div slot="submit">${RectangleButton({label:STR_LABEL, color: STR_RED,size: STR_MEDIUM, imgrighthidden:true, imglefthidden:true,})}</div>
+        <plain-black title="${STR_LOGGEDOUT}" slot="noaccount"></plain-blue>
     </login-full>
     
     `
