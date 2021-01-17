d14172341bf4ce6a760d5d1b87ed03e240fedb8d
Naomi Avraham <naomikudren@gmail.com>
2021-01-17 12:20:33 +0200

    Revert "Merge remote-tracking branch 'rivka/sandbox'"
    This reverts commit d446a7602cec7e172b59c35ffedc76b51c284e31, reversing
    changes made to 16002a07e25071c3f990c331526617841f15d5a2.

--- frontend/elements/src/icon-wparagraph.ts
+++ frontend/elements/src/icon-wparagraph.ts
@@ -1,85 +1,85 @@
-import { MEDIA_UI } from '@utils/path';
-import { LitElement, html, css, customElement, property } from 'lit-element';
-@customElement('icon-wparagraph')
-export class _ extends LitElement {
-  static get styles() {
-    return [css`
-  img-ui{
-    width: 246px;
-    height: 236px;
-    object-fit: contain;
+// import { MEDIA_UI } from '@utils/path';
+// import { LitElement, html, css, customElement, property } from 'lit-element';
+// @customElement('icon-wparagraph')
+// export class _ extends LitElement {
+//   static get styles() {
+//     return [css`
+//   img-ui{
+//     width: 246px;
+//     height: 236px;
+//     object-fit: contain;
 
-  }
-  h2{
-    font-family: Poppins;
-    font-size: 18px;
-    font-weight: bold;
+//   }
+//   h2{
+//     font-family: Poppins;
+//     font-size: 18px;
+//     font-weight: bold;
 
-  }
-  p{
-    font-family: Poppins;
-    font-size: 16px;
-    line-height: 1.5;
-    color: #383838;
-    margin-top:5px;
+//   }
+//   p{
+//     font-family: Poppins;
+//     font-size: 16px;
+//     line-height: 1.5;
+//     color: #383838;
+//     margin-top:5px;
 
-  }
-  .wrapper{
-    width: 274px;
-margin-left:50px;
+//   }
+//   .wrapper{
+//     width: 274px;
+// margin-left:50px;
 
-  }
-  .pink {
-    color: #fd6b71;
-  }
-  .darkblue{
-    color: #2040a3;
-  }
-  .green{
-    color: #46ba6f;
-  }
-  .orange{
-    color: #fea559;
-  }
-  .lightblue{
-    color:#6ca1fc;
-  }
+//   }
+//   .pink {
+//     color: #fd6b71;
+//   }
+//   .darkblue{
+//     color: #2040a3;
+//   }
+//   .green{
+//     color: #46ba6f;
+//   }
+//   .orange{
+//     color: #fea559;
+//   }
+//   .lightblue{
+//     color:#6ca1fc;
+//   }
 
-  .inside{
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr 1fr 1fr;
-  }
-    `]
-  }
+//   .inside{
+//     display: grid;
+//     grid-template-columns: 1fr;
+//     grid-template-rows: 1fr 1fr 1fr;
+//   }
+//     `]
+//   }
 
 
-  @property()
-  path:string = ""; 
-  @property()
-  paragraph:string = ""; 
-  @property()
-  title:string = ""; 
-  @property()
-  color:string = ""; 
+//   @property()
+//   path:string = ""; 
+//   @property()
+//   paragraph:string = ""; 
+//   @property()
+//   title:string = ""; 
+//   @property()
+//   color:string = ""; 
  
 
-  render() {
+//   render() {
 
-    const {path, paragraph,title,color} = this;
+//     const {path, paragraph,title,color} = this;
 
-    return html`
-     <div class="wrapper">
-        <img-ui class="img" path="${path}"></img-ui>
-        <div class="inside">
-          <h2 class="${color}">${title}</h2>
-          <p>${paragraph}</p>
-          <slot></slot>
-        </div>
-      </div>
+//     return html`
+//      <div class="wrapper">
+//         <img-ui class="img" path="${path}"></img-ui>
+//         <div class="inside">
+//           <h2 class="${color}">${title}</h2>
+//           <p>${paragraph}</p>
+//           <slot></slot>
+//         </div>
+//       </div>
         
-  `;
-  }
-}
+//   `;
+//   }
+// }
 
  
