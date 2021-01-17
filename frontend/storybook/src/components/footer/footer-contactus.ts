19ceb5a29a264dd24c3c73d8cb3714fd225db440
Naomi Avraham <naomikudren@gmail.com>
2021-01-17 12:20:42 +0200

    Merge remote-tracking branch 'rivka/sandbox'

--- frontend/storybook/src/components/footer/footer-contactus.ts
+++ frontend/storybook/src/components/footer/footer-contactus.ts
@@ -7,16 +7,16 @@ export default {
   }
 
 
-  const STR_TITLE="Contact us"
-  const STR_LINE1="info@jewishinteractive.org"
-  const STR_LINE2="Ji United States"
-  const STR_LINE3=" Tel: +1 (703) 517-5182"
-  const STR_LINE4="  Ji United Kingdom"
-  const STR_LINE5="   Tel: +44 (0)79 6641 4417  "
-  const STR_LINE6="   Ji South Africa   "
-  const STR_LINE7="  Tel: +27 (79) 886 5326  "
-  const STR_LINE8="  Ji Israel  "
-  const STR_LINE9="  Tel: +972 (0) 54-597 9555"  
+  const STR_TITLE="Contact us";
+  const STR_LINE1="info@jewishinteractive.org";
+  const STR_LINE2="Ji United States";
+  const STR_LINE3=" Tel: +1 (703) 517-5182";
+  const STR_LINE4="Ji United Kingdom";
+  const STR_LINE5="Tel: +44 (0)79 6641 4417  ";
+  const STR_LINE6=" Ji South Africa";
+  const STR_LINE7="  Tel: +27 (79) 886 5326  ";
+  const STR_LINE8="  Ji Israel  ";
+  const STR_LINE9="  Tel: +972 (0) 54-597 9555";  
 
   
 
