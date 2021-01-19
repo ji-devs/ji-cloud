b7d94d803d6d9823822abcbd400cf2b312520218
David Komer <david.komer@gmail.com>
2021-01-19 14:10:47 +0200

    dropdown element

--- frontend/elements/src/core/lists/list-height.ts
+++ frontend/elements/src/core/lists/list-height.ts
@@ -1,6 +1,6 @@
 import { MEDIA_UI } from '@utils/path';
 import { LitElement, html, css, customElement, property } from 'lit-element';
-@customElement('list-400')
+@customElement('list-height')
 export class _ extends LitElement {
   static get styles() {
     return [css`
