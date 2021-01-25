6ac862f5c5997e9aac3607c4017f665569d0625b
Naomi Avraham <naomikudren@gmail.com>
2021-01-25 17:03:12 +0200

    PR Fixes

--- frontend/storybook/src/components/entry/publish/pages/publish-two.ts
+++ frontend/storybook/src/components/entry/publish/pages/publish-two.ts
@@ -5,10 +5,12 @@ import "@elements/widgets/tags/icon";
 import "@elements/entry/jig/publish/publish-dropdown";
 import "@elements/core/inputs/textarea";
 import "@elements/core/dividers/spacer-fourty";
-import "@elements/core/titles/variants/title-w-icon";
+import "@elements/entry/jig/publish/dropdown-list-object";
+import "@elements/entry/jig/publish/publish-embed";
 import { Rectangle } from "~/components/core/buttons/rectangle";
+import {Color, Size, IconAfter, IconBefore} from "@elements/core/buttons/rectangle";
 import { colorStyles } from "@elements/_styles/colors";
-
+import "@elements/widgets/tooltips/right";
 export default {
   title: 'Entry/Jig/Publish/Pages',
 }
@@ -16,15 +18,16 @@ export default {
   
   interface PublishArgs {
     closed:boolean,
+    embedclosed:boolean,
   
     }
 
     const DEFAULT_ARGS:PublishArgs = {
-       closed:true,
+       closed:false,
+       embedclosed:false,
       }
 
-      const STR_TITLE = "Your JIG is on air now";
-      const STR_SUBTITLE = "What would you like to do next?";
+      
       const STR_MEDIUM = "medium";
       const STR_WHITE = "white";
       const STR_SHARE = "share the JIG";
@@ -38,6 +41,8 @@ export default {
       const STR_SHARESTUDENT ="Share with Students";
       const STR_URLICON = "icn-url.svg";
       const STR_URL ="Copy URL";
+      const STR_EMBED ="Embed this JIG";
+      const STR_EMBEDICON ="Icn_Embed.svg";
 
       
 
@@ -45,23 +50,31 @@ export default {
 
 export const PublishFullTwo = (props?:PublishArgs) => {
 
- const {closed} = props || DEFAULT_ARGS;
+ const {closed, embedclosed} = props || DEFAULT_ARGS;
  
 
     return `
-    <publish-page-two title="${STR_TITLE}" subtitle="${STR_SUBTITLE}">
+    <publish-page-two>
         <placeholder-img slot="animation"></placeholder-img>
         <div slot="button-collection">
         <rectangle-button label="${STR_SHARE}" imgrighthidden=true size="${STR_MEDIUM} color="${STR_WHITE}" path="${STR_ICNSHARE}" imglefthidden=false bold=false italic=false></rectangle-button>
-        ${Rectangle({contents:STR_SHARE, size:STR_MEDIUM,color:STR_WHITE, bold:false, italic:false })}
+        ${Rectangle({contents:STR_SHARE, size:STR_MEDIUM,color:STR_WHITE, bold:false, italic:false, iconBefore:"share"})}
         
         </div>
         <publish-dropdown ${closed && 'closed'} title="${STR_DROPDOWNTITLE}" slot="dropdown">
-          <icon-wtext path="${STR_STUDENT}" text="${STR_SHARESTUDENT}"></icon-wtext>
-          <icon-wtext icon="${STR_URLICON}" text="${STR_URL}"></icon-wtext>
+          <dropdown-list-object  slot="list" icon="${STR_STUDENT}" label="${STR_SHARESTUDENT}"></dropdown-list-object>
+          <dropdown-list-object slot="list" icon="${STR_EMBEDICON}" label="${STR_EMBED}"></dropdown-list-object>
+          <dropdown-list-object slot="list" icon="${STR_URLICON}" label="${STR_URL}">
+          <tooltip-right kind="success" slot="tooltip">Copied to the clipboard</tooltip-right>
+          </dropdown-list-object>
+          
         </publish-dropdown>
-        <div slot="button-collection">${Rectangle({contents:STR_CREATE, size:STR_MEDIUM,color:STR_WHITE,  bold:false, italic:false  })}</div>
-        <div slot="button-collection">${Rectangle({contents:STR_PLAY, size:STR_MEDIUM,color:STR_WHITE, bold:false, italic:false  })}
+        <publish-embed slot="dropdown" ${embedclosed && "closed"}>
+          <tooltip-right kind="success" slot="tooltip">Copied to the clipboard</tooltip-right>
+
+        </publish-embed>
+        <div slot="button-collection">${Rectangle({contents:STR_CREATE, size:STR_MEDIUM,color:STR_WHITE,  bold:false, italic:false, iconBefore:"create "})}</div>
+        <div slot="button-collection">${Rectangle({contents:STR_PLAY, size:STR_MEDIUM,color:STR_WHITE, bold:false, italic:false, iconBefore:"play"  })}
     
         </div>
 
