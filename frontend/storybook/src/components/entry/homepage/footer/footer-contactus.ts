fa526afb50a9e37256d00e10891e76d364e65df0
Naomi Avraham <naomikudren@gmail.com>
2021-01-25 16:27:39 +0200

    PR fixes

--- frontend/storybook/src/components/entry/homepage/footer/footer-contactus.ts
+++ frontend/storybook/src/components/entry/homepage/footer/footer-contactus.ts
@@ -1,36 +1,15 @@
-import "@elements/entry/home/TOSORT/column-details";
+import "@elements/entry/home/sections/contactus-section.ts";
 import "@elements/entry/home/TOSORT/column-list";
 
 export default {
     title: 'Homepage Paragraph',
   }
 
-
-  const STR_TITLE="";
-  const STR_LINE1="";
-  const STR_LINE2="";
-  const STR_LINE3="";
-  const STR_LINE4="";
-  const STR_LINE5="";
-  const STR_LINE6="";
-  const STR_LINE7="";
-  const STR_LINE8="";
-  const STR_LINE9=""  ;
-  const STR_LINE10="                         " ; 
-
-  const STR_WHITE ="";
-  const STR_PATHYOUTUBE="";
-  const STR_PATHFACEBOOK="Icn_Youtube.png";
-  const STR_PATHINSTAGRAM="Icn_Youtube.png";
-  const STR_PATHLINKEDIN="Icn_Youtube.png";
-
-
-
     export const footercontactus= () => {
     return `
-    <column-details head_title="">
+    <contactus-section>
   
 
-     </column-details>
+     </contactus-section>
     `
 }
--- frontend/storybook/src/components/entry/homepage/footer/footer-whoweare.ts
+++ frontend/storybook/src/components/entry/homepage/footer/footer-whoweare.ts
@@ -1,4 +1,4 @@
-import "@elements/entry/home/TOSORT/column-details";
+import "@elements/entry/home/sections/whoweare-section.ts";
 import "@elements/entry/home/TOSORT/column-list";
 import "@elements/core/buttons/rectangle";
 
@@ -6,24 +6,13 @@ import "@elements/core/buttons/rectangle";
 export default {
     title: 'Homepage Paragraph',
   }
-  const STR_TITLE="";
-  const STR_LINE1="   ";
-  const STR_LINE2="";
-  const STR_LINE3="  ";
-  const STR_LINE4=" ";
-  const STR_LINE5="    ";
-  const STR_LINE6=" ";
-  const STR_LINE7=" ";
-  const STR_WHITE ="";
-  const STR_DARKBLUE="darkblue";
-  const STR_LINE10="                        "; 
-  const STR_DONATE="";
+  
 
 export const footerwhoweare= () => {
     return `
-    <column-details head_title="${STR_TITLE}">
+    <whoweare-section>
 
 
-     </column-details>
+     </whoweare-section>
     `
 }
