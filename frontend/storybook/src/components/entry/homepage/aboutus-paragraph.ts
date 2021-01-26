dd493b8a1e492896491e3abdc477af1402a27a70
Naomi Avraham <naomikudren@gmail.com>
2021-01-26 11:21:09 +0200

    PR fix

--- frontend/storybook/src/components/entry/homepage/aboutus-paragraph.ts
+++ frontend/storybook/src/components/entry/homepage/aboutus-paragraph.ts
@@ -35,19 +35,13 @@ const STR_TITLETEACHERS = "Teachers";
 const STR_SUBTITLETEACHERS = "Sarah Nazirah, Mexico";
 
 const STR_PINK = "pink";
-const STR_SIZELARGE = "large";
+
 const STR_LINE1="I want to tell you, because of JI, my children are";
 const STR_LINE2="learning Hebrew and English simultaneously. For my ";
 const STR_LINE3="children, you are not only teaching two children, you"
 const STR_LINE4="are also saving their souls. I reaffirm that they have";
 const STR_LINE5="achieved educational rehabilitation, thanks to JI.";
-const STR_YELLOW="yellow";
-const STR_BLACK="black";
-const STR_SMALL="small";
-const STR_WHITE="white";
-const STR_MEDIUM="medium";
-const STR_SARAHN="sarah-nazirah.png";
-const STR_SARAHH="Sara-Halberstadt.png";
+
 
 
 export const AboutUsParagraph = (props?:Partial<Args>) => {
@@ -58,8 +52,8 @@ export const AboutUsParagraph = (props?:Partial<Args>) => {
     <aboutus-section>
    
     <title-section  title="${STR_STARTTITLE}"   slot="title"></title-section>
-    <title-section titlecolor="${STR_YELLOW}" title="${STR_ENDTTITLE}" size="${STR_SIZELARGE}" slot="title"></title-section>
-<title-sub-paragraph slot="title-sub-paragraph-right"     colorsubtitle="${STR_BLACK}" sizesubtitle="${STR_SMALL}"  title="${STR_TITLEPARENTS}" subtitle="${STR_SUBTITLEPARENTS}">
+    <title-section titlecolor="yellow" title="${STR_ENDTTITLE}" size="large" slot="title"></title-section>
+<title-sub-paragraph slot="title-sub-paragraph-right"     colorsubtitle="black" sizesubtitle="small"  title="${STR_TITLEPARENTS}" subtitle="${STR_SUBTITLEPARENTS}">
  
 
 <column-list slot="line" text_line="${STR_LINE1}" size="medium"></column-list>
@@ -70,15 +64,15 @@ export const AboutUsParagraph = (props?:Partial<Args>) => {
 
 </title-sub-paragraph>
    
-<circle-div slot="rpoint" color="${STR_WHITE}" size="${STR_MEDIUM}"></circle-div>
-<circle-div slot="rpoint" color="${STR_PINK}" size="${STR_MEDIUM}"></circle-div>
-<circle-div slot="rpoint" color="${STR_PINK}" size="${STR_MEDIUM}"></circle-div>
-<circle-div slot="rpoint" color="${STR_PINK}" size="${STR_MEDIUM}"></circle-div>
-<circle-div slot="rpoint" color="${STR_PINK}" size="${STR_MEDIUM}"></circle-div>
-<img-ui slot="imgparent" path="${STR_SARAHN}"> </img-ui>
+<circle-div slot="rpoint" color="white" size="medium"></circle-div>
+<circle-div slot="rpoint" color="pink" size="medium"></circle-div>
+<circle-div slot="rpoint" color="pink" size="medium"></circle-div>
+<circle-div slot="rpoint" color="pink" size="medium"></circle-div>
+<circle-div slot="rpoint" color="pink" size="medium"></circle-div>
+<img-ui slot="imgparent" path="sarah-nazirah.png"> </img-ui>
 
 
-<title-sub-paragraph slot="title-sub-paragraph-left"   ${argsToAttrs(deleteNone(titleProps))}  colorsubtitle="${STR_BLACK}"   sizesubtitle="${STR_SMALL}" title="${STR_TITLETEACHERS}" subtitle="${STR_SUBTITLETEACHERS}">
+<title-sub-paragraph slot="title-sub-paragraph-left"   ${argsToAttrs(deleteNone(titleProps))}  colorsubtitle="black"   sizesubtitle="small" title="${STR_TITLETEACHERS}" subtitle="${STR_SUBTITLETEACHERS}">
 
 
 
@@ -90,23 +84,15 @@ export const AboutUsParagraph = (props?:Partial<Args>) => {
 
 </title-sub-paragraph>
 
-<circle-div slot="lpoint" color="${STR_WHITE}" size="${STR_MEDIUM}"></circle-div>
-<circle-div slot="lpoint" color="${STR_PINK}" size="${STR_MEDIUM}"></circle-div>
-<circle-div slot="lpoint" color="${STR_PINK}" size="${STR_MEDIUM}"></circle-div>
-<circle-div slot="lpoint" color="${STR_PINK}" size="${STR_MEDIUM}"></circle-div>
-<circle-div slot="lpoint" color="${STR_PINK}" size="${STR_MEDIUM}"></circle-div>
-<img-ui slot="imgteacher" path="${STR_SARAHH}"> </img-ui>
+<circle-div slot="lpoint" color="white" size="medium"></circle-div>
+<circle-div slot="lpoint" color="pink" size="medium"></circle-div>
+<circle-div slot="lpoint" color="pink" size="medium"></circle-div>
+<circle-div slot="lpoint" color="pink" size="medium"></circle-div>
+<circle-div slot="lpoint" color="pink" size="medium"></circle-div>
+<img-ui slot="imgteacher" path="Sara-Halberstadt.png"> </img-ui>
 
 
     </aboutus-section>
     `
 }
 AboutUsParagraph.args = DEFAULT_ARGS;
-AboutUsParagraph.argTypes = {
-  color: {
-      control: {
-          type: 'inline-radio',
-          options: ["white", "black", "yellow"]
-      }
-  }
-}
