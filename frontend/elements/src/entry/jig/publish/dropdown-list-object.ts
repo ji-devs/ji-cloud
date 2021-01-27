a0598aa33b7d4d8557b32710b3267c43484492df
Naomi Avraham <naomikudren@gmail.com>
2021-01-27 16:55:58 +0200

    Fix mode

--- frontend/elements/src/entry/jig/publish/dropdown-list-object.ts
+++ frontend/elements/src/entry/jig/publish/dropdown-list-object.ts
@@ -1,6 +1,8 @@
 import { MEDIA_UI } from '@utils/path';
 import { LitElement, html, css, customElement, property } from 'lit-element';
+import { nothing } from "lit-html";
 
+export type Mode = "share" | "embed" | "link";
 @customElement('dropdown-list-object')
 export class _ extends LitElement {
 
@@ -33,22 +35,34 @@ export class _ extends LitElement {
    
     `];
   }
-@property()
-label: string = "";
+
 @property()
 icon: string = "";
+@property()
+mode: Mode = "share"
 
   
 
   render() {
-    const {icon, label} = this;
-   
+    const {icon,mode} = this;
+    const STR_SHARE ="Share with Students";
+    const STR_URL ="Copy URL";
+    const STR_EMBED ="Embed this JIG";
+    const iconPath = mode === "share" ? "icn-student.svg"
+      : mode === "embed" ? "Icn_Embed.svg"
+      : mode === "link" ? "icn-url.svg"
+      : nothing;
+      
+     const label = mode === "share" ? STR_SHARE
+      : mode === "embed" ? STR_EMBED
+      : mode === "link" ? STR_URL
+      : nothing;
    
 
     return html`
 <div class="wrapper">
     <div class="inside-wrapper">
-        <img-ui path="${icon}"></img-ui>
+        <img-ui path="${iconPath}"></img-ui>
         <p>${label}</p>
     </div>
     <img-ui path="icn-record-activity-hover.svg"></img-ui>
