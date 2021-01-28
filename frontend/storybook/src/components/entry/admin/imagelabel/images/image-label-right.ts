777dfea281f796933785ff9c77460abf9a288c73
Naomi Avraham <naomikudren@gmail.com>
2021-01-28 14:08:56 +0200

    Directory fix

---
+++ frontend/storybook/src/components/entry/admin/imagelabel/images/image-label-right.ts
@@ -0,0 +1,32 @@
+import {ImageStyle} from "~/components/entry/admin/imagelabel/images/lists/image-style";
+import {Age} from "~/components/entry/admin/imagelabel/images/lists/age";
+import {Stream} from "~/components/entry/admin/imagelabel/images/lists/stream";
+import "@elements/core/inputs/text-underline";
+import "@elements/core/lists/list-two-column";
+import "@elements/core/images/ji";
+
+export default {
+  title: 'Admin/Images/Settings',
+}
+
+const STR_USED = "To be used only for";
+
+export const LabelRight = () => {
+    
+    return `
+    <list-two-column>
+     <div slot="one">${ImageStyle()}</div>
+     <div slot="two">
+      <input-text-underline label="${STR_USED}" icon=true></input-text-underline>
+      ${
+        Age() +
+        Stream()
+      
+      }
+     </div>
+
+    </list-two-column>
+
+    
+    `
+}
