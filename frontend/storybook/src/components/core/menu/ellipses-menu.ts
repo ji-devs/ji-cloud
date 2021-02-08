4ad2a836dd967cbb6e1bad4591b3a6fec0f83628
Naomi <naomikudren@gmail.com>
2021-02-08 17:20:28 +0200

    admin fixes (#795)

--- frontend/storybook/src/components/core/menu/ellipses-menu.ts
+++ frontend/storybook/src/components/core/menu/ellipses-menu.ts
@@ -18,7 +18,8 @@ export const EllipsesMenu = (props?:Args) => {
     props = props ? {...DEFAULT_ARGS, ...props} : DEFAULT_ARGS;
     const {menuVisible} = props
 
-    return `<ellipses-menu ${argsToAttrs(props)} ${menuVisible && "menuVisible"}></ellipses-menu>`;
+    return `<ellipses-menu ${argsToAttrs(props)} ${menuVisible && "menuVisible"}>
+    <div>Hello</div></ellipses-menu>`;
 }
 
 EllipsesMenu.args = DEFAULT_ARGS;
