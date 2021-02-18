881ad9197b7032ac9bf525fdfd448cf20f80aed2
David Komer <david.komer@gmail.com>
2021-02-18 10:57:29 +0200

    upgrade deps

---
+++ frontend/elements/src/_bundles/jig/edit/imports.ts
@@ -0,0 +1,14 @@
+import "@elements/entry/jig/edit/sidebar/sidebar";
+import "@elements/entry/jig/edit/sidebar/header";
+import "@elements/entry/jig/edit/pages/landing";
+import "@elements/entry/jig/edit/selection/card";
+import "@elements/entry/jig/edit/selection/selection";
+import "@elements/entry/jig/edit/sidebar/module/menu";
+import "@elements/entry/jig/edit/sidebar/module/window";
+import "@elements/entry/jig/edit/sidebar/header";
+import "@elements/entry/jig/edit/sidebar/module";
+import "@elements/entry/jig/edit/sidebar/sidebar";
+import "@elements/entry/jig/_common/bg";
+import "@elements/core/buttons/icon";
+import "@elements/core/menu/kebab";
+import "@elements/core/menu/menu-line";
