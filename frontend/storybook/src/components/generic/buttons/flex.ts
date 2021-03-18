c18455cbd90c8e525370e736846a612682d9c7ce
David Komer <david.komer@gmail.com>
2021-03-18 11:24:13 +0200

    memory button

---
+++ frontend/storybook/src/components/generic/buttons/flex.ts
@@ -0,0 +1,26 @@
+import {argsToAttrs} from "@utils/attributes";
+import "@elements/generic/buttons/flex";
+
+export default {
+    title: "Generic / Buttons"
+}
+
+
+interface Args {
+}
+
+const DEFAULT_ARGS:Args = {
+}
+
+export const Flex = (props?:Partial<Args>) => {
+    props = props ? {...DEFAULT_ARGS, ...props} : DEFAULT_ARGS;
+
+    //The content is just local / proof-of-concept
+    return `
+    <button-flex ${argsToAttrs(props)}>
+        <div style="background-color: red; width: 50px; height: 50px; display: flex; justify-content: center; align-items: center;">One</div>
+        <div style="background-color: blue; color: white;">Two</div>
+    </button-flex>`;
+}
+
+Flex.args = DEFAULT_ARGS;
