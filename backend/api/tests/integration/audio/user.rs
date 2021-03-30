62c6e1a18637a271603e33948591bf55d3affb60
Chloe Ross <orangesnowfox@gmail.com>
2021-03-30 10:57:24 -0700

    fix(backend/api): make user audio library properly set `created_at`

---
+++ backend/api/tests/integration/audio/user.rs
@@ -0,0 +1,31 @@
+use http::StatusCode;
+use shared::domain::{audio::AudioId, CreateResponse};
+
+use crate::{
+    fixture::Fixture,
+    helpers::{initialize_server, LoginExt},
+};
+
+#[actix_rt::test]
+async fn create_returns_created() -> anyhow::Result<()> {
+    let app = initialize_server(&[Fixture::User]).await;
+
+    let port = app.port();
+
+    let client = reqwest::Client::new();
+
+    let resp = client
+        .post(&format!("http://0.0.0.0:{}/v1/user/me/audio", port))
+        .login()
+        .send()
+        .await?
+        .error_for_status()?;
+
+    assert_eq!(resp.status(), StatusCode::CREATED);
+
+    let body: CreateResponse<AudioId> = resp.json().await?;
+
+    insta::assert_json_snapshot!(body, {".id" => "[id]"});
+
+    Ok(())
+}
