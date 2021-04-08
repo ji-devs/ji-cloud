d7997e457587b2af264e6f64b79d4ecbbe9150e6
David Komer <david.komer@gmail.com>
2021-04-08 17:37:32 +0300

    add node resolve

--- frontend/apps/rollup.release.js
+++ frontend/apps/rollup.release.js
@@ -23,5 +23,6 @@ export default {
 			cargoArgs: ["--features", "release"],
             debug: false,
         }),
+		nodeResolve(),
     ],
 };
--- frontend/apps/rollup.sandbox.js
+++ frontend/apps/rollup.sandbox.js
@@ -24,5 +24,6 @@ export default {
 			cargoArgs: ["--features", "sandbox"],
             debug: false,
         }),
+		nodeResolve(),
     ],
 };
