befd5a675272fd05be70ce021798f3f153e9b952
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2021-04-09 10:59:03 -0400

    Vertical center the "Edit words on cards"

--- frontend/elements/src/module/_common/sidebar-body.ts
+++ frontend/elements/src/module/_common/sidebar-body.ts
@@ -4,17 +4,18 @@ import {nothing} from "lit-html";
 
 @customElement('module-sidebar-body')
 export class _ extends LitElement {
-  static get styles() {
-      return [css`
-          :host {
-              display: block;
-              width: 492px;
-              margin: 0 auto;
-          }
-    `];
-  }
+    static get styles() {
+        return [css`
+            :host {
+                display: block;
+                width: 492px;
+                margin: 0 auto;
+                min-height: 100%;
+            }
+        `];
+    }
 
-  render() {
-      return html`<slot></slot>`
-  }
+    render() {
+        return html`<slot></slot>`
+    }
 }
--- frontend/elements/src/module/_common/sidebar.ts
+++ frontend/elements/src/module/_common/sidebar.ts
@@ -1,50 +1,40 @@
 import { LitElement, html, css, customElement, property } from 'lit-element';
-import {classMap} from "lit-html/directives/class-map";
-import {nothing} from "lit-html";
 
 @customElement('module-sidebar')
 export class _ extends LitElement {
-  static get styles() {
-      return [css`
-          :host {
-              display: grid;
-              grid-template-rows: 160px 1fr;
-              height: 100%;
-              width: 556px;
-              box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
-              background-color: #f6fafe;
-          }
-          .nav {
-              padding-top: 40px;
-              padding-left: 50px;
-              padding-right: 50px;
-          }
-          article {
-              display: flex;
-              flex-direction: column;
-              align-items: center;
-              width: 100%;
-              height: 100%;
-              overflow-y: auto;
-          }
+    static get styles() {
+        return [css`
+            :host {
+                display: grid;
+                grid-template-rows: 160px 1fr;
+                height: 100%;
+                width: 556px;
+                box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+                background-color: #f6fafe;
+            }
+            .nav {
+                padding-top: 40px;
+                padding-left: 50px;
+                padding-right: 50px;
+            }
+            article {
+                display: flex;
+                flex-direction: column;
+                width: 100%;
+                height: 100%;
+                overflow-y: auto;
+            }
+        `];
+    }
 
-          .content {
-              width: 100%;
-          }
-    `];
-  }
-
-  render() {
-      return html`
-          <div class="nav">
-              <slot name="nav"></slot>
-          </div>
-          <article>
-              <div class="content">
-                  <slot name="content"></slot>
-              </div>
-          </article>
-
-      `
-  }
+    render() {
+        return html`
+            <div class="nav">
+                <slot name="nav"></slot>
+            </div>
+            <article class="content">
+                <slot name="content"></slot>
+            </article>
+        `;
+    }
 }
