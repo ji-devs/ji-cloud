642d0ecd54f25cdc431b893fc633cddcf0152a12
David Komer <david.komer@gmail.com>
2021-05-19 16:52:49 +0300

    more module skeleton

--- frontend/apps/crates/entry/module/_skeleton/edit/src/steps/sidebar/state.rs
+++ frontend/apps/crates/entry/module/_skeleton/edit/src/steps/sidebar/state.rs
@@ -1,19 +1,56 @@
 use components::module::edit::*;
-use super::super::state::Base;
+use super::super::state::{Step, Base};
 use std::rc::Rc;
+use dominator_helpers::futures::AsyncLoader;
+use futures_signals::signal::{Mutable, SignalExt};
+use dominator::clone;
 
 pub struct Sidebar {
-    pub base: Rc<Base>
+    pub base: Rc<Base>,
+    pub step_reactor: AsyncLoader,
+    pub step_state: Mutable<Option<Rc<StepState>>>
 }
 
 
 impl Sidebar {
     pub fn new(base: Rc<Base>) -> Self {
+        let step_reactor = AsyncLoader::new();
+
+        let step_state = Mutable::new(None);
+
+        step_reactor.load(base.step.signal().for_each(clone!(step_state => move |step| {
+            match step {
+                Step::One => {
+                    step_state.set(Some(Rc::new(StepState::One)));
+                },
+                Step::Two => {
+                    step_state.set(Some(Rc::new(StepState::Two)));
+                },
+                Step::Three => {
+                    step_state.set(Some(Rc::new(StepState::Three)));
+                },
+                _ => {
+                    step_state.set(None);
+                }
+            }
+            async {}
+        })));
+
         Self {
-            base 
+            base,
+            step_reactor,
+            step_state,
         }
     }
 }
 
 impl SidebarExt for Sidebar {
+}
+
+
+//these will be much heavier...
+pub enum StepState {
+    One,
+    Two,
+    Three
 }
--- frontend/apps/crates/entry/module/poster/edit/src/steps/sidebar/dom.rs
+++ frontend/apps/crates/entry/module/poster/edit/src/steps/sidebar/dom.rs
@@ -7,8 +7,16 @@ use futures_signals::signal::SignalExt;
 impl DomRenderable for Sidebar {
     fn render(state: Rc<Sidebar>) -> Dom {
         html!("div", {
-            .text_signal(state.base.step.signal().map(|step| {
-                step.label()
+            .text_signal(state.step_state.signal_cloned().map(|step_state| {
+                if let Some(step_state) = step_state.as_ref() {
+                    match step_state.as_ref() {
+                        StepState::One => "step 1 here!",
+                        StepState::Two => "step 2 here!",
+                        StepState::Three => "step 3 here!",
+                    }
+                } else {
+                    "nothing!"
+                }
             }))
         })
     }
