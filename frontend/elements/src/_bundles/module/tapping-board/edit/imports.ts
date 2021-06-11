60d94ad6465070c44eb8c51022fdde9ab568fa14
David Komer <david.komer@gmail.com>
2021-06-11 18:43:26 +0300

    tapping board ci

--- frontend/elements/src/_bundles/module/tapping-board/edit/imports.ts
+++ frontend/elements/src/_bundles/module/tapping-board/edit/imports.ts
@@ -1,3 +1,5 @@
 import "@elements/_sub-bundles/module-edit";
 import "@elements/module/poster/edit/steps/sidebar/container";
 import "@elements/module/poster/edit/steps/sidebar/option";
+
+// Trigger CI
