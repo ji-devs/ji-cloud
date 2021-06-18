7d76c495de13e681cc3209a2eafeea45a48ae29d
David Komer <david.komer@gmail.com>
2021-06-18 14:33:07 +0300

    delete unused strings

--- frontend/apps/crates/entry/module/memory/play/src/strings.rs
+++ frontend/apps/crates/entry/module/memory/play/src/strings.rs
@@ -11,16 +11,3 @@ pub mod steps_nav {
     pub const STR_SETTINGS:&'static str = "Settings";
     pub const STR_PREVIEW:&'static str = "Preview";
 }
-
-
-pub mod mode {
-    pub const STR_TITLE:&'static str = "Create a Memory Game";
-    pub const STR_DUPLICATE:&'static str = "Duplicate";
-    pub const STR_WORDS_IMAGES:&'static str = "Words & Images";
-    pub const STR_BEGINS_WITH:&'static str  = "What begins with...";
-    pub const STR_LETTERING:&'static str  = "Lettering";
-    pub const STR_RIDDLES:&'static str  = "Riddles";
-    pub const STR_OPPOSITES:&'static str  = "Opposites";
-    pub const STR_SYNONYMNS:&'static str  = "Synonymns";
-    pub const STR_TRANSLATE:&'static str  = "Translate";
-}
