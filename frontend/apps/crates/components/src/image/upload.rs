ea1972bab5498728cbd73fca0fb2f9ae2a55e069
David Komer <david.komer@gmail.com>
2021-07-27 17:54:27 +0300

    fix user upload

--- frontend/apps/crates/components/src/image/upload.rs
+++ frontend/apps/crates/components/src/image/upload.rs
@@ -1,7 +1,7 @@
 use awsm_web::loaders::helpers::AbortController;
 use shared::{
     media::MediaLibrary,
-    domain::image::*,
+    domain::image::{*, user::*},
     error::*,
     api::{ApiEndpoint, endpoints},
 };
@@ -42,25 +42,57 @@ impl UploadError {
  */
 
 pub async fn upload_image(id: ImageId, lib: MediaLibrary, file: &File, abort_controller: Option<&AbortController>) -> Result<(), UploadError> {
-    let req = ImageUploadRequest {
-        file_size: file.size() as usize
+    let session_uri = {
+        match lib {
+            MediaLibrary::Global => {
+                let req = ImageUploadRequest {
+                    file_size: file.size() as usize
+                };
+
+                let path = endpoints::image::Upload::PATH.replace("{id}",&id.0.to_string());
+
+                let resp = api_with_auth_abortable::<ImageUploadResponse, EmptyError, _>(&path, endpoints::image::Upload::METHOD, abort_controller, Some(req)).await
+                    .map_err(|aborted| {
+                        if aborted {
+                            UploadError::Aborted
+                        } else {
+                            UploadError::Other(awsm_web::errors::Error::Empty)
+                        }
+                    })
+                    .and_then(|resp| {
+                        resp.map_err(|_| UploadError::Other(awsm_web::errors::Error::Empty))
+                    })?;
+
+                let ImageUploadResponse {session_uri} = resp;
+                session_uri
+            },
+
+            MediaLibrary::User => {
+                let req = UserImageUploadRequest {
+                    file_size: file.size() as usize
+                };
+
+                let path = endpoints::image::user::Upload::PATH.replace("{id}",&id.0.to_string());
+
+                let resp = api_with_auth_abortable::<UserImageUploadResponse, EmptyError, _>(&path, endpoints::image::user::Upload::METHOD, abort_controller, Some(req)).await
+                    .map_err(|aborted| {
+                        if aborted {
+                            UploadError::Aborted
+                        } else {
+                            UploadError::Other(awsm_web::errors::Error::Empty)
+                        }
+                    })
+                    .and_then(|resp| {
+                        resp.map_err(|_| UploadError::Other(awsm_web::errors::Error::Empty))
+                    })?;
+
+                let UserImageUploadResponse {session_uri} = resp;
+                session_uri
+            },
+
+            _ => panic!("Cannot upload images other than to global or user library!")
+        }
     };
-
-    let path = endpoints::image::Upload::PATH.replace("{id}",&id.0.to_string());
-
-    let resp = api_with_auth_abortable::<ImageUploadResponse, EmptyError, _>(&path, endpoints::image::Upload::METHOD, abort_controller, Some(req)).await
-        .map_err(|aborted| {
-            if aborted {
-                UploadError::Aborted
-            } else {
-                UploadError::Other(awsm_web::errors::Error::Empty)
-            }
-        })
-        .and_then(|resp| {
-            resp.map_err(|_| UploadError::Other(awsm_web::errors::Error::Empty))
-        })?;
-
-    let ImageUploadResponse {session_uri} = resp;
 
     //upload to GCS
     upload_file_gcs(&session_uri, &file, abort_controller)
