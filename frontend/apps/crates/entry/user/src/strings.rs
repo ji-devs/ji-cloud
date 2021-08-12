4316b0e6f555a65863bfa1dd15a8eb85323a6712
David Komer <david.komer@gmail.com>
2021-08-12 18:44:36 +0300

    text update
    fixes #1029

--- frontend/apps/crates/entry/user/src/strings.rs
+++ frontend/apps/crates/entry/user/src/strings.rs
@@ -1,9 +1,9 @@
 pub const STR_AUTH_OAUTH_LOGIN_FAIL:&'static str = "Create an account first :)";
 pub const STR_CHANGE_EMAIL:&'static str = "Change email account";
 pub const STR_SUBMIT:&'static str = "Submit";
-pub const STR_EMAIL_LABEL:&'static str = "Email";
+pub const STR_EMAIL_LABEL:&'static str = "Email*";
 pub const STR_EMAIL_PLACEHOLDER:&'static str = "Type or paste your email";
-pub const STR_PASSWORD_CREATE_LABEL:&'static str = "Create Password";
+pub const STR_PASSWORD_CREATE_LABEL:&'static str = "Create Password*";
 pub const STR_PASSWORD_PLACEHOLDER:&'static str ="********";
 
 pub const STR_NOT_AUTHORIZED:&'static str = "Not authorized!";
@@ -11,7 +11,7 @@ pub const STR_USER_EXISTS:&'static str = "User already exists!";
 pub const STR_EMPTY_USERNAME:&'static str = "Empty username!";
 
 
-pub const STR_PASSWORD_LABEL:&'static str = "Password";
+pub const STR_PASSWORD_LABEL:&'static str = "Password*";
 pub const STR_PASSWORD_FORGOTTEN:&'static str ="Forgot your Password?";
 
 pub mod register {
