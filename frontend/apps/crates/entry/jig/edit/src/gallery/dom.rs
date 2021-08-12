e97016fbb08e0dbe67292a84977b2ae93481ecb5
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2021-08-12 07:03:29 -0400

    feat(frontend/jig/edit): header and footer in gallery

--- frontend/apps/crates/entry/jig/edit/src/gallery/dom.rs
+++ frontend/apps/crates/entry/jig/edit/src/gallery/dom.rs
@@ -1,4 +1,5 @@
 use super::{actions, state::*};
+use components::{page_header, page_footer};
 use dominator::{clone, html, Dom};
 use futures_signals::signal::SignalExt;
 use futures_signals::signal_vec::SignalVecExt;
@@ -30,91 +31,97 @@ impl GalleryDom {
 
         actions::load_data(state.clone());
 
-        html!("jig-gallery", {
-            .child(html!("jig-gallery-create", {
-                .property("slot", "create-jig")
-                .event(clone!(state => move |_: events::Click| {
-                    actions::create_jig(state.clone());
-                }))
-            }))
-            .children(TEMPLATE_KINDS.iter().map(|kind| {
-                html!("jig-gallery-template", {
-                    .property("slot", "jig-templates")
-                    .property("kind", *kind)
-                })
-            }))
-            .child(html!("input-search", {
-                .property("slot", "search-input")
-                .property("placeholder", STR_SEARCH)
-                .event(clone!(state => move |evt: events::CustomSearch| {
-                    let value = evt.query();
-                    if !value.is_empty() {
-                        actions::search_jigs(state.clone(), value);
-                    } else {
-                        actions::load_jigs_regular(state.clone());
-                    }
-                }))
-            }))
-            .child(html!("input-select", {
-                .property("slot", "filters")
-                .property_signal("value", state.visible_jigs.signal_cloned().map(|visible_jigs| Self::visible_jigs_option_string(&visible_jigs)))
-                .children(VisibleJigs::iter().map(|option| {
-                    html!("input-select-option", {
-                        .property("value", &option.to_string())
-                        .text(Self::visible_jigs_option_string(&option))
-                        .property_signal("selected", state.visible_jigs.signal_cloned().map(clone!(state, option => move |visible_jigs| {
-                            if visible_jigs == option {
-                                true
+        html!("empty-fragment", {
+            .child(page_header::dom::render(Rc::new(page_header::state::State::new()), None))
+            .child(
+                html!("jig-gallery", {
+                    .child(html!("jig-gallery-create", {
+                        .property("slot", "create-jig")
+                        .event(clone!(state => move |_: events::Click| {
+                            actions::create_jig(state.clone());
+                        }))
+                    }))
+                    .children(TEMPLATE_KINDS.iter().map(|kind| {
+                        html!("jig-gallery-template", {
+                            .property("slot", "jig-templates")
+                            .property("kind", *kind)
+                        })
+                    }))
+                    .child(html!("input-search", {
+                        .property("slot", "search-input")
+                        .property("placeholder", STR_SEARCH)
+                        .event(clone!(state => move |evt: events::CustomSearch| {
+                            let value = evt.query();
+                            if !value.is_empty() {
+                                actions::search_jigs(state.clone(), value);
                             } else {
-                                false
-                            }
-                        })))
-                        .event(clone!(state, option => move |evt: events::CustomSelectedChange| {
-                            if evt.selected() {
-                                state.visible_jigs.set(option.clone());
                                 actions::load_jigs_regular(state.clone());
                             }
                         }))
-                    })
-                }))
-            }))
-            // todo: deal with loading
-            // .child(html!("window-loader-block", {
-            //     .property("slot", "recent-items")
-            //     .property_signal("visible", state.loader.is_loading())
-            // }))
-            .children_signal_vec(state.jigs.signal_vec_cloned().map(clone!(state => move |jig| {
-                let jig_ages = jig.age_ranges.clone();
-                html!("jig-gallery-recent", {
-                    .property("slot", "recent-items")
-                    .property("href", jig.id.0.to_string())
-                    .property("draft", "")
-                    .property("label", jig.display_name.clone())
-                    .property("img", "mock/resized/jig-gallery.jpg")
-                    .property_signal("ages", state.age_ranges.signal_cloned().map(move|age_ranges| {
-                        age_ranges.range_string(&jig_ages)
                     }))
-                    .property("lastEdited", "???")
-                    .children(&mut [
-                        html!("menu-line", {
-                            .property("slot", "menu-content")
-                            .property("icon", "duplicate")
-                            .text(STR_DUPLICATE)
-                            .event(clone!(state, jig => move |_: events::Click| {
-                                actions::copy_jig(state.clone(), &jig.id);
+                    .child(html!("input-select", {
+                        .property("slot", "filters")
+                        .property_signal("value", state.visible_jigs.signal_cloned().map(|visible_jigs| Self::visible_jigs_option_string(&visible_jigs)))
+                        .children(VisibleJigs::iter().map(|option| {
+                            html!("input-select-option", {
+                                .property("value", &option.to_string())
+                                .text(Self::visible_jigs_option_string(&option))
+                                .property_signal("selected", state.visible_jigs.signal_cloned().map(clone!(state, option => move |visible_jigs| {
+                                    if visible_jigs == option {
+                                        true
+                                    } else {
+                                        false
+                                    }
+                                })))
+                                .event(clone!(state, option => move |evt: events::CustomSelectedChange| {
+                                    if evt.selected() {
+                                        state.visible_jigs.set(option.clone());
+                                        actions::load_jigs_regular(state.clone());
+                                    }
+                                }))
+                            })
+                        }))
+                    }))
+                    // todo: deal with loading
+                    // .child(html!("window-loader-block", {
+                    //     .property("slot", "recent-items")
+                    //     .property_signal("visible", state.loader.is_loading())
+                    // }))
+                    .children_signal_vec(state.jigs.signal_vec_cloned().map(clone!(state => move |jig| {
+                        let jig_ages = jig.age_ranges.clone();
+                        html!("jig-gallery-recent", {
+                            .property("slot", "recent-items")
+                            .property("href", jig.id.0.to_string())
+                            .property("draft", "")
+                            .property("label", jig.display_name.clone())
+                            .property("img", "mock/resized/jig-gallery.jpg")
+                            .property_signal("ages", state.age_ranges.signal_cloned().map(move|age_ranges| {
+                                age_ranges.range_string(&jig_ages)
                             }))
-                        }),
-                        html!("menu-line", {
-                            .property("slot", "menu-content")
-                            .property("icon", "delete")
-                            .text(STR_DELETE)
-                            .event(clone!(state, jig => move |_: events::Click| {
-                                actions::delete_jig(state.clone(), jig.id);
-                            }))
-                        }),
-                    ])
+                            .property("lastEdited", "???")
+                            .children(&mut [
+                                html!("menu-line", {
+                                    .property("slot", "menu-content")
+                                    .property("icon", "duplicate")
+                                    .text(STR_DUPLICATE)
+                                    .event(clone!(state, jig => move |_: events::Click| {
+                                        actions::copy_jig(state.clone(), &jig.id);
+                                    }))
+                                }),
+                                html!("menu-line", {
+                                    .property("slot", "menu-content")
+                                    .property("icon", "delete")
+                                    .text(STR_DELETE)
+                                    .event(clone!(state, jig => move |_: events::Click| {
+                                        actions::delete_jig(state.clone(), jig.id);
+                                    }))
+                                }),
+                            ])
+                        })
+                    })))
                 })
-            })))
+            )
+            .child(page_footer::dom::render(None))
         })
     }
 }
