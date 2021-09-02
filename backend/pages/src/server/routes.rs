2129e4bcdc35f0fbbd1551cd2e27c3db66f05179
rrcwang <15862033+rrcwang@users.noreply.github.com>
2021-09-02 11:09:31 -0700

    feat(backend/pages): serve home template on `/home` and `/home/*`

--- backend/pages/src/server/routes.rs
+++ backend/pages/src/server/routes.rs
@@ -30,6 +30,8 @@ pub fn configure(config: &mut ServiceConfig) {
         )
         .route("/dev/{path:.*}", web::get().to(spa::dev_template))
         .route("/", web::get().to(spa::home_template))
+        .route("/home", web::get().to(spa::home_template))
+        .route("/home/{path:.*}", web::get().to(spa::home_template))
         .route("/no-auth", web::get().to(direct_template_no_auth))
         .route("/info", web::get().to(info_template))
         .route("/epoch", web::get().to(epoch_page));
