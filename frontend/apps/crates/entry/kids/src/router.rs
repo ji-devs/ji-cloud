3c81a57a69360485d89f5f3c160fb989384a30b8
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2021-09-10 14:05:49 -0400

    fix(fe/kids): Removed auth from kids router

--- frontend/apps/crates/entry/kids/src/router.rs
+++ frontend/apps/crates/entry/kids/src/router.rs
@@ -1,88 +1,34 @@
-use utils::{prelude::*, routes::{Route, KidsRoute}};
-use shared::{
-    api::{endpoints::user::Profile, ApiEndpoint},
-    domain::user::UserProfile,
-    error::EmptyError,
-};
+use dominator::{Dom, html};
+use futures_signals::signal::Signal;
 use std::rc::Rc;
-use futures_signals::{
-    map_ref,
-    signal::{Mutable, SignalExt}
-};
-use dominator::{Dom, html, clone};
+use utils::routes::{KidsRoute, Route};
 use super::student_code;
 
+
 pub struct Router {
-    profile: Mutable<Option<Option<UserProfile>>>,
 }
 
 impl Router {
-    pub fn new() -> Rc<Self> {
-        Rc::new(Self {
-            profile: Mutable::new(None),
+    pub fn render() -> Dom {
+        html!("main", {
+            .child_signal(Self::dom_signal())
         })
     }
-}
 
-impl Router {
-    pub fn render(state: Rc<Self>) -> Dom {
-
-        html!("div", {
-            .future(clone!(state => async move {
-                let (result, status) = api_with_auth_status::<UserProfile, EmptyError, ()>(&Profile::PATH, Profile::METHOD, None).await;
-
-                match status  {
-                    401 | 403 => {
-                        state.profile.set(Some(None));
-                    }
-                    _ => {
-                        match result {
-                            Err(_) => {
-                                log::info!("error fetching profile");
+    fn dom_signal() -> impl Signal<Item = Option<Dom>> {
+        dominator::routing::url()
+            .signal_ref(|url| {
+                let route = Route::from_url(&url);
+                match route {
+                    Route::Kids(route) => {
+                        match route {
+                            KidsRoute::StudentCode => {
+                                Some(student_code::dom::render(Rc::new(student_code::state::State::new())))
                             },
-                            Ok(profile) => {
-                                state.profile.set(Some(Some(profile)));
-                            }
                         }
                     }
-                };
-            }))
-            .children_signal_vec(
-                map_ref!{
-                    let route = dominator::routing::url().signal_ref(|url| Route::from_url(&url)),
-                    let profile = state.profile.signal_cloned()
-                        => move {
-                            let mut children:Vec<Dom> = Vec::new();
-
-                            children.push(components::page_header::dom::render(
-                                Rc::new(components::page_header::state::State::new()),
-                                None,
-                                None,
-                            ));
-
-                            let dom = match route.clone() {
-                                Route::Kids(route) => {
-                                    match route.clone() {
-                                        KidsRoute::StudentCode => {
-                                            Some(student_code::dom::render(Rc::new(student_code::state::State::new())))
-                                        },
-                                    }
-                                }
-                                _ => None
-
-                            };
-
-                            if let Some(dom) = dom {
-                                children.push(dom);
-                            }
-
-
-                            children
-                        }
+                    _ => None
                 }
-                .to_signal_vec()
-            )
-        })
+            })
     }
 }
-
