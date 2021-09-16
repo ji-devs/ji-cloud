df7caf63c36d2b4b732b8cb3480ff147f90555c3
rrcwang <15862033+rrcwang@users.noreply.github.com>
2021-09-15 17:39:42 -0700

    fix(backend/api): fix broken migration

--- backend/api/migrations/20210504224859_jig_language_categories.sql
+++ backend/api/migrations/20210504224859_jig_language_categories.sql
@@ -1,7 +1,7 @@
 create table jig_category (
-    jig_id      uuid        not null references jig(id) on delete cascade,
-    category_id uuid        not null references category(id) on delete cascade,
-    created_at  timestamptz not null default now()
+                              jig_id      uuid        not null references jig(id) on delete cascade,
+                              category_id uuid        not null references category(id) on delete cascade,
+                              created_at  timestamptz not null default now()
 );
 
 alter table jig add column language text not null default 'en';
