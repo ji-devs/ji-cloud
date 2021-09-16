7bf5f267d379305cf00d1d3978dfb9733c0430a0
rrcwang <15862033+rrcwang@users.noreply.github.com>
2021-09-16 12:55:33 -0700

    fix(backend/api): update non-nullable columns

--- backend/api/migrations/20210916164531_jig_module_fix_timestamps.sql
+++ backend/api/migrations/20210916164531_jig_module_fix_timestamps.sql
@@ -1,2 +1,9 @@
-alter table jig_module alter column updated_at set default now(),
+alter table jig_module
+    alter column updated_at set default now();
+
+update jig_module
+set updated_at = now()
+where updated_at is not distinct from null;
+
+alter table jig_module
     alter column updated_at set not null;
