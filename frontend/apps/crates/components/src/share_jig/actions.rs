a13fadd5f72c592c3982df38689b4e12b718aaf5
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2021-09-17 11:12:39 -0400

    fix(fe/share-jig): Update generate code call to new api

--- frontend/apps/crates/components/src/share_jig/actions.rs
+++ frontend/apps/crates/components/src/share_jig/actions.rs
@@ -1,7 +1,7 @@
 use std::rc::Rc;
 
 use dominator::clone;
-use shared::{api::{ApiEndpoint, endpoints::jig}, domain::jig::{JigPlayerSettings, player::{JigPlayerSessionCode, JigPlayerSessionCreateRequest}}, error::EmptyError};
+use shared::{api::endpoints::jig, domain::jig::JigPlayerSettings};
 use utils::prelude::*;
 
 use super::state::State;
@@ -9,20 +9,16 @@ use super::state::State;
 pub(super) fn generate_student_code(state: Rc<State>) {
     state.loader.load(clone!(state => async move {
         let req = shared::domain::jig::player::JigPlayerSessionCreateRequest {
-                jig_id: state.jig_id.clone(),
-                settings: JigPlayerSettings::default(),
-            };
+            jig_id: state.jig_id.clone(),
+            settings: JigPlayerSettings::default(),
+        };
 
-        match api_with_auth::<JigPlayerSessionCode, EmptyError, JigPlayerSessionCreateRequest>(
-            jig::player::Create::PATH, jig::player::Create::METHOD, Some(req)
-        ).await {
+        match jig::player::Create::api_with_auth(Some(req)).await {
             Err(_) => todo!(),
             Ok(res) => {
-                // let code = res.index.to_string();
-                let code = format!("{:04}", res.index);
+                let code = format!("{:04}", res.index.0);
                 state.student_code.set(Some(code));
             },
         };
-
     }));
 }
