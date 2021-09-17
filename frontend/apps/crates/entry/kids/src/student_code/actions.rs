537e7cbc74928d6597a0d465d1bd37ba072d836b
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2021-09-17 13:51:43 -0400

    fix(fe/kids/student-code): Reimplemented student code

--- frontend/apps/crates/entry/kids/src/student_code/actions.rs
+++ frontend/apps/crates/entry/kids/src/student_code/actions.rs
@@ -1,30 +1,35 @@
 use std::rc::Rc;
 
 use dominator::clone;
-use shared::{api::{ApiEndpoint, endpoints::jig}, domain::jig::player::JigPlayerSession, error::EmptyError};
+use shared::{api::endpoints::jig, domain::jig::player::{JigPlayerSessionIndex, instance::{PlayerSessionInstanceCreateRequest, PlayerSessionInstanceResponse}}};
 use utils::prelude::*;
 
 use super::state::State;
 
 pub fn submit_code(state: Rc<State>, number: String) {
     state.loader.load(clone!(state => async move {
-        panic!("fix needed");
-        let path = jig::player::instance::Create::PATH.replace("{index}", &number);
-
-        let (result, status) = api_no_auth_status::<JigPlayerSession, EmptyError, ()>(&path, jig::player::instance::Create::METHOD, None).await;
-
-        match status {
-            404 => {
-                state.error.set(true);
-            },
-            _ => match result {
-                Err(_) => {}
-                Ok(res) => {
-                    state.error.set_neq(false);
-                    // state.play_jig.set(Some((res.jig_id, res.settings)));
-                }
-            },
+        match code_to_jig_id(number).await {
+            Err(_) => {}
+            Ok(res) => {
+                state.error.set_neq(false);
+                state.play_jig.set(Some((res.jig_id, res.settings)));
+            }
         };
 
     }));
+}
+
+async fn code_to_jig_id(number: String) -> Result<PlayerSessionInstanceResponse, ()> {
+    let number = number.parse::<i16>().map_err(|_| ())?;
+    let index = JigPlayerSessionIndex(number);
+    let req = PlayerSessionInstanceCreateRequest {
+        index
+    };
+
+    let (result, status) = jig::player::instance::Create::api_no_auth_status(Some(req)).await;
+
+    match status {
+        404 => Err(()),
+        _ => result.map_err(|_| ()),
+    }
 }
