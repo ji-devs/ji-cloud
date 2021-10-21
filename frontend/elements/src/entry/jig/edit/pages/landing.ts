915b746c949aa4f9ac408b0f0d2307399462a408
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2021-10-21 06:51:36 -0400

    fix(fe/jig/edit): Sidebar higher z-index

--- frontend/elements/src/entry/jig/edit/pages/landing.ts
+++ frontend/elements/src/entry/jig/edit/pages/landing.ts
@@ -1,34 +1,34 @@
-import { LitElement, html, css, customElement, property } from "lit-element";
-import { classMap } from "lit-html/directives/class-map";
-import { nothing } from "lit-html";
+import { LitElement, html, css, customElement } from "lit-element";
 import "@elements/core/images/ui";
 import "@elements/entry/jig/_common/bg";
 
 @customElement("jig-edit-page")
 export class _ extends LitElement {
-  static get styles() {
-    return [
-        css`
-        section {
-            display: flex;
-            height: 100%;
-        }
-        .main {
-            width: 100%;
-        }
-        `,
-    ];
-  }
+    static get styles() {
+        return [
+            css`
+                section {
+                    display: flex;
+                    height: 100%;
+                }
+                .sidebar {
+                    z-index: 1;
+                }
+                .main {
+                    width: 100%;
+                }
+            `,
+        ];
+    }
 
-
-  render() {
-      return html`
-        <bg-jig>
-          <section>
-              <div><slot name="sidebar"></slot></div>
-              <div class="main"><slot name="main"></slot></div>
-          </section>
-        </bg-jig>
-    `;
-  }
+    render() {
+        return html`
+            <bg-jig>
+                <section>
+                    <div class="sidebar"><slot name="sidebar"></slot></div>
+                    <div class="main"><slot name="main"></slot></div>
+                </section>
+            </bg-jig>
+        `;
+    }
 }
