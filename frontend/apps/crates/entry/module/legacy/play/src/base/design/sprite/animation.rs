011c54b0bf2c67be705d109d1872380a48fb65ed
David Komer <david.komer@gmail.com>
2021-10-22 14:20:44 +0300

    gif playback

--- frontend/apps/crates/entry/module/legacy/play/src/base/design/sprite/animation.rs
+++ frontend/apps/crates/entry/module/legacy/play/src/base/design/sprite/animation.rs
@@ -6,18 +6,21 @@ use shared::domain::jig::module::body::legacy::design::{
     Sprite as RawSprite,
     Animation
 };
+use std::borrow::Borrow;
 use std::convert::TryInto;
 use std::slice::SliceIndex;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicU8, AtomicUsize, Ordering};
 use std::{cell::RefCell, rc::Rc, sync::atomic::AtomicBool};
 use web_sys::{Blob, CanvasRenderingContext2d, HtmlCanvasElement, HtmlImageElement, ImageData, Worker, window};
 use crate::base::state::{Base, WorkerKind};
 use std::io::Cursor;
+use std::cell::{Cell, Ref};
 use utils::prelude::*;
 use dominator::clone;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use serde::{Serialize, Deserialize};
+use js_sys::{Object, Reflect};
 
 pub struct AnimationPlayer {
     pub base: Rc<Base>,
@@ -29,13 +32,14 @@ pub struct AnimationPlayer {
     pub worker: Worker,
     pub worker_listener: RefCell<Option<EventListener>>,
     pub ctx: RefCell<Option<CanvasRenderingContext2d>>,
-    pub num_frames: AtomicUsize,
     pub curr_frame_index: AtomicUsize,
+    pub prev_frame_info: RefCell<Option<FrameInfo>>,
+    pub prev_frame_data: RefCell<Option<ImageData>>,
     pub frame_infos: RefCell<Vec<FrameInfo>>,
     pub timer: RefCell<Option<Timeout>>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct FrameInfo {
     pub data_length: usize, 
     pub data_offset: usize,
@@ -58,18 +62,16 @@ struct GifWorkerEvent {
     pub data: GifWorkerEventData
 }
 
-// Serialized messages
-// the raw data buffer skips this system and requires
-// manual deserialization to strip the id
+// Simple messages
+// the raw image data payloads requires
+// manual (de)serialization 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "kind", content = "data", rename_all="snake_case")]
 enum GifWorkerEventData {
     // main -> worker: id, url
     Load(usize, String),
-    // worker -> main: id, width, height, number of frames, frame infos 
-    Init(usize, f64, f64, usize, Vec<FrameInfo>),
-    // main -> worker: id, frame index 
-    FrameReq(usize, usize),
+    // worker -> main: id, width, height, frame infos 
+    Init(usize, f64, f64, Vec<FrameInfo>),
 }
 
 static WORKER_ID:AtomicUsize = AtomicUsize::new(0);
@@ -98,8 +100,9 @@ impl AnimationPlayer {
             worker_id,
             worker_listener: RefCell::new(None),
             ctx: RefCell::new(None),
-            num_frames: AtomicUsize::new(0),
             curr_frame_index: AtomicUsize::new(0),
+            prev_frame_info: RefCell::new(None), 
+            prev_frame_data: RefCell::new(None), 
             frame_infos: RefCell::new(Vec::new()),
             timer: RefCell::new(None),
         });
@@ -108,42 +111,32 @@ impl AnimationPlayer {
             let event = event.dyn_ref::<web_sys::MessageEvent>().unwrap_throw();
             let data = event.data();
 
-            /// sending a large buffer via serialization is slow
-            /// so we handle this as a special case
-            if ArrayBuffer::is_view(&data) {
-                //get it as a buffer
-                let buffer = data.unchecked_into::<Uint8Array>();
-                //extract the id
-                let view = DataView::new(&buffer.buffer(), (buffer.length() - 4).try_into().unwrap_ji(), 4);
-                let id = view.get_uint32(0) as usize;
-
-                if id == state.worker_id {
-                    // get the pixels-only buffer
-                    let pixels = buffer.subarray(0, buffer.length() - 4);
-
-                    let frame_index = state.curr_frame_index.load(Ordering::SeqCst);
-                    //this is concerning! shouldn't happen...
-                    if let Some(frame_info) = state.frame_infos.borrow().get(frame_index) {
-                        state.paint(frame_info, pixels);
+            if let Ok(obj_data) = Reflect::get(&data, &JsValue::from_str("data")) {
+                /// ImageData payloads are slow to serde, so manually deserialize
+                if Reflect::get(&obj_data, &JsValue::from_str("kind")) == Ok(JsValue::from_str("frame_resp")) {
+                    let data = Reflect::get(&obj_data, &JsValue::from_str("data")).unwrap_ji();
+                    let id = Reflect::get(&data, &JsValue::from_str("id")).unwrap_ji().as_f64().unwrap_ji() as usize;
+                    let img_data:ImageData = Reflect::get(&data, &JsValue::from_str("img_data")).unwrap_ji().unchecked_into();
+                    if id == state.worker_id {
+                        state.blit(img_data);
                         state.clone().next_frame();
                     }
+                } else if let Ok(msg) = serde_wasm_bindgen::from_value::<GifWorkerEvent>(data) {
+                    match msg.data {
+                        GifWorkerEventData::Init(id, width, height, frame_infos)=> {
+                            if id == state.worker_id {
+                                *state.frame_infos.borrow_mut() = frame_infos; 
+
+                                //this will cause the canvas to be created
+                                //which in turn will make the first frame request
+                                state.size.set(Some((width, height)));
+
+                            }
+                        },
+                        _ => {}
+                    }
+
                 }
-            } else if let Ok(msg) = serde_wasm_bindgen::from_value::<GifWorkerEvent>(data) {
-                match msg.data {
-                    GifWorkerEventData::Init(id, width, height, num_frames, frame_infos)=> {
-                        if id == state.worker_id {
-                            //log::info!("width: {}, height: {}, num_frames: {}", width, height, num_frames);
-                            state.num_frames.store(num_frames, Ordering::SeqCst);
-                            state.size.set(Some((width, height)));
-
-                            *state.frame_infos.borrow_mut() = frame_infos; 
-
-                            state.clone().request_frame();
-                        }
-                    },
-                    _ => {}
-                }
-
             }
         })));
 
@@ -154,9 +147,17 @@ impl AnimationPlayer {
         state
     }
 
+    pub fn num_frames(&self) -> usize {
+        self.frame_infos.borrow().len()
+    }
+    pub fn map_current_frame<A>(&self, f: impl FnOnce(usize, &FrameInfo) -> A) -> A {
+        let frame_index = self.curr_frame_index.load(Ordering::SeqCst);
+        f(frame_index, self.frame_infos.borrow().get(frame_index).as_ref().unwrap_ji())
+    }
+
     pub fn next_frame(self: Rc<Self>) {
         let frame_index = self.curr_frame_index.fetch_add(1, Ordering::SeqCst);
-        if frame_index == self.num_frames.load(Ordering::SeqCst) - 1 {
+        if frame_index == self.num_frames() - 1 {
             self.curr_frame_index.store(0, Ordering::SeqCst);
         }
 
@@ -168,40 +169,121 @@ impl AnimationPlayer {
         })));
     }
     pub fn request_frame(self: Rc<Self>) {
-        let frame_index = self.curr_frame_index.load(Ordering::SeqCst);
 
-        self.worker.post_message(&serde_wasm_bindgen::to_value(&GifWorkerEvent{
-            data: GifWorkerEventData::FrameReq(self.worker_id, frame_index)
-        }).unwrap_ji());
+        self.map_current_frame(|frame_index, frame_info| {
+            let img_data = self.prep_frame();
 
-        let state = self;
+            // manually constructing due to binary buffer
+            let obj = Object::new();
+            let data = Object::new();
+            let payload = Object::new();
+
+            Reflect::set(&payload, &JsValue::from_str("img_data"), &img_data);
+            Reflect::set(&payload, &JsValue::from_str("frame_index"), &JsValue::from_f64(frame_index as f64));
+            Reflect::set(&payload, &JsValue::from_str("id"), &JsValue::from_f64(self.worker_id as f64));
+            Reflect::set(&data, &JsValue::from_str("kind"), &JsValue::from_str("frame_req"));
+            Reflect::set(&data, &JsValue::from_str("data"), &payload);
+            Reflect::set(&obj, &JsValue::from_str("data"), &data);
+            
+            self.worker.post_message(&obj).unwrap_ji();
+
+        });
+
     }
 
-    fn paint(&self, frame_info: &FrameInfo, pixels: Uint8Array) {
+    // based on: https://github.com/movableink/omggif/blob/example-web/example_web/index.html
+    fn prep_frame(&self) -> ImageData {
+        let ctx = self.ctx.borrow();
+        let ctx = ctx.as_ref().unwrap_ji();
 
-        if let Some(ctx) = self.ctx.borrow().as_ref() {
+        let (canvas_width, canvas_height) = self.size.get_cloned().unwrap_ji();
+
+        let stash = ctx.get_image_data(0.0, 0.0, canvas_width, canvas_height).unwrap_ji();
+        self.clear_frame();
+        // this is a fairly expensive operation, takes like 5-15ms
+        let buffer = ctx.get_image_data(0.0, 0.0, canvas_width, canvas_height).unwrap_ji();
+
+        ctx.put_image_data(&stash, 0.0, 0.0);
+
+        buffer
+    }
+
+    fn clear_frame(&self) {
+        self.map_current_frame(|frame_index, frame_info| {
             //let start = web_sys::window().unwrap_ji().performance().unwrap_ji().now();
+            let ctx = self.ctx.borrow();
+            let ctx = ctx.as_ref().unwrap_ji();
+
             let (canvas_width, canvas_height) = self.size.get_cloned().unwrap_ji();
 
-            // log::info!("{}", pixels.length());
+            if frame_index == 0 {
+                ctx.clear_rect(0.0, 0.0, canvas_width, canvas_height);
+            }
 
-            //TODO - disposal?
+            let prev_frame_info = self.prev_frame_info.replace(Some(frame_info.clone()));
+
+            if let Some(prev_frame_info) = prev_frame_info.as_ref() {
+                match prev_frame_info.disposal {
+                    0 => {
+                        // "No disposal specified" - do nothing, we draw over the existing canvas
+                    },
+                    1 => {
+                        // "Do not dispose" - do nothing, we draw over the existing canvas
+                    },
+                    2 => {
+                        // "Restore to background" - browsers ignore background color, so
+                        // in practice it is always "Restore to transparent"
+                        ctx.clear_rect(
+                            prev_frame_info.x as f64, 
+                            prev_frame_info.y as f64,
+                            prev_frame_info.width as f64,
+                            prev_frame_info.height as f64
+                        );
+                    },
+                    3 => {
+                        // "Restore to previous" - revert back to most recent frame that was
+                        // not set to "Restore to previous", or frame 0
+                        log::info!("should restore!");
+                        if let Some(prev_frame_data) = self.prev_frame_data.borrow().as_ref() {
+                            ctx.put_image_data(prev_frame_data, 0.0, 0.0);
+                        }
+                    },
+                    _ => {}
+                }
+            }
+
+            if frame_index == 0 || prev_frame_info.map(|x| x.disposal < 2) == Some(true) {
+                *self.prev_frame_data.borrow_mut() = Some(
+                    // this is a fairly expensive operation, takes like 5-15ms
+                    ctx.get_image_data(0.0, 0.0, canvas_width, canvas_height).unwrap_ji()
+                );
+            }
+
+
+            //log::info!("blit time: {}", web_sys::window().unwrap_ji().performance().unwrap_ji().now() - start);
+        });
+    }
+    fn blit(&self, img_data: ImageData) {
+        self.map_current_frame(|frame_index, frame_info| {
+            //let start = web_sys::window().unwrap_ji().performance().unwrap_ji().now();
+            let ctx = self.ctx.borrow();
+            let ctx = ctx.as_ref().unwrap_ji();
+
+            let (canvas_width, canvas_height) = self.size.get_cloned().unwrap_ji();
+
             //ctx.clear_rect(0.0, 0.0, canvas_width, canvas_height);
+            ctx.put_image_data_with_dirty_x_and_dirty_y_and_dirty_width_and_dirty_height(
+                &img_data,
+                0.0,
+                0.0,
+                frame_info.x as f64, 
+                frame_info.y as f64,
+                frame_info.width as f64,
+                frame_info.height as f64
+            ).unwrap_ji();
 
-
-            let img_data = ImageData::new_with_u8_clamped_array_and_sh(
-                wasm_bindgen::Clamped(&pixels.to_vec()), // would be nice to just use the typed array without a copy...
-                //pixels,
-                frame_info.width,
-                frame_info.height,
-            )
-            .unwrap_ji();
-
-            ctx.put_image_data(&img_data, frame_info.x as f64, frame_info.y as f64);
-            //log::info!("draw time: {}", web_sys::window().unwrap_ji().performance().unwrap_ji().now() - start);
-        }
-
-            
+            //log::info!("blit time: {}", web_sys::window().unwrap_ji().performance().unwrap_ji().now() - start);
+        });
     }
 }
 
