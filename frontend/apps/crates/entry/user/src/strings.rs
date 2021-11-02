102dc69aa7aa78851b91b504a282ed96b217ccfb
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2021-11-02 07:37:09 -0400

    feat(fe/user/register): Removed asterisk as it's no longer required

--- frontend/apps/crates/entry/user/src/strings.rs
+++ frontend/apps/crates/entry/user/src/strings.rs
@@ -47,7 +47,7 @@ pub mod register {
         pub const STR_SUBMIT: &str = "Submit";
         pub const STR_LOCATION_LABEL: &str = "Location *";
         pub const STR_PERSONA_LABEL: &str = "I am signing up as a... *";
-        pub const STR_ORGANIZATION_LABEL: &str = "School/Organization *";
+        pub const STR_ORGANIZATION_LABEL: &str = "School/Organization";
         pub const STR_TERMS_LABEL_ONE: &str = "I have read and accept the ";
         pub const STR_TERMS_TERMS: &str = "terms & conditions";
         pub const STR_TERMS_LABEL_TWO: &str = " and ";
