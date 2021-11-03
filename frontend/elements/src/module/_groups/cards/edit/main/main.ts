af6ba548a9aad0e60e22b21d5fe95c3ae108e3b1
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2021-11-03 12:50:16 -0400

    feat(fe/module/cards): Fit more cards on page

--- frontend/elements/src/module/_groups/cards/edit/main/main.ts
+++ frontend/elements/src/module/_groups/cards/edit/main/main.ts
@@ -1,31 +1,37 @@
-import { LitElement, html, css, customElement, property } from 'lit-element';
-import {classMap} from "lit-html/directives/class-map";
-import {nothing} from "lit-html";
+import { LitElement, html, css, customElement } from "lit-element";
 
-@customElement('main-cards')
+@customElement("main-cards")
 export class _ extends LitElement {
-  static get styles() {
-      return [css`
-          :host {
-              display: flex;
-              justify-content: center;
-              overflow-x: hidden;
-          }
-          section {
-              margin: 80px;
-              display: flex;
-              flex-wrap: wrap;
-              justify-content: center;
-              gap: 80px;
-          }
-    `];
-  }
+    static get styles() {
+        return [
+            css`
+                :host {
+                    display: flex;
+                    justify-content: center;
+                    overflow-x: hidden;
+                }
+                section {
+                    display: flex;
+                    flex-wrap: wrap;
+                    justify-content: start;
+                    margin: 24px;
+                    gap: 24px;
+                }
+                @media (min-width: 1920px) {
+                    section {
+                        margin: 32px;
+                        gap: 32px;
+                    }
+                }
+            `,
+        ];
+    }
 
-  render() {
-      return html`
-          <section>
-              <slot></slot> 
-          </section>
-      `
-  }
+    render() {
+        return html`
+            <section>
+                <slot></slot>
+            </section>
+        `;
+    }
 }
