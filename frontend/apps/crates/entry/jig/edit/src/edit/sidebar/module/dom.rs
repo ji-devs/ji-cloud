d40c4eb71a83670b5f27125abf81b2d15e21927c
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2021-11-08 14:17:39 -0500

    feat(fe/jig/edit): Open sidebar when click on emprty module

--- frontend/apps/crates/entry/jig/edit/src/edit/sidebar/module/dom.rs
+++ frontend/apps/crates/entry/jig/edit/src/edit/sidebar/module/dom.rs
@@ -73,6 +73,7 @@ impl ModuleDom {
                             },
                             None =>  {
                                 state.sidebar.jig_edit_state.route.set_neq(JigEditRoute::Landing);
+                                state.sidebar.collapsed.set_neq(false);
                             },
                         };
                     })
