ff1507e2968f9f64ad5c14f793828cda0ecec327
Matthew Pagan <mastapegs01@gmail.com>
2021-12-14 07:20:13 -0500

    remove missing deps

--- frontend/storybook/src/components/entry/admin/jig-table/single-jig.ts
+++ frontend/storybook/src/components/entry/admin/jig-table/single-jig.ts
@@ -1,4 +1,3 @@
-import { Meta, Story } from "@storybook/web-components";
 import { html } from "lit-element";
 import { JigData } from "./types";
 
@@ -16,9 +15,9 @@ import "../single-jig";
 export default {
   title: "Single Jig",
   component: "single-jig",
-} as Meta;
+};
 
-const Template: Story = ({ jig }) => html`
+const Template = ({ jig }) => html`
   <single-jig>
     <span slot="jig-name">${jig.jig_name}</span>
     <span slot="author">${jig.author}</span>
