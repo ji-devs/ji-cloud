4fb93629a476224f354b1cb32f7e78a25d1b84cd
Matthew Pagan <mastapegs01@gmail.com>
2021-12-14 07:16:21 -0500

    add stories for jig table elements

---
+++ frontend/storybook/src/components/entry/admin/jig-table/story-data.ts
@@ -0,0 +1,52 @@
+import { JigData } from "./types";
+
+export const jigs: JigData[] = [
+  {
+    jig_name: "Hebrew Letters",
+    author: "Michael Wikes",
+    author_badge: "JI Team",
+    date: "Aug. 5, 2020",
+    language: "English (American)",
+    curators: "Anat (13.7.21)",
+  },
+  {
+    jig_name: "Hebrew Numbers & Balloons",
+    author: "Michael Wikes",
+    author_badge: "JI Team",
+    date: "Aug. 5, 2020",
+    language: "English (American)",
+    curators: "Anat (13.7.21)",
+  },
+  {
+    jig_name: "Hebrew Letters",
+    author: "Michael Wikes",
+    author_badge: "JI Team",
+    date: "Aug. 5, 2020",
+    language: "English (American)",
+    curators: "Anat (13.7.21)",
+  },
+  {
+    jig_name: "Hebrew Letters",
+    author: "Michael Wikes",
+    author_badge: "JI Team",
+    date: "Aug. 5, 2020",
+    language: "English (American)",
+    curators: "Anat (13.7.21)",
+  },
+  {
+    jig_name: "Hebrew Letters",
+    author: "Michael Wikes",
+    author_badge: "JI Team",
+    date: "Aug. 5, 2020",
+    language: "English (American)",
+    curators: "Anat (13.7.21)",
+  },
+  {
+    jig_name: "Hebrew Letters",
+    author: "Michael Wikes",
+    author_badge: "JI Team",
+    date: "Aug. 5, 2020",
+    language: "English (American)",
+    curators: "Anat (13.7.21)",
+  },
+];
---
+++ frontend/storybook/src/components/entry/admin/jig-table/types.ts
@@ -0,0 +1,8 @@
+export interface JigData {
+  jig_name: string;
+  author: string;
+  author_badge: string;
+  date: string;
+  language: string;
+  curators: string;
+}
