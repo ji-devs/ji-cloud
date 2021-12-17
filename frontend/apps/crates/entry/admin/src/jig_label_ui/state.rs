9931d1df8b4dd1e1068a45883a0074e803ce756a
Matthew Pagan <mastapegs01@gmail.com>
2021-12-16 22:44:56 -0500

    working render

--- frontend/apps/crates/entry/admin/src/jig_label_ui/state.rs
+++ frontend/apps/crates/entry/admin/src/jig_label_ui/state.rs
@@ -1,15 +1,18 @@
-use std::rc::Rc;
+use dominator_helpers::futures::AsyncLoader;
 use futures_signals::signal_vec::MutableVec;
 use shared::domain::jig::JigResponse;
+use std::rc::Rc;
 
 pub struct JigUI {
   pub jigs: MutableVec<JigResponse>,
+  pub loader: AsyncLoader,
 }
 
 impl JigUI {
   pub fn new() -> Rc<Self> {
     Rc::new(Self {
       jigs: MutableVec::new(),
+      loader: AsyncLoader::new(),
     })
   }
 }
