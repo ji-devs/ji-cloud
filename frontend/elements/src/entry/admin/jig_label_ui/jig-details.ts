54d74783c8a4d260cfa335621359eea2a201655c
Matthew Pagan <mastapegs01@gmail.com>
2021-12-21 13:15:39 -0500

    more closely align with zeplin

--- frontend/elements/src/entry/admin/jig_label_ui/jig-details.ts
+++ frontend/elements/src/entry/admin/jig_label_ui/jig-details.ts
@@ -7,7 +7,22 @@ import "@elements/core/inputs/wrapper";
 export class JigLabelUI extends LitElement {
   static styles = css`
   #container {
-    padding: 10px;
+    padding: 44px;
+  }
+  #input-container {
+    margin-top: 8px;
+    padding: 31px 24px;
+    border-radius: 12px;
+    border: solid 2px #e6f0ff;
+  }
+  #general-summary {
+    color: var(--dark-gray-5);
+  }
+  input-wrapper, input-select {
+    margin-top: 24px;
+  }
+  [label="JIG's name"] {
+    margin-top: 0;
   }
   `;
   
@@ -21,35 +36,38 @@ export class JigLabelUI extends LitElement {
   render() {
     return html`
     <div id="container">
-      <input-wrapper label="JIG's name">
-        <input type="text" value=${this.name}>
-      </input-wrapper>
-      <input-wrapper label="Author name">
-        <input type="text" value=${this.authorName}>
-      </input-wrapper>
-      <input-select label="Instruction Language">
-        <input-select-option>English</input-select-option>
-        <input-select-option>Spanish</input-select-option>
-        <input-select-option>Hebrew</input-select-option>
-        <input-select-option>French</input-select-option>
-        <input-select-option>Italian</input-select-option>
-      </input-select>
-      <input-select label="Suitable for age">
-        <input-select-option>All ages</input-select-option>
-        <input-select-option>No ages</input-select-option>
-      </input-select>
-      <input-select label="Affiliation">
-        <input-select-option>Affiliation 1</input-select-option>
-        <input-select-option>Affiliation 2</input-select-option>
-        <input-select-option>Affiliation 3</input-select-option>
-      </input-select>
-      <input-wrapper label="JIG teacher's description">
-        <textarea id="description" rows="6" value=${this.description}></textarea>
-      </input-wrapper>
-      <input-wrapper label="Additional keywords">
-        <textarea rows="6" value=${this.keywords}></textarea>
-      </input-wrapper>
-      <slot></slot>
+      <div id="general-summary">General Summary</div>
+      <!-- Cancel and Save Changes Button -->
+      <div id="input-container">
+        <input-wrapper label="JIG's name">
+          <input type="text" value=${this.name}>
+        </input-wrapper>
+        <input-wrapper label="Author name">
+          <input type="text" value=${this.authorName}>
+        </input-wrapper>
+        <input-select label="Instruction Language">
+          <input-select-option>English</input-select-option>
+          <input-select-option>Spanish</input-select-option>
+          <input-select-option>Hebrew</input-select-option>
+          <input-select-option>French</input-select-option>
+          <input-select-option>Italian</input-select-option>
+        </input-select>
+        <input-select label="Suitable for age">
+          <input-select-option>All ages</input-select-option>
+          <input-select-option>No ages</input-select-option>
+        </input-select>
+        <input-select label="Affiliation">
+          <input-select-option>Affiliation 1</input-select-option>
+          <input-select-option>Affiliation 2</input-select-option>
+          <input-select-option>Affiliation 3</input-select-option>
+        </input-select>
+        <input-wrapper label="JIG teacher's description">
+          <textarea id="description" rows="6" value=${this.description}></textarea>
+        </input-wrapper>
+        <input-wrapper label="Additional keywords">
+          <textarea rows="6" value=${this.keywords}></textarea>
+        </input-wrapper>
+      </div>
     </div>
     `
   }
