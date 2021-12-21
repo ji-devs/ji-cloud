bc7d95d3c61a4427bc2d125d27a0fdd328059de9
Matthew Pagan <mastapegs01@gmail.com>
2021-12-21 13:57:20 -0500

    extracted input elements from jig-detail container

--- frontend/elements/src/entry/admin/jig_label_ui/jig-details.ts
+++ frontend/elements/src/entry/admin/jig_label_ui/jig-details.ts
@@ -29,20 +29,16 @@ export class JigLabelUI extends LitElement {
     border-radius: 12px;
     border: solid 2px #e6f0ff;
   }
-  input-wrapper, input-select {
+  ::slotted(input-wrapper) {
     margin-top: 24px;
   }
-  [label="JIG's name"] {
+  ::slotted(input-select) {
+    margin-top: 24px;
+  }
+  ::slotted(#first-input) {
     margin-top: 0;
   }
   `;
-  
-  @property({type: String}) name: string = ""
-  @property({type: String}) authorName: string = ""
-  @property({type: String}) language: string = ""
-  @property({type: String}) suitableForAge: string = ""
-  @property({type: String}) description: string = ""
-  @property({type: String}) keywords: string = ""
   
   render() {
     return html`
@@ -57,34 +53,7 @@ export class JigLabelUI extends LitElement {
         </div>
       </div>
       <div id="input-container">
-        <input-wrapper label="JIG's name">
-          <input type="text" value=${this.name}>
-        </input-wrapper>
-        <input-wrapper label="Author name">
-          <input type="text" value=${this.authorName}>
-        </input-wrapper>
-        <input-select label="Instruction Language">
-          <input-select-option>English</input-select-option>
-          <input-select-option>Spanish</input-select-option>
-          <input-select-option>Hebrew</input-select-option>
-          <input-select-option>French</input-select-option>
-          <input-select-option>Italian</input-select-option>
-        </input-select>
-        <input-select label="Suitable for age">
-          <input-select-option>All ages</input-select-option>
-          <input-select-option>No ages</input-select-option>
-        </input-select>
-        <input-select label="Affiliation">
-          <input-select-option>Affiliation 1</input-select-option>
-          <input-select-option>Affiliation 2</input-select-option>
-          <input-select-option>Affiliation 3</input-select-option>
-        </input-select>
-        <input-wrapper label="JIG teacher's description">
-          <textarea id="description" rows="6" value=${this.description}></textarea>
-        </input-wrapper>
-        <input-wrapper label="Additional keywords">
-          <textarea rows="6" value=${this.keywords}></textarea>
-        </input-wrapper>
+        <slot></slot>
       </div>
     </div>
     `
--- frontend/storybook/src/components/entry/admin/jig-table/jig-details.ts
+++ frontend/storybook/src/components/entry/admin/jig-table/jig-details.ts
@@ -8,7 +8,36 @@ export default {
 
 export const JigDetails = () => {
   return `
-    <jig-details></jig-details>
+    <jig-details>
+      <input-wrapper id="first-input" label="JIG's name">
+      <input type="text" value="">
+      </input-wrapper>
+      <input-wrapper label="Author name">
+        <input type="text" value="">
+      </input-wrapper>
+      <input-select label="Instruction Language">
+        <input-select-option>English</input-select-option>
+        <input-select-option>Spanish</input-select-option>
+        <input-select-option>Hebrew</input-select-option>
+        <input-select-option>French</input-select-option>
+        <input-select-option>Italian</input-select-option>
+      </input-select>
+      <input-select label="Suitable for age">
+        <input-select-option>All ages</input-select-option>
+        <input-select-option>No ages</input-select-option>
+      </input-select>
+      <input-select label="Affiliation">
+        <input-select-option>Affiliation 1</input-select-option>
+        <input-select-option>Affiliation 2</input-select-option>
+        <input-select-option>Affiliation 3</input-select-option>
+      </input-select>
+      <input-wrapper label="JIG teacher's description">
+        <textarea id="description" rows="6" value=""></textarea>
+      </input-wrapper>
+      <input-wrapper label="Additional keywords">
+        <textarea rows="6" value=""></textarea>
+      </input-wrapper>
+    </jig-details>
   `
 };
 JigDetails.args = {}
