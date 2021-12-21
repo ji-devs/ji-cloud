f4278213c2d81a3bae7bde1846c8d317e2e51c05
Matthew Pagan <mastapegs01@gmail.com>
2021-12-21 10:14:43 -0500

    hook up storybook to draft of jig-details

---
+++ frontend/storybook/src/components/entry/admin/jig-table/jig-details.ts
@@ -0,0 +1,13 @@
+import "@elements/entry/admin/jig_label_ui/jig-details";
+
+export default {
+  title: "Entry/Admin/Jig Details",
+  component: "jig-details",
+};
+
+export const JigDetails = () => {
+  return `
+    <jig-details></jig-details>
+  `
+};
+JigDetails.args = {}
