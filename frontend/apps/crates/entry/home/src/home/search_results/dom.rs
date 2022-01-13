311ef77952e93927d6924617f06469f71fd835e6
Tyrone Tudehope <tyronetudehope@gmail.com>
2022-01-13 11:55:05 +0200

    feat(fe/search): Hide search results when the total is 0

--- frontend/apps/crates/entry/home/src/home/search_results/dom.rs
+++ frontend/apps/crates/entry/home/src/home/search_results/dom.rs
@@ -1,21 +1,29 @@
-
-
-use dominator::{html, Dom};
-
-
+use dominator::{clone, html, Dom};
+use futures_signals::signal::SignalExt;
 use std::rc::Rc;
-
+use crate::home::search_results::search_results_section::SearchResultsSection;
 
 use super::state::SearchResults;
 
 impl SearchResults {
     pub fn render(self: &Rc<Self>) -> Dom {
         let state = self;
+
+        let search_results_signal = |state: Rc<SearchResultsSection>| {
+            state.total.signal_cloned().map(clone!(state => move |total| {
+                if total > 0 {
+                    Some(state.render())
+                } else {
+                    None
+                }
+            }))
+        };
+
         html!("home-search-results", {
             .property_signal("resultsCount", state.total_results_count_signal())
             .property("query", &state.query)
-            .child(state.jigs.render())
-            .child(state.resources.render())
+            .child_signal(search_results_signal(state.jigs.clone()))
+            .child_signal(search_results_signal(state.resources.clone()))
         })
     }
 }
