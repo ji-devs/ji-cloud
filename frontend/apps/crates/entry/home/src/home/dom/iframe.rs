21aba1cccd030a5f752ab45180abed6d3c1650bc
Matthew Pagan <mastapegs01@gmail.com>
2022-01-17 00:02:19 +0000

    postmessaging browser, trying to figure out how to receive response

--- frontend/apps/crates/entry/home/src/home/dom/iframe.rs
+++ frontend/apps/crates/entry/home/src/home/dom/iframe.rs
@@ -2,9 +2,12 @@ use dominator::{clone, events, html, with_node, Dom};
 use futures_signals::signal::{Mutable, SignalExt};
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::{JsValue, JsCast};
 use web_sys::HtmlIFrameElement;
+use js_sys::Function;
 
 const STR_JEWISH_INTERACTIVE_URL: &str = "https://www.jewishinteractive.org/jigzi-home";
+const STR_TARGET_DOMAIN: &str = "https://www.jewishinteractive.org/";
 
 pub struct Iframe {
     height: Mutable<usize>,
@@ -45,12 +48,17 @@ impl Iframe {
 #[wasm_bindgen]
 pub fn get_height(iframe: &HtmlIFrameElement) -> Result<usize, JsValue> {
     let height: usize = match iframe.content_window() {
-        Some(window) => {
-            log::info!("{:#?}", window.name());
+        Some(content_window) => {
+            match content_window.post_message(&JsValue::from_str("scrollHeight"), STR_TARGET_DOMAIN) {
+                Ok(_) => log::info!("post_message worked"),
+                Err(error) => log::info!("post_message failed\n{:?}", error),
+            };
             3000
         },
         None => 3000
     };
     
     Ok(height)
+
+    // iframe.post_message("", "");
 }
