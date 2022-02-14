39a649df03db7de3d2299617eb86893ae8b225e3
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-02-14 14:53:31 -0500

    feat(fe/jig/play): New end of jig animation

--- frontend/elements/src/entry/jig/play/done-popup.ts
+++ frontend/elements/src/entry/jig/play/done-popup.ts
@@ -10,12 +10,8 @@ export class _ extends PopupBase {
         return [
             ...super.styles,
             css`
-                .images {
-                    display: grid;
-                }
-                .images .trophy {
-                    width: 100px;
-                    place-self: center;
+                img-ui {
+                    height: 350px;
                 }
                 h3 {
                     margin: 0;
@@ -41,23 +37,14 @@ export class _ extends PopupBase {
         return html`
             ${this.renderBase(() => {
                 return html`
-                    <div class="images">
-                        <img-ui
-                            path="entry/jig/play/party-poppers.svg"
-                        ></img-ui>
-                        <img-ui
-                            class="trophy"
-                            path="entry/jig/play/trophy-gold.svg"
-                        ></img-ui>
-                    </div>
+                    <img-ui path="entry/jig/play/jig-finish.gif"></img-ui>
                     ${this.score !== undefined
                         ? html`
-                              <div class="score-section">
-                                  <h3>${STR_SCORE}</h3>
-                                  <h2>${this.score}</h2>
-                              </div>
-                          `
-                        : nothing}
+                            <div class="score-section">
+                                <h3>${STR_SCORE}</h3>
+                                <h2>${this.score}</h2>
+                            </div>
+                        ` : nothing}
                 `;
             })}
         `;
