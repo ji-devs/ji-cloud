069a4d088fc5b11ebe5d3a97c3550edb04fbe2c4
Matthew Pagan <mastapegs01@gmail.com>
2022-03-01 15:04:56 -0500

    fix: handle none case for age in age range

--- frontend/apps/crates/utils/src/ages.rs
+++ frontend/apps/crates/utils/src/ages.rs
@@ -13,31 +13,44 @@ impl AgeRangeVecExt for Vec<AgeRange> {
         if selected.len() == self.len() || selected.len() == 0 {
             STR_ALL_AGES.to_string()
         } else if selected.len() == 1 {
-            let age = self.iter().find(|age| selected.contains(&age.id)).unwrap();
-            let age_text = age.short_display_name.as_ref().unwrap_or(&age.display_name).to_string();
+            let age = self.iter().find(|age| selected.contains(&age.id));
 
-            age_text
+            if let Some(age) = age {
+                let age_text = age
+                    .short_display_name
+                    .as_ref()
+                    .unwrap_or(&age.display_name)
+                    .to_string();
+
+                age_text
+            } else {
+                String::new()
+            }
         } else {
-            let first_age = self
-                .iter()
-                .find(|age| selected.contains(&age.id))
-                .unwrap();
-            let last_age = self
-                .iter()
-                .rev()
-                .find(|age| selected.contains(&age.id))
-                .unwrap();
+            let first_age = self.iter().find(|age| selected.contains(&age.id));
+            let last_age = self.iter().rev().find(|age| selected.contains(&age.id));
 
-            let first_age_text = first_age.short_display_name.as_ref().unwrap_or(&first_age.display_name);
-            let last_age_text = last_age.short_display_name.as_ref().unwrap_or(&last_age.display_name);
+            if let (Some(first_age), Some(last_age)) = (first_age, last_age) {
+                let first_age_text = first_age
+                    .short_display_name
+                    .as_ref()
+                    .unwrap_or(&first_age.display_name);
 
-            let mut age_text = String::new();
+                let last_age_text = last_age
+                    .short_display_name
+                    .as_ref()
+                    .unwrap_or(&last_age.display_name);
 
-            age_text.push_str(first_age_text);
-            age_text.push_str(STR_DASH);
-            age_text.push_str(last_age_text);
+                let mut age_text = String::new();
 
-            age_text
+                age_text.push_str(first_age_text);
+                age_text.push_str(STR_DASH);
+                age_text.push_str(last_age_text);
+
+                age_text
+            } else {
+                String::new()
+            }
         }
     }
 }
