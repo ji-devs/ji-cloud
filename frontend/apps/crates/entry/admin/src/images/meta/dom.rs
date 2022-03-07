a490724cadbf7240b0805a1fac34d12f39578dc6
Tyrone Tudehope <tyronetudehope@gmail.com>
2022-03-07 08:13:02 +0200

    fix(fe/admin): Include page_limit in ImageSearchQuery

--- frontend/apps/crates/entry/admin/src/images/meta/dom.rs
+++ frontend/apps/crates/entry/admin/src/images/meta/dom.rs
@@ -66,6 +66,7 @@ impl ImageMetaPage {
                                     is_premium: None,
                                     is_published: None,
                                     kind: None,
+                                    page_limit: None,
                                 };
                                 let route:String = Route::Admin(AdminRoute::ImageSearch(Some(query))).into();
                                 dominator::routing::go_to_url(&route);
