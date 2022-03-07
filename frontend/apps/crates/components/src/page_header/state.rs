05f88d1575aeaf55a52da9912b29fe2e8cdd83b6
Tyrone Tudehope <tyronetudehope@gmail.com>
2022-03-07 10:33:49 +0200

    fix(fe): Only show the tooltip once

--- frontend/apps/crates/components/src/page_header/state.rs
+++ frontend/apps/crates/components/src/page_header/state.rs
@@ -2,7 +2,7 @@ use awsm_web::loaders::helpers::AsyncLoader;
 use futures_signals::signal::Mutable;
 use shared::domain::user::UserProfile;
 use strum_macros::EnumIter;
-use utils::routes::{HomeRoute, JigRoute, Route};
+use utils::{routes::{HomeRoute, JigRoute, Route}, storage, unwrap::UnwrapJiExt};
 
 const TARGET_SELF: &str = "_self";
 const TARGET_BLANK: &str = "_blank";
@@ -15,10 +15,29 @@ pub struct State {
 
 impl State {
     pub fn new() -> Self {
+        let show_beta_tooltip = {
+            if let Ok(local_storage) = storage::get_local_storage() {
+                // If we have access to local_storage, and the item is not set, show the tooltip...
+                let show_beta_tooltip = local_storage.get_item("beta-tooltip").unwrap_ji().is_none();
+
+                if show_beta_tooltip {
+                    // And then immediately set the item so that it isn't shown again
+                    // automatically.
+                    let _ = local_storage.set_item("beta-tooltip", "hidden");
+                }
+
+                show_beta_tooltip
+            } else {
+                // If for some reason we don't have access to local_storage, then never
+                // automatically show the tooltip because otherwise it would _always_ show it.
+                false
+            }
+        };
+
         Self {
             logged_in: Mutable::new(LoggedInState::Loading),
             loader: AsyncLoader::new(),
-            beta_tooltip: Mutable::new(true),
+            beta_tooltip: Mutable::new(show_beta_tooltip),
         }
     }
 }
