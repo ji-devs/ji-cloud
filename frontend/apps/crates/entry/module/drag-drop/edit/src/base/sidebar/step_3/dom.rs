395bc0e3b756740d9b7ef74959bff5e7421b3871
Tyrone Tudehope <tyronetudehope@gmail.com>
2022-03-09 13:46:14 +0200

    feat(fe/module/drag-drop): Use empty-sidebar element for "Drop Areas" step

--- frontend/apps/crates/entry/module/drag-drop/edit/src/base/sidebar/step_3/dom.rs
+++ frontend/apps/crates/entry/module/drag-drop/edit/src/base/sidebar/step_3/dom.rs
@@ -4,8 +4,9 @@ pub fn render_step_3() -> Dom {
     html!("module-sidebar-body", {
         .property("slot", "body")
         .child(
-            html!("div", {
-                .text(crate::strings::STR_SIDEBAR_TRACE)
+            html!("sidebar-empty", {
+                .property("label", crate::strings::STR_SIDEBAR_TRACE)
+                .property("imagePath", "module/_common/edit/sidebar/illustration-trace-area.svg")
             })
         )
     })
