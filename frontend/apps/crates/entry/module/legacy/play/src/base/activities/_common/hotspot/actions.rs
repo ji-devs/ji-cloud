d36546cc6e54eb1a4d7d51bfd72e70b55e9f4a5f
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-03-31 07:26:34 -0400

    style(fe/legacy/play): Compiler warning

--- frontend/apps/crates/entry/module/legacy/play/src/base/activities/_common/hotspot/actions.rs
+++ frontend/apps/crates/entry/module/legacy/play/src/base/activities/_common/hotspot/actions.rs
@@ -5,7 +5,7 @@ impl Hotspot {
     pub fn fade_out(&self) {
         self.fade_animation.animate_to(Percentage::new(1.0));
     }
-    pub fn fade_in(&self) {
+    pub fn _fade_in(&self) {
         self.fade_animation.animate_to(Percentage::new(0.0));
     }
 }
