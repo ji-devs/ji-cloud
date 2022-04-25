66095e2672eed94e8684b898c649452b616e8c28
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-04-25 12:20:31 -0400

    feat(fe): Added some image tags

--- frontend/apps/crates/components/src/image/tag.rs
+++ frontend/apps/crates/components/src/image/tag.rs
@@ -7,6 +7,17 @@ const STR_MULTIPLE_CHOICE: &str = "Multiple Choice";
 const STR_DRAG_AND_DROP: &str = "Drag & Drop";
 const STR_VIDEO: &str = "Video";
 const STR_NAVIGATION_BUTTON: &str = "Navigation Button";
+const STR_TABLE: &str = "Table";
+const STR_MAP: &str = "Map";
+const STR_BOARDS: &str = "Boards";
+const STR_TIMELINE: &str = "Timeline";
+const STR_BOOK: &str = "Book";
+const STR_PRINTABLES: &str = "Printables";
+const STR_PHOTO_ALBUM: &str = "Photo Album";
+const STR_MUSIC: &str = "Music";
+const STR_COMIX: &str = "Comix";
+const STR_WARDROBE: &str = "Wardrobe";
+const STR_STAGE: &str = "Stage";
 
 #[repr(i16)]
 #[derive(EnumIter, Debug, PartialEq, Copy, Clone, Eq, Hash)]
@@ -18,6 +29,17 @@ pub enum ImageTag {
     DragAndDrop = 4,
     Video = 5,
     NavigationButton = 6,
+    Table = 7,
+    Map = 8,
+    Boards = 9,
+    Timeline = 10,
+    Book = 11,
+    Printables = 12,
+    PhotoAlbum = 13,
+    Music = 14,
+    Comix = 15,
+    Wardrobe = 16,
+    Stage = 17,
 }
 
 impl ImageTag {
@@ -30,6 +52,17 @@ impl ImageTag {
             Self::DragAndDrop => STR_DRAG_AND_DROP,
             Self::Video => STR_VIDEO,
             Self::NavigationButton => STR_NAVIGATION_BUTTON,
+            Self::Table => STR_TABLE,
+            Self::Map => STR_MAP,
+            Self::Boards => STR_BOARDS,
+            Self::Timeline => STR_TIMELINE,
+            Self::Book => STR_BOOK,
+            Self::Printables => STR_PRINTABLES,
+            Self::PhotoAlbum => STR_PHOTO_ALBUM,
+            Self::Music => STR_MUSIC,
+            Self::Comix => STR_COMIX,
+            Self::Wardrobe => STR_WARDROBE,
+            Self::Stage => STR_STAGE,
         }
     }
 
