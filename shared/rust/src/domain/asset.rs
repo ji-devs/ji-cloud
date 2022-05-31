d9faf4133c5334b06f9953b82bfe3afd7de35238
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-05-31 16:55:04 -0400

    feat(shared): Asset.cover handle course case

--- shared/rust/src/domain/asset.rs
+++ shared/rust/src/domain/asset.rs
@@ -197,7 +197,7 @@ impl Asset {
     /// get cover
     pub fn cover(&self) -> Option<&LiteModule> {
         match self {
-            Self::Jig(jig) => jig.jig_data.modules.get(0),
+            Self::Jig(jig) => jig.jig_data.modules.first(),
             Self::Course(course) => course.course_data.cover.as_ref(),
         }
     }
