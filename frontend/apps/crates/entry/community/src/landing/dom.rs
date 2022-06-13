681def3ab0f0d5df21f655a9b1dfc2acc0e406c9
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-06-13 14:55:31 -0400

    feat(fe/community): Landing page styles

--- frontend/apps/crates/entry/community/src/landing/dom.rs
+++ frontend/apps/crates/entry/community/src/landing/dom.rs
@@ -1,20 +1,26 @@
 use std::rc::Rc;
 
-use dominator::{clone, html, Dom};
+use dominator::{clone, html, Dom, link};
 use futures_signals::signal::SignalExt;
 use shared::domain::{badge::Badge, user::public_user::PublicUser};
+use utils::routes::{Route, CommunityRoute, CommunityMembersRoute, CommunityBadgesRoute};
+use wasm_bindgen::JsValue;
 
 use super::CommunityLanding;
+
+const STR_SEE_MORE: &str = "See more";
 
 impl CommunityLanding {
     pub fn render(self: Rc<Self>) -> Dom {
         let state = self;
         state.load_data();
 
-        html!("div", {
+        html!("community-landing", {
             .children_signal_vec(state.top_members.signal_ref(clone!(state => move |top_members| {
                 match top_members {
-                    None => vec![html!("progress")],
+                    None => vec![html!("progress", {
+                        .property("slot", "members")
+                    })],
                     Some(top_members) => {
                         top_members.iter().map(|member| {
                             state.render_member(member)
@@ -22,9 +28,19 @@ impl CommunityLanding {
                     },
                 }
             })).to_signal_vec())
+            .child(html!("button-rect", {
+                .property("slot", "members-link")
+                .property("color", "blue")
+                .text(STR_SEE_MORE)
+                .apply(move |dom| dominator::on_click_go_to_url!(dom, {
+                    Route::Community(CommunityRoute::Members(CommunityMembersRoute::List)).to_string()
+                }))
+            }))
             .children_signal_vec(state.top_badges.signal_ref(clone!(state => move |top_badges| {
                 match top_badges {
-                    None => vec![html!("progress")],
+                    None => vec![html!("progress", {
+                        .property("slot", "badges")
+                    })],
                     Some(top_badges) => {
                         top_badges.iter().map(|badge| {
                             state.render_badge(badge)
@@ -32,17 +48,48 @@ impl CommunityLanding {
                     },
                 }
             })).to_signal_vec())
+            .child(html!("button-rect", {
+                .property("slot", "badges-link")
+                .property("color", "blue")
+                .text(STR_SEE_MORE)
+                .apply(move |dom| dominator::on_click_go_to_url!(dom, {
+                    Route::Community(CommunityRoute::Badges(CommunityBadgesRoute::List)).to_string()
+                }))
+            }))
         })
     }
 
     fn render_member(self: &Rc<Self>, member: &PublicUser) -> Dom {
-        html!("div", {
+        link!(Route::Community(CommunityRoute::Members(CommunityMembersRoute::Member(member.id))).to_string(), {
+            .property("slot", "members")
+            .child(html!("profile-image", {
+                .style("height", "64px")
+                .style("width", "64px")
+                .style("overflow", "hidden")
+                .style("border-radius", "50%")
+                .property("slot", "profile-image")
+                .property("imageId", {
+                    match &member.profile_image {
+                        Some(image_id) => JsValue::from_str(&image_id.0.to_string()),
+                        None => JsValue::UNDEFINED,
+                    }
+                })
+            }))
             .text(&member.given_name)
         })
     }
 
     fn render_badge(self: &Rc<Self>, badge: &Badge) -> Dom {
         html!("div", {
+            .property("slot", "badges")
+            .child(html!("img", {
+                .style("height", "90px")
+                .style("width", "90px")
+                .style("box-shadow", "0 0 8px 0 rgba(0, 0, 0, 0.06)")
+                .style("border", "solid 1px var(--light-gray-1)")
+                .style("border-radius", "50%")
+                .property("src", badge.thumbnail.as_str())
+            }))
             .text(&badge.display_name)
         })
     }
---
+++ frontend/elements/src/entry/community/landing.ts
@@ -0,0 +1,97 @@
+import { LitElement, html, css, customElement } from "lit-element";
+
+const STR_WELCOME_NEW_MEMBERS = "Welcome our new members";
+const STR_NEW_BADGES = "New badges";
+
+@customElement("community-landing")
+export class _ extends LitElement {
+    static get styles() {
+        return [
+            css`
+                :host {
+                    background-color: #fff6d9;
+                    display: grid;
+                    grid-template-columns: auto auto;
+                    align-items: start;
+                    gap: 40px;
+                    padding: 40px;
+                }
+                section {
+                    padding: 36px;
+                    border-radius: 16px;
+                    border: solid 1px var(--light-orange-3);
+                    background-color: #ffffff;
+                    display: grid;
+                    row-gap: 30px;
+                }
+                h3 {
+                    font-size: 24px;
+                    font-weight: bold;
+                    color: var(--dark-blue-4);
+                    margin: 0;
+                }
+                h3 fa-icon {
+                    color: var(--main-yellow);
+                    margin-right: 5px;
+                }
+                ::slotted(a[slot=members]) {
+                    cursor: pointer;
+                    text-decoration: none;
+                    display: grid;
+                    grid-template-columns: 64px 1fr;
+                    grid-auto-rows: 64px;
+                    column-gap: 40px;
+                    align-items: center;
+                    border-top: solid 1px var(--main-yellow);
+                    padding: 16px 0;
+                    color: var(--dark-gray-6);
+                    font-size: 16px;
+                    font-weight: 500;
+                }
+                ::slotted([slot=members-link]) {
+                    justify-self: center;
+                }
+                .badge-items {
+                    border-top: solid 1px var(--main-yellow);
+                    padding: 16px 0;
+                    display: grid;
+                    grid-template-columns: repeat(3, auto);
+                    justify-content: space-between;
+                    row-gap: 30px;
+                }
+                ::slotted(div[slot=badges]) {
+                    display: grid;
+                    justify-items: center;
+                }
+                ::slotted([slot=badges-link]) {
+                    justify-self: center;
+                }
+            `,
+        ];
+    }
+
+    render() {
+        return html`
+            <section class="member-section">
+                <h3>
+                    <fa-icon icon="fa-solid fa-stars"></fa-icon>
+                    ${STR_WELCOME_NEW_MEMBERS}
+                </h3>
+                <div class="member-items">
+                    <slot name="members"></slot>
+                </div>
+                <slot name="members-link"></slot>
+            </section>
+            <section class="badge-section">
+                <h3>
+                    <fa-icon icon="fa-solid fa-people-group"></fa-icon>
+                    ${STR_NEW_BADGES}
+                </h3>
+                <div class="badge-items">
+                    <slot name="badges"></slot>
+                </div>
+                <slot name="badges-link"></slot>
+            </section>
+        `;
+    }
+}
