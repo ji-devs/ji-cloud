f76ddb91f2c48b4e167af9cc79476dced9e1a95c
Tyrone Tudehope <tyronetudehope@gmail.com>
2022-06-14 17:38:18 +0200

    fix(fe/utils): Make age ranges optional

--- frontend/apps/crates/utils/src/ages.rs
+++ frontend/apps/crates/utils/src/ages.rs
@@ -1,53 +1,44 @@
 use shared::domain::meta::{AgeRange, AgeRangeId};
 pub use shared::domain::module::body::ThemeId;
 
-use crate::prelude::UnwrapJiExt;
-
 const STR_ALL_AGES: &str = "All Ages";
 
 pub trait AgeRangeVecExt {
-    fn range(&self, selected: &[AgeRangeId]) -> (String, Option<String>);
+    fn range(&self, selected: &[AgeRangeId]) -> (Option<String>, Option<String>);
 }
 
 impl AgeRangeVecExt for Vec<AgeRange> {
-    fn range(&self, selected: &[AgeRangeId]) -> (String, Option<String>) {
+    fn range(&self, selected: &[AgeRangeId]) -> (Option<String>, Option<String>) {
         if selected.len() == self.len() || selected.is_empty() {
-            (STR_ALL_AGES.to_string(), None)
+            (Some(STR_ALL_AGES.to_string()), None)
         } else if selected.len() == 1 {
             (get_age_text(self, selected, false), None)
         } else {
             let first_age_text = get_age_text(self, selected, false);
             let last_age_text = get_age_text(self, selected, true);
 
-            (first_age_text, Some(last_age_text))
+            (first_age_text, last_age_text)
         }
     }
 }
 
-fn get_age_text(ages: &[AgeRange], selected: &[AgeRangeId], get_last: bool) -> String {
+fn get_age_text(ages: &[AgeRange], selected: &[AgeRangeId], get_last: bool) -> Option<String> {
     match get_last {
-        false => ages
-            .iter()
-            .find(|age| selected.contains(&age.id))
-            .map(|age_range| {
-                age_range
-                    .short_display_name
-                    .as_ref()
-                    .unwrap_or(&age_range.display_name)
-                    .to_string()
-            })
-            .unwrap_ji(),
-        true => ages
-            .iter()
-            .rev()
-            .find(|age| selected.contains(&age.id))
-            .map(|age_range| {
-                age_range
-                    .short_display_name
-                    .as_ref()
-                    .unwrap_or(&age_range.display_name)
-                    .to_string()
-            })
-            .unwrap_ji(),
+        false => find_age(ages.iter(), selected),
+        true => find_age(ages.iter().rev(), selected),
     }
+}
+
+fn find_age<'range>(
+    mut ages: impl Iterator<Item = &'range AgeRange>,
+    selected: &[AgeRangeId],
+) -> Option<String> {
+    ages.find(|age| selected.contains(&age.id))
+        .map(|age_range| {
+            age_range
+                .short_display_name
+                .as_ref()
+                .unwrap_or(&age_range.display_name)
+                .to_string()
+        })
 }
