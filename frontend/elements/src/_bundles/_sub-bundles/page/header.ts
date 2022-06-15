926cfd6e32e55b75298c7f288688946e0befa5db
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-06-15 12:49:36 -0400

    fe(fe/community): Added required import

--- frontend/elements/src/_bundles/_sub-bundles/page/header.ts
+++ frontend/elements/src/_bundles/_sub-bundles/page/header.ts
@@ -6,3 +6,4 @@ import "@elements/core/page-header/page-header-profile";
 import "@elements/core/page-header/page-header-student-code";
 import "@elements/core/profile-image";
 import "@elements/core/buttons/rectangle";
+import "@elements/core/overlays/tooltip/info";
