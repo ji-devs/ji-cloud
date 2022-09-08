da686074f1d9f6976161ffd29ec47c7e451cfa07
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-09-08 16:00:31 -0400

    feat(fe/module/memory/play): Small sidebar UI updates

--- frontend/elements/src/module/memory/play/sidebar.ts
+++ frontend/elements/src/module/memory/play/sidebar.ts
@@ -1,50 +1,28 @@
-import {
-    LitElement,
-    html,
-    css,
-    customElement,
-    property,
-    unsafeCSS,
-} from "lit-element";
-import { mediaUi } from "@utils/path";
-
-const STR_TITLE = "My Pairs";
+import { LitElement, html, css, customElement } from "lit-element";
 
 @customElement("play-sidebar")
 export class _ extends LitElement {
     static get styles() {
         return [
             css`
-                :host {
-                }
-                section {
+                .pairs {
                     width: calc(263rem * (1920 / 1719));
                     height: calc(915rem * (1920 / 1719));
                     margin-left: calc(24rem * (1920 / 1719));
                     margin-top: calc(24rem * (1920 / 1719));
                     border-radius: 16px;
                     box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
-                    background-color: var(--dark-blue-7);
-                }
-
-                .title {
-                    padding-top: calc(16rem * (1920 / 1719));
-                    font-size: calc(22rem * (1920 / 1719));
-                    text-align: center;
-                    color: var(--light-gray-4);
-                }
-                .pairs {
+                    background-color: rgba(10, 48, 82, .2);
                 }
             `,
         ];
     }
 
     render() {
-        return html` <section>
-            <div class="title">${STR_TITLE}</div>
+        return html`
             <div class="pairs">
                 <slot></slot>
             </div>
-        </section>`;
+        `;
     }
 }
