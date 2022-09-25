a6451bf15d5132873fcc04ca0ac48f08f8250473
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-09-25 05:48:16 -0400

    feat(fe/module/card/edit): Only shoe hebrew keyboard when editing

--- frontend/apps/crates/components/src/module/_groups/cards/edit/main/pair/dom.rs
+++ frontend/apps/crates/components/src/module/_groups/cards/edit/main/pair/dom.rs
@@ -1,9 +1,10 @@
 use dominator::{html, Dom};
+use futures_signals::map_ref;
 use std::rc::Rc;
 
 use wasm_bindgen::prelude::*;
 
-use futures_signals::signal::SignalExt;
+use futures_signals::signal::{Signal, SignalExt};
 
 use super::card::dom::render as render_card;
 use super::state::*;
@@ -15,8 +16,23 @@ pub fn render<RawData: RawDataExt, E: ExtraExt>(state: Rc<MainPair<RawData, E>>)
         .property_signal("index", state.index.signal().map(|x| {
             JsValue::from_f64(x.unwrap_or_default() as f64)
         }))
-        .child(HebrewButtons::keyboard_only().render(Some("hebrew-buttons")))
+        .child_signal(editing_a_card_signal(&state).map(|editing| {
+            editing.then(|| {
+                HebrewButtons::keyboard_only().render(Some("hebrew-buttons"))
+            })
+        }))
         .child(render_card(state.left.clone()))
         .child(render_card(state.right.clone()))
     })
+}
+
+fn editing_a_card_signal<RawData: RawDataExt, E: ExtraExt>(
+    state: &Rc<MainPair<RawData, E>>,
+) -> impl Signal<Item = bool> {
+    map_ref! {
+        let editing_left = state.left.editing_active.signal(),
+        let editing_right = state.right.editing_active.signal() => {
+            *editing_left || *editing_right
+        }
+    }
 }
