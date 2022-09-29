a58230e9558f7403fbe3e001f3251c6281e8b4ba
Tyrone Tudehope <tyronetudehope@gmail.com>
2022-09-29 12:53:27 +0200

    fix(fe/drag-drop): Show hints and start activity only after the student has clicked play

--- frontend/apps/crates/entry/module/drag-drop/play/src/base/game/hints/dom.rs
+++ frontend/apps/crates/entry/module/drag-drop/play/src/base/game/hints/dom.rs
@@ -2,17 +2,24 @@ use super::state::*;
 use std::rc::Rc;
 
 use components::{
+    module::_common::play::prelude::ModulePlayPhase,
     stickers::dom::render_stickers_raw,
     traces::show::{TracesShow, TracesShowMode},
 };
 use dominator::{clone, html, Dom};
+use futures_signals::signal::SignalExt;
 use gloo_timers::future::TimeoutFuture;
 pub fn render(state: Rc<Hints>) -> Dom {
     html!("empty-fragment", {
-        .future(clone!(state => async move {
-            TimeoutFuture::new(crate::config::HINT_TIME).await;
-            state.finish();
-        }))
+        .future(state.game.base.module_phase.signal_cloned().for_each(clone!(state => move |phase| {
+            clone!(state => async move {
+                // Only start the timer if the activity is in the Playing phase.
+                if let ModulePlayPhase::Playing = phase {
+                    TimeoutFuture::new(crate::config::HINT_TIME).await;
+                    state.finish();
+                }
+            })
+        })))
         .child(render_stickers_raw(
             &state.game.base.items
                 .iter()
@@ -22,13 +29,19 @@ pub fn render(state: Rc<Hints>) -> Dom {
                 .collect::<Vec<_>>(),
             state.game.base.theme_id
         ))
-        .child(TracesShow::render(TracesShow::new(
-                state.game.base.target_areas
-                    .iter()
-                    .map(|area| area.trace.clone())
-                    .collect(),
-                TracesShowMode::Cutout,
-                TracesShow::on_select_noop()
-        )))
+        .child_signal(state.game.base.module_phase.signal_cloned().map(clone!(state => move |phase| {
+            match phase {
+                // Only render the hints if the activity is in the Playing phase.
+                ModulePlayPhase::Playing => Some(TracesShow::render(TracesShow::new(
+                    state.game.base.target_areas
+                        .iter()
+                        .map(|area| area.trace.clone())
+                        .collect(),
+                    TracesShowMode::Cutout,
+                    TracesShow::on_select_noop()
+                ))),
+                _ => None,
+            }
+        })))
     })
 }
