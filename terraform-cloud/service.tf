2e823b6a99954ae3d7053e025adb4abb02a351c6
Ricky Saechao <ricky@launchbadge.com>
2022-10-06 04:46:50 -0700

    staging: initialize staging services

---
+++ terraform-cloud/service.tf
@@ -0,0 +1,35 @@
+resource "google_cloud_run_service" "api" {
+  name     = "ji-cloud-api-staging"
+  location = "europe-west1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/ji-cloud-developer-staging/ji-cloud-api-staging:latest"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service" "pages" {
+  name     = "ji-cloud-pages-staging"
+  location = "europe-west1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/ji-cloud-developer-staging/ji-cloud-pages-staging:latest"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
