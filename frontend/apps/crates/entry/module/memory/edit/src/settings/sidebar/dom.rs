cd36c1e6d73892f0241262baa07fbf26cc18d111
Tyrone Tudehope <tyronetudehope@gmail.com>
2022-12-20 10:16:16 +0200

    feat(fe/memory): Update memory activity settings

--- frontend/apps/crates/entry/module/memory/edit/src/settings/sidebar/dom.rs
+++ frontend/apps/crates/entry/module/memory/edit/src/settings/sidebar/dom.rs
@@ -1,71 +1,100 @@
-use dominator::{clone, Dom};
+use dominator::{clone, html, Dom};
+use shared::config::MAX_LIST_WORDS;
 use std::rc::Rc;
 
 use super::state::*;
 use components::module::_common::edit::settings::prelude::*;
-use futures_signals::signal::SignalExt;
+use futures_signals::{
+    signal::{Mutable, SignalExt},
+    signal_vec::SignalVecExt,
+};
 
 pub fn render(state: Rc<SidebarSettings>) -> Dom {
-    if state.base.extra.settings.use_default_pairs.get() {
+    if state.base.extra.settings.show_all_pairs.get() {
         state.set_default_pairs();
     }
 
-    render_settings(Rc::new(ModuleSettings {
-        lines: vec![
-            ModuleSettingsLine::new(
-                LineKind::GameDisplay,
-                vec![Some(
-                    SettingsButtonBuilder::new(
-                        SettingsButtonKind::NumPairs,
-                        clone!(state => move || {
-                            state.base.extra.settings.use_default_pairs.signal().map(|v| !v)
-                        }),
-                    )
-                    .value(SettingsValue::new_mutable(
-                        state.settings().pairs_to_display.clone(),
-                        clone!(state => move |value| {
-                            state.set_pairs_to_display(value);
-                        }),
-                    ))
-                    .on_click(clone!(state => move || state.toggle_use_default_pairs()))
-                    .build(),
-                )],
-            ),
-            ModuleSettingsLine::new(
-                LineKind::TimeLimit,
-                vec![
-                    Some(
-                        SettingsButtonBuilder::new(
-                            SettingsButtonKind::TimeLimitOff,
-                            clone!(state => move || {
-                                state.base.extra.settings.has_time_limit
-                                    .signal()
-                                    .map(|flag| !flag)
-                            }),
-                        )
-                        .on_click(clone!(state => move || state.set_has_time_limit(false)))
-                        .build(),
+    let tooltip = Mutable::new(None);
+
+    html!("empty-fragment", {
+        .child_signal(state.base.pairs.signal_vec_cloned().len().map(clone!(state, tooltip => move |len| {
+            let show_all_kind = if len > MAX_LIST_WORDS {
+                SettingsButtonKind::custom_kind(SettingsButtonKind::CardsShowAll, "show max")
+            } else {
+                SettingsButtonKind::CardsShowAll
+            };
+            Some(render_settings(Rc::new(ModuleSettings {
+                lines: vec![
+                    ModuleSettingsLine::new_with_label(
+                        "Should student view all pairs?".into(),
+                        vec![Some(
+                            SettingsButtonBuilder::new(
+                                show_all_kind,
+                                clone!(state => move || state.base.extra.settings.show_all_pairs.signal()),
+                            )
+                            .on_click(clone!(state => move || state.toggle_use_default_pairs()))
+                            .build()),
+                            Some(SettingsButtonBuilder::new(
+                                SettingsButtonKind::CardsShowSome,
+                                clone!(state => move || {
+                                    state.base.extra.settings.show_all_pairs.signal().map(|v| !v)
+                                }),
+                            )
+                            .value(SettingsValue::new_mutable(
+                                state.settings().pairs_to_display.clone(),
+                                clone!(state, tooltip => move |value| {
+                                    if state.set_pairs_to_display(value) {
+                                        tooltip.set(Some(format!("We can only show a maximum of {MAX_LIST_WORDS} pairs in a memory game.")))
+                                    }
+                                }),
+                            ))
+                            .on_click(clone!(state => move || {
+                                if state.settings().show_all_pairs.get() {
+                                    state.settings().show_all_pairs.set(false);
+                                    state.set_default_pairs();
+                                }
+                            }))
+                            .tooltip(tooltip.clone())
+                            .build(),
+                        )],
                     ),
-                    Some(
-                        SettingsButtonBuilder::new(
-                            SettingsButtonKind::TimeLimit,
-                            clone!(state => move || {
-                                state.base.extra.settings.has_time_limit
-                                    .signal()
-                            }),
-                        )
-                        .value(SettingsValue::new(
-                            state.settings().time_limit.get(),
-                            clone!(state => move |value| {
-                                state.set_time_limit(value);
-                            }),
-                        ))
-                        .on_click(clone!(state => move || state.set_has_time_limit(true)))
-                        .build(),
+                    ModuleSettingsLine::new(
+                        LineKind::TimeLimit,
+                        vec![
+                            Some(
+                                SettingsButtonBuilder::new(
+                                    SettingsButtonKind::TimeLimitOff,
+                                    clone!(state => move || {
+                                        state.base.extra.settings.has_time_limit
+                                            .signal()
+                                            .map(|flag| !flag)
+                                    }),
+                                )
+                                .on_click(clone!(state => move || state.set_has_time_limit(false)))
+                                .build(),
+                            ),
+                            Some(
+                                SettingsButtonBuilder::new(
+                                    SettingsButtonKind::TimeLimit,
+                                    clone!(state => move || {
+                                        state.base.extra.settings.has_time_limit
+                                            .signal()
+                                    }),
+                                )
+                                .value(SettingsValue::new(
+                                    state.settings().time_limit.get(),
+                                    clone!(state => move |value| {
+                                        state.set_time_limit(value);
+                                    }),
+                                ))
+                                .on_click(clone!(state => move || state.set_has_time_limit(true)))
+                                .build(),
+                            ),
+                        ],
                     ),
+                    // NOTE - not including score until player/jig story is resolved
                 ],
-            ),
-            // NOTE - not including score until player/jig story is resolved
-        ],
-    }))
+            })))
+        })))
+    })
 }
--- frontend/apps/crates/entry/module/memory/edit/src/settings/state.rs
+++ frontend/apps/crates/entry/module/memory/edit/src/settings/state.rs
@@ -4,7 +4,7 @@ use shared::domain::module::body::memory::Content;
 pub struct Settings {
     pub time_limit: Mutable<u32>,
     pub has_time_limit: Mutable<bool>,
-    pub use_default_pairs: Mutable<bool>,
+    pub show_all_pairs: Mutable<bool>,
     pub pairs_to_display: Mutable<u32>,
 }
 
@@ -19,7 +19,7 @@ impl Settings {
                     .unwrap_or(crate::config::DEFAULT_TIME_LIMIT),
             ),
             has_time_limit: Mutable::new(settings.time_limit.is_some()),
-            use_default_pairs: Mutable::new(settings.pairs_to_display.is_none()),
+            show_all_pairs: Mutable::new(settings.pairs_to_display.is_none()),
             pairs_to_display: Mutable::new(settings.pairs_to_display.unwrap_or_default()),
         }
     }
