09476f1473914e6e2bb62d034dac02150c70d681
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-12-21 12:47:37 -0500

    feat(fe/asset/play/course): Reduced UI

--- frontend/elements/src/entry/asset/play/course/course-item.ts
+++ frontend/elements/src/entry/asset/play/course/course-item.ts
@@ -8,8 +8,8 @@ export class _ extends LitElement {
                 :host {
                     display: grid;
                     grid-template-columns: repeat(2, auto) 1fr auto;
-                    column-gap: 20px;
-                    padding: 20px;
+                    column-gap: 16px;
+                    padding: 16px;
                     cursor: pointer;
                     align-items: center;
                     background-color: #fff;
@@ -22,41 +22,43 @@ export class _ extends LitElement {
                     place-content: center;
                     width: 10px;
                 }
-                @media (min-width: 1920px) {
+                @media (min-width: 1024px) {
                     .index {
-                        width: 40px;
-                        height: 40px;
+                        width: 30px;
+                        height: 30px;
                         border-radius: 50%;
                         border: solid 1px var(--dark-blue-1);
                     }
                 }
                 ::slotted([slot=thumbnail]) {
-                    width: 190px;
+                    width: 150px;
                     border-radius: 10px;
                     aspect-ratio: 16 / 9;
                 }
                 .column-3 {
                     display: grid;
-                    row-gap: 10px;
+                    row-gap: 8px;
                     color: var(--dark-gray-6);
                 }
                 .name {
-                    font-size: 18px;
+                    font-size: 16px;
                     font-weight: 600;
                 }
                 .description {
                     display: none;
                 }
-                @media (min-width: 1920px) {
+                @media (min-width: 1024px) {
                     .description {
                         display: block;
                         overflow-wrap: break-word;
                         white-space: pre-wrap;
+                        font-size: 12px;
                     }
                 }
                 ::slotted([slot=play-button]) {
-                    height: 40px;
-                    width: 40px;
+                    height: 30px;
+                    width: 30px;
+                    font-size: 17px;
                     background-color: var(--main-blue);
                     color: white;
                     border-radius: 50%;
