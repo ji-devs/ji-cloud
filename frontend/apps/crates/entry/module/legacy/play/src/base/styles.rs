5bbf56e0a7e4c12b6804922bfe09bef33c647ae4
David Komer <david.komer@gmail.com>
2023-01-01 12:22:49 +0200

    legacy - allow click through say something bg
    fixes #3311

--- frontend/apps/crates/entry/module/legacy/play/src/base/styles.rs
+++ frontend/apps/crates/entry/module/legacy/play/src/base/styles.rs
@@ -11,6 +11,17 @@ pub static FULL_STAGE: Lazy<String> = Lazy::new(|| {
     }
 });
 
+pub static FULL_STAGE_NO_POINTER: Lazy<String> = Lazy::new(|| {
+    class! {
+        .style("position", "absolute")
+        .style("top", "0")
+        .style("left", "0")
+        .style("width", "100%")
+        .style("height", "100%")
+        .style("pointer-events", "none")
+    }
+});
+
 pub static SVG_FILL_TRANSPARENT_CLICK_CLASS: Lazy<String> = Lazy::new(|| {
     class! {
         .style("fill", "black")
