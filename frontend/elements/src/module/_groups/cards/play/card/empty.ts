f39d4e3717c78cfee390888580c2ebf0dceab9d8
Tyrone Tudehope <tyronetudehope@gmail.com>
2023-01-03 10:35:32 +0200

    feat(fe/matching): Empty card content colour should be the same as the current theme's colour

--- frontend/elements/src/module/_groups/cards/play/card/empty.ts
+++ frontend/elements/src/module/_groups/cards/play/card/empty.ts
@@ -35,10 +35,6 @@ export class _ extends LitElement {
                     width: 61.78rem;
                     height: 97.75rem;
                 }
-
-                .question-mark-fill {
-                    fill: #d3ddea;
-                }
             `,
         ];
     }
@@ -72,7 +68,7 @@ function renderQuestion(theme: ThemeId, active: boolean) {
     return html`
         <section>
             <div class="content question-mark" style=${style}>
-                ${renderQuestionMark(theme, active)}
+                ${renderQuestionMark(theme)}
             </div>
         </section>
     `;
@@ -86,17 +82,15 @@ function renderTranslucent() {
     `;
 }
 
-function renderQuestionMark(theme: ThemeId, active: boolean) {
-    const style = active
-        ? styleMap({ fill: `var(--theme-${theme}-cards-border-color)` })
-        : styleMap({});
+function renderQuestionMark(theme: ThemeId) {
+    const style = styleMap({ fill: `var(--theme-${theme}-cards-border-color)` });
 
     return svg`
 		<svg xmlns="http://www.w3.org/2000/svg" width="39.234" height="61.713" viewBox="0 0 39.234 61.713">
 		<g id="noun_question_mark_2152791" data-name="noun_question mark_2152791" transform="translate(-186.501 -276.296)">
 		<g id="Group_15074" data-name="Group 15074" transform="translate(101.617 276.287)">
-		<path style=${style} class="question-mark-fill" data-name="Path 155363" d="M124.118,19.591a19.617,19.617,0,0,0-39.234,0h11.6a8.018,8.018,0,1,1,11.962,6.962v.007a19.677,19.677,0,0,0-8.451,9.914h0a19.541,19.541,0,0,0-1.128,4.489h.011v3.468h11.854V40.964h-.022a8.051,8.051,0,0,1,3.733-4.489h-.011A19.552,19.552,0,0,0,124.118,19.591Z" />
-		<circle style=${style} class="question-mark-fill" data-name="Ellipse 613" cx="7" cy="7" r="7" transform="translate(99 47.722)" />
+		<path style=${style} data-name="Path 155363" d="M124.118,19.591a19.617,19.617,0,0,0-39.234,0h11.6a8.018,8.018,0,1,1,11.962,6.962v.007a19.677,19.677,0,0,0-8.451,9.914h0a19.541,19.541,0,0,0-1.128,4.489h.011v3.468h11.854V40.964h-.022a8.051,8.051,0,0,1,3.733-4.489h-.011A19.552,19.552,0,0,0,124.118,19.591Z" />
+		<circle style=${style} data-name="Ellipse 613" cx="7" cy="7" r="7" transform="translate(99 47.722)" />
 		</g>
 		</g>
 		</svg>
