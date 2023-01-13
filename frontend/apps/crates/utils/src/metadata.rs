30e17a37d1dddc780c6e386dfd317818ca20d3e0
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2023-01-13 11:28:41 -0500

    refactor(fe/utils/metadata): `Arc` to avoid cloning

--- frontend/apps/crates/utils/src/metadata.rs
+++ frontend/apps/crates/utils/src/metadata.rs
@@ -1,4 +1,4 @@
-use std::sync::Mutex;
+use std::sync::{Mutex, Arc};
 
 use crate::callback_future::CallbackFuture;
 use crate::{fetch::*, unwrap::UnwrapJiExt};
@@ -15,13 +15,38 @@ use wasm_bindgen_futures::spawn_local;
 #[derive(Clone, Debug)]
 enum State {
     Init,
-    Loading(Shared<CallbackFuture<MetadataResponse>>),
-    Ready(MetadataResponse),
+    Loading(Shared<CallbackFuture<Arc<Metadata>>>),
+    Ready(Arc<Metadata>),
 }
 
 static STATE: Mutex<State> = Mutex::new(State::Init);
 
-pub async fn get_metadata() -> MetadataResponse {
+#[derive(Clone, Debug)]
+pub struct Metadata {
+    pub image_styles: Arc<Vec<ImageStyle>>,
+    pub animation_styles: Arc<Vec<AnimationStyle>>,
+    pub age_ranges: Arc<Vec<AgeRange>>,
+    pub affiliations: Arc<Vec<Affiliation>>,
+    pub resource_types: Arc<Vec<ResourceType>>,
+    pub subjects: Arc<Vec<Subject>>,
+    pub image_tags: Arc<Vec<ImageTag>>,
+}
+
+impl From<MetadataResponse> for Metadata {
+    fn from(meta: MetadataResponse) -> Self {
+        Self {
+            image_styles: Arc::new(meta.image_styles),
+            animation_styles: Arc::new(meta.animation_styles),
+            age_ranges: Arc::new(meta.age_ranges),
+            affiliations: Arc::new(meta.affiliations),
+            resource_types: Arc::new(meta.resource_types),
+            subjects: Arc::new(meta.subjects),
+            image_tags: Arc::new(meta.image_tags),
+        }
+    }
+}
+
+pub async fn get_metadata() -> Arc<Metadata> {
     let mut state = STATE.lock().unwrap_ji();
     match &*state {
         State::Init => {
@@ -29,6 +54,7 @@ pub async fn get_metadata() -> MetadataResponse {
                 spawn_local(async {
                     let meta = endpoints::meta::Get::api_no_auth(GetMetadataPath(), None)
                         .await
+                        .map(|meta| Arc::new(meta.into()))
                         .unwrap_ji();
                     resolve(meta);
                 });
@@ -36,7 +62,7 @@ pub async fn get_metadata() -> MetadataResponse {
             .shared();
 
             *state = State::Loading(future.clone());
-            // drop state while awaiting
+            // unlock state while awaiting
             drop(state);
 
             let meta = future.await;
@@ -50,7 +76,7 @@ pub async fn get_metadata() -> MetadataResponse {
         State::Loading(future) => {
             let future = future.clone();
 
-            // drop state while awaiting
+            // unlock state while awaiting
             drop(state);
 
             future.await
@@ -59,30 +85,30 @@ pub async fn get_metadata() -> MetadataResponse {
     }
 }
 
-pub async fn get_image_styles() -> Vec<ImageStyle> {
-    get_metadata().await.image_styles.clone()
+pub async fn get_image_styles() -> Arc<Vec<ImageStyle>> {
+    Arc::clone(&get_metadata().await.image_styles)
 }
 
-pub async fn get_animation_styles() -> Vec<AnimationStyle> {
-    get_metadata().await.animation_styles.clone()
+pub async fn get_animation_styles() -> Arc<Vec<AnimationStyle>> {
+    Arc::clone(&get_metadata().await.animation_styles)
 }
 
-pub async fn get_age_ranges() -> Vec<AgeRange> {
-    get_metadata().await.age_ranges.clone()
+pub async fn get_age_ranges() -> Arc<Vec<AgeRange>> {
+    Arc::clone(&get_metadata().await.age_ranges)
 }
 
-pub async fn get_affiliations() -> Vec<Affiliation> {
-    get_metadata().await.affiliations.clone()
+pub async fn get_affiliations() -> Arc<Vec<Affiliation>> {
+    Arc::clone(&get_metadata().await.affiliations)
 }
 
-pub async fn get_resource_types() -> Vec<ResourceType> {
-    get_metadata().await.resource_types.clone()
+pub async fn get_resource_types() -> Arc<Vec<ResourceType>> {
+    Arc::clone(&get_metadata().await.resource_types)
 }
 
-pub async fn get_subjects() -> Vec<Subject> {
-    get_metadata().await.subjects.clone()
+pub async fn get_subjects() -> Arc<Vec<Subject>> {
+    Arc::clone(&get_metadata().await.subjects)
 }
 
-pub async fn get_image_tags() -> Vec<ImageTag> {
-    get_metadata().await.image_tags.clone()
+pub async fn get_image_tags() -> Arc<Vec<ImageTag>> {
+    Arc::clone(&get_metadata().await.image_tags)
 }
