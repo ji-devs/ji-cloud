d9e556c74ca722fa68703b90755349c4f6383915
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2023-05-04 08:34:48 -0400

    fix(be/pages): Passthrough include headers

--- backend/pages/src/templates/passthrough.rs
+++ backend/pages/src/templates/passthrough.rs
@@ -1,19 +1,27 @@
 // routes that are passed on to the frontend server. Needed for resources that have to be served from the same origin
 
 use core::settings::RuntimeSettings;
-use std::error::Error;
+use std::{collections::HashMap, error::Error};
 
 use actix_web::{error::ErrorInternalServerError, web::Data, HttpResponse};
 use cached::proc_macro::cached;
 use shared::config::RemoteTarget;
 
 #[cached(time = 60, result = true, sync_writes = true)]
-async fn get_from_frontend_server(route: String) -> Result<String, Box<dyn Error>> {
-    Ok(reqwest::get(route)
-        .await?
-        .error_for_status()?
-        .text()
-        .await?)
+async fn get_from_frontend_server(
+    route: String,
+) -> Result<(String, HashMap<String, Vec<u8>>), Box<dyn Error>> {
+    let res = reqwest::get(route).await?;
+
+    let headers = res
+        .headers()
+        .into_iter()
+        .map(|(key, value)| (key.to_string(), value.as_bytes().to_vec()))
+        .collect();
+
+    let body = res.text().await?;
+
+    Ok((body, headers))
 }
 
 fn static_frontend_url(remote_target: RemoteTarget, path: &str) -> String {
@@ -22,21 +30,33 @@ fn static_frontend_url(remote_target: RemoteTarget, path: &str) -> String {
 pub async fn service_worker(settings: Data<RuntimeSettings>) -> actix_web::Result<HttpResponse> {
     let route = static_frontend_url(settings.remote_target(), "service-worker.js");
 
-    let res: String = get_from_frontend_server(route).await.map_err(|e| {
+    let (body, headers) = get_from_frontend_server(route).await.map_err(|e| {
         println!("Error fetching service-worker.js: {:?}", e);
         ErrorInternalServerError("")
     })?;
 
-    Ok(actix_web::HttpResponse::Ok().body(res))
+    let mut res = actix_web::HttpResponse::Ok();
+    headers.into_iter().for_each(|header| {
+        res.insert_header(header);
+    });
+    let res = res.body(body);
+
+    Ok(res)
 }
 
 pub async fn manifest(settings: Data<RuntimeSettings>) -> actix_web::Result<HttpResponse> {
     let route = static_frontend_url(settings.remote_target(), "manifest.json");
 
-    let res: String = get_from_frontend_server(route).await.map_err(|e| {
+    let (body, headers) = get_from_frontend_server(route).await.map_err(|e| {
         println!("Error fetching manifest.json: {:?}", e);
         ErrorInternalServerError("")
     })?;
 
-    Ok(actix_web::HttpResponse::Ok().body(res))
+    let mut res = actix_web::HttpResponse::Ok();
+    headers.into_iter().for_each(|header| {
+        res.insert_header(header);
+    });
+    let res = res.body(body);
+
+    Ok(res)
 }
