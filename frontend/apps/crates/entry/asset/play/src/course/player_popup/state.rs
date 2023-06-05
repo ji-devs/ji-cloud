829962dc938018658d44ecf04ec396300d911dca
Ricky Saechao <76449893+RickyLB@users.noreply.github.com>
2023-06-05 16:15:31 -0700

    feat(fe/asset/play/course): add Read more for long descriptions

--- frontend/apps/crates/entry/asset/play/src/course/player_popup/state.rs
+++ frontend/apps/crates/entry/asset/play/src/course/player_popup/state.rs
@@ -7,6 +7,10 @@ use crate::course::state::CoursePlayer;
 pub struct PlayerPopup {
     pub player_state: Rc<CoursePlayer>,
     pub is_full_screen: Mutable<bool>,
+    pub read_more: Mutable<bool>,
+    pub render_popup: Mutable<bool>,
+    pub description: Mutable<Option<String>>,
+    pub name: Mutable<Option<String>>
 }
 
 impl PlayerPopup {
@@ -14,6 +18,10 @@ impl PlayerPopup {
         Rc::new(Self {
             player_state: Rc::clone(&player_state),
             is_full_screen: Default::default(),
+            read_more: Mutable::new(false),
+            render_popup: Mutable::new(false),
+            description: Mutable::new(None),
+            name: Mutable::new(None)
         })
     }
 }
