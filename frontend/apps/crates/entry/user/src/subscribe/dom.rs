f4a1dce7836feb5e60085dcad70d263d99f746cc
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2023-07-06 11:58:31 -0400

    feat(fe/user/subscribe): Page styling

--- frontend/apps/crates/entry/user/src/subscribe/dom.rs
+++ frontend/apps/crates/entry/user/src/subscribe/dom.rs
@@ -1,54 +1,87 @@
 use crate::subscribe::stripe::Stripe;
 
 use super::state::Subscribe;
-use dominator::{clone, html, with_node, Dom};
+use dominator::{clone, html, with_node, DomBuilder};
 use futures_signals::signal::SignalExt;
 use std::rc::Rc;
 use utils::{
-    events,
+    component::Component,
+    events, gap, icon,
     prelude::{get_school_id, SETTINGS},
     routes::{Route, UserRoute},
     unwrap::UnwrapJiExt,
 };
-use web_sys::HtmlElement;
+use web_sys::{HtmlElement, ShadowRoot};
 
-impl Subscribe {
-    pub fn render(self: &Rc<Self>) -> Dom {
+impl Component<Subscribe> for Rc<Subscribe> {
+    fn styles() -> &'static str {
+        include_str!("./styles.css")
+    }
+
+    fn apply_on_host(&self, host: DomBuilder<HtmlElement>) -> DomBuilder<HtmlElement> {
+        let state = self;
+        host.child_signal(state.stripe_client_secret.signal_cloned().map(clone!(state => move |secret| {
+            secret.map(|secret| {
+                html!("div" => HtmlElement, {
+                    .with_node!(elem => {
+                        .apply(clone!(state => move|dom| {
+                            state.loader.load(clone!(state => async move {
+                                *state.stripe.borrow_mut() = Some(Stripe::new(elem, secret).await);
+                            }));
+                            dom
+                        }))
+                    })
+                })
+            })
+        })))
+    }
+
+    fn dom(&self, dom: DomBuilder<ShadowRoot>) -> DomBuilder<ShadowRoot> {
         let state = self;
 
         state.start_intent();
 
-        html!("div", {
-            .child(html!("p", {
-                .text("Plan: ")
-                .text(state.plan_type.as_str())
-            }))
-            .children_signal_vec(state.stripe_client_secret.signal_cloned().map(clone!(state => move |secret| {
-                secret.map(clone!(state => move |secret| {
-                    vec![
-                        html!("div" => HtmlElement, {
-                            .with_node!(elem => {
-                                .apply(clone!(state => move|dom| {
-                                    state.loader.load(clone!(state => async move {
-                                        *state.stripe.borrow_mut() = Some(Stripe::new(elem, secret).await);
-                                    }));
-                                    dom
-                                }))
-                            })
-                        }),
-                        html!("button-rect", {
-                            .text("Submit")
-                            .event(clone!(state => move |_: events::Click| {
-                                let stripe = state.stripe.take();
-                                state.loader.load(async move {
-                                    stripe.unwrap_ji().submit(&get_next_page_url()).await;
-                                });
-                            }))
-                        }),
-                    ]
-                })).unwrap_or_default()
-            })).to_signal_vec())
-        })
+        dom.child(html!("h1", {
+            .text("Try Jigzi school FREE for 14 days")
+            .text(" : ")
+            .text(state.plan_type.as_str())
+        }))
+        .child(html!("p", {
+            .class("list-item")
+            .child(icon!("fa-solid fa-check"))
+            .text("Get a 14 day trial, cancel any time.")
+        }))
+        .child(html!("p", {
+            .class("list-item")
+            .child(icon!("fa-solid fa-check"))
+            .text("Get a reminder 24 hours before your trial ends.")
+        }))
+        .child(gap!(30))
+        .child(html!("h2", {
+            .text("Payment method")
+        }))
+        .child(html!("hr"))
+        .child(html!("h2", {
+            .text("Request other payment method")
+        }))
+        .child(html!("slot"))
+        .child(gap!(48))
+        .child_signal(state.stripe_client_secret.signal_cloned().map(
+            clone!(state => move |secret| {
+                secret.map(|_| {
+                    html!("button-rect", {
+                        .prop("size", "large")
+                        .text("Start free trial")
+                        .event(clone!(state => move |_: events::Click| {
+                            let stripe = state.stripe.take();
+                            state.loader.load(async move {
+                                stripe.unwrap_ji().submit(&get_next_page_url()).await;
+                            });
+                        }))
+                    })
+                })
+            }),
+        ))
     }
 }
 
