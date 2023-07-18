0b0da155ff030f006ae53bc14b4e4a4d86769bc3
Tyrone Tudehope <tyronetudehope@gmail.com>
2023-07-18 12:28:07 +0200

    fix(fe/user): Include school admin email in subscribe flow

--- frontend/apps/crates/entry/user/src/school_start/actions.rs
+++ frontend/apps/crates/entry/user/src/school_start/actions.rs
@@ -12,23 +12,31 @@ use utils::{
 
 use super::state::*;
 use std::rc::Rc;
+use utils::prelude::get_user_cloned;
 
 impl SchoolStart {
     pub fn save(self: &Rc<Self>) {
         let state = self;
+
+        // We shouldn't be at this point without a logged-in user - Unwrap is safe.
+        let user = get_user_cloned().unwrap_ji();
+
         state.loader.load(clone!(state => async move {
             let req = CreateSchoolAccountRequest {
                 name: SchoolNameRequest::Value(state.name.get_cloned().into()),
                 location: state.location.get_cloned(),
-                email: Default::default(),
+                email: user.email,
                 description: Default::default(),
                 profile_image: Default::default(),
                 website: Default::default(),
                 organization_type: Default::default()
             };
             endpoints::account::CreateSchoolAccount::api_with_auth(CreateSchoolAccountPath(), Some(req)).await.unwrap_ji();
+
+            // Update current user state - UserProfile should include account summary now.
             let user = endpoints::user::Profile::api_with_auth(GetProfilePath(), None).await.unwrap_ji();
             get_user_mutable().set(Some(user));
+
             go_to_url(&Route::User(UserRoute::Subscribe1(state.plan_type)).to_string());
         }));
     }
