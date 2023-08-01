3e2beb2b83ca54887798b85352dc719752f50650
Tyrone Tudehope <tyronetudehope@gmail.com>
2023-08-01 09:49:54 +0200

    fix(be/billing): Fix non-null constraint in migration

--- backend/api/migrations/20230728064140_create-internal-school-name.sql
+++ backend/api/migrations/20230728064140_create-internal-school-name.sql
@@ -3,11 +3,22 @@ alter table school
 ;
 
 alter table school
-    add column if not exists school_name citext not null,
+    add column if not exists school_name citext,
     add column if not exists verified bool default false,
     alter column internal_school_name_id drop not null
 ;
 
 alter table school_name
     drop column if exists verified
+;
+
+update school
+    set school_name = school_name.name
+from school_name
+where
+    school.internal_school_name_id = school_name.school_name_id
+;
+
+alter table school
+    alter column school_name set not null
 ;
