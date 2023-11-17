3dafe8239002d8573dba6572f6c3e2da30b1dd17
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2023-11-17 13:00:54 -0500

    feat(fe/classroom/codes): Display basic matching module

--- frontend/apps/crates/entry/classroom/src/codes/code_sessions/dom.rs
+++ frontend/apps/crates/entry/classroom/src/codes/code_sessions/dom.rs
@@ -2,7 +2,13 @@ use std::rc::Rc;
 
 use dominator::{clone, html, Dom, DomBuilder};
 use futures_signals::{signal::SignalExt, signal_vec::SignalVecExt};
-use shared::domain::{jig::codes::JigCodeSessionResponse, module::ModuleResponse};
+use shared::domain::{
+    jig::codes::{JigCodeSessionResponse, JigPlaySessionMatching, JigPlaySessionModule},
+    module::{
+        body::{_groups::cards::CardContent, matching},
+        ModuleResponse,
+    },
+};
 use utils::component::Component;
 use web_sys::ShadowRoot;
 
@@ -104,11 +110,84 @@ impl Component<CodeSessions> for Rc<CodeSessions> {
 impl CodeSessions {
     fn render_session(
         self: &Rc<Self>,
-        _info: JigCodeSessionResponse,
-        _module: &ModuleResponse,
+        info: JigCodeSessionResponse,
+        module: &ModuleResponse,
+    ) -> Dom {
+        let state = self;
+
+        let module = match &module.module.body {
+            shared::domain::module::ModuleBody::Matching(module) => {
+                Some(module.content.clone().unwrap())
+            }
+            _ => None,
+        };
+
+        html!("div", {
+            .apply(|dom| {
+                match info.info {
+                    Some(info) => {
+                        dom.children(info.modules.into_iter().map(|m| match m {
+                            JigPlaySessionModule::Matching(matching) => state.render_session_matching(matching, module.as_ref().unwrap())
+                        }))
+                    },
+                    None => dom,
+                }
+            })
+        })
+    }
+
+    fn render_session_matching(
+        self: &Rc<Self>,
+        info: JigPlaySessionMatching,
+        module: &matching::Content,
     ) -> Dom {
         html!("div", {
-            .text("aaa")
+            .children(
+                info.rounds.iter().enumerate().map(|(i, round)| {
+                    // html!("progress")
+                    html!("div", {
+                        .class("wrapper")
+                        .child(html!("h4", {
+                            .text("Round ")
+                            .text(&i.to_string())
+                        }))
+                        .child(html!("div", {
+                            .class("round-items")
+
+                            .children(round.iter().map(|(id, item)| {
+                                html!("div", {
+                                    .class("round-item")
+                                    .child(html!("div", {
+                                        .apply(|dom| {
+                                            match &module.base.pairs[*id].0.card_content {
+                                                CardContent::Text(s) => {
+                                                    dom.child(html!("p", {
+                                                        .text(&s)
+                                                    }))
+                                                },
+                                                CardContent::Image(Some(_i)) => {
+                                                    dom.child(html!("img-ji", {
+                                                        // .prop("")
+                                                    }))
+                                                },
+                                                CardContent::Image(None) => {
+                                                    dom
+                                                }
+                                            }
+                                        })
+                                    }))
+                                    .child(html!("p", {
+                                        .text("Tries ")
+                                        .child(html!("strong", {
+                                            .text(&(item.failed_tries + 1).to_string())
+                                        }))
+                                    }))
+                                })
+                            }))
+                        }))
+                    })
+                })
+            )
         })
     }
     //     fn module_report_signal(
