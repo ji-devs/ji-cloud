51999ba7af93adf73a80466b7f66b987c136fed3
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2024-05-09 12:00:32 -0400

    feat(fe/algolia-events): Send correct events even for unauthenticated users

--- frontend/apps/crates/utils/src/algolia_events.rs
+++ frontend/apps/crates/utils/src/algolia_events.rs
@@ -1,9 +1,9 @@
 use awsm_web::loaders::fetch::fetch_with_headers_and_data;
 use serde::Serialize;
-use shared::domain::jig::JigId;
+use shared::domain::{jig::JigId, user::UserId};
 use wasm_bindgen_futures::spawn_local;
 
-use crate::{prelude::get_user_id, unwrap::UnwrapJiExt};
+use crate::{prelude::get_user_id, storage::get_session_storage, unwrap::UnwrapJiExt};
 
 #[cfg(feature = "release")]
 const JIG_INDEX: &str = "release_jig";
@@ -11,6 +11,8 @@ const JIG_INDEX: &str = "release_jig";
 const JIG_INDEX: &str = "sandbox_jig";
 
 const ALGOLIA_EVENTS_URL: &str = "https://insights.algolia.io/1/events";
+
+const UNAUTHENTICATED: &str = "unauthenticated-token";
 
 fn send_event(data: impl Serialize + 'static) {
     spawn_local(async move {
@@ -29,6 +31,28 @@ fn send_event(data: impl Serialize + 'static) {
     });
 }
 
+enum UserToken {
+    Authenticated(UserId),
+    Unauthenticated(String),
+}
+impl UserToken {
+    fn get() -> Self {
+        match get_user_id() {
+            Some(user_id) => UserToken::Authenticated(user_id),
+            None => UserToken::Unauthenticated(Self::get_unauthenticated_id()),
+        }
+    }
+
+    fn get_unauthenticated_id() -> String {
+        let storage = get_session_storage().unwrap_ji();
+        storage.get(UNAUTHENTICATED).unwrap_ji().unwrap_or_else(|| {
+            let token = js_sys::Math::random().to_string();
+            storage.set(UNAUTHENTICATED, &token).unwrap_ji();
+            token
+        })
+    }
+}
+
 pub fn viewed_jig(jig_id: JigId) {
     let mut data = serde_json::json!({
         "eventType": "click",
@@ -40,6 +64,18 @@ pub fn viewed_jig(jig_id: JigId) {
         data.as_object_mut()
             .unwrap()
             .insert("userToken".into(), user_id.to_string().into());
+    }
+    match UserToken::get() {
+        UserToken::Authenticated(user_id) => {
+            data.as_object_mut()
+                .unwrap()
+                .insert("authenticatedUserToken".into(), user_id.to_string().into());
+        },
+        UserToken::Unauthenticated(user_id) => {
+            data.as_object_mut()
+                .unwrap()
+                .insert("userToken".into(), user_id.to_string().into());
+        },
     }
     let data = serde_json::json!({
         "events": [data],
@@ -54,10 +90,17 @@ pub fn finished_jig(jig_id: JigId) {
         "index": JIG_INDEX,
         "objectIDs": [jig_id.to_string()],
     });
-    if let Some(user_id) = get_user_id() {
-        data.as_object_mut()
-            .unwrap()
-            .insert("userToken".into(), user_id.to_string().into());
+    match UserToken::get() {
+        UserToken::Authenticated(user_id) => {
+            data.as_object_mut()
+                .unwrap()
+                .insert("authenticatedUserToken".into(), user_id.to_string().into());
+        },
+        UserToken::Unauthenticated(user_id) => {
+            data.as_object_mut()
+                .unwrap()
+                .insert("userToken".into(), user_id.to_string().into());
+        },
     }
     let data = serde_json::json!({
         "events": [data],
