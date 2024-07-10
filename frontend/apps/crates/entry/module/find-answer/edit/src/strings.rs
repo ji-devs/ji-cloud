8d5f0327b1206197482c4242892a800048323c1f
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2024-07-10 13:19:08 -0400

    feat(fe/module/find-answer/edit): Added text about being optional

--- frontend/apps/crates/entry/module/find-answer/edit/src/strings.rs
+++ frontend/apps/crates/entry/module/find-answer/edit/src/strings.rs
@@ -1,4 +1,4 @@
 pub mod step_3 {
-    pub const STR_LABEL: &str = "Written question";
+    pub const STR_LABEL: &str = "Written question (optional)";
     pub const STR_PLACEHOLDER: &str = "Type your question";
 }
