409527d29bcde4040ac4cb79ec60580d7fe0ccd7
Tyrone Tudehope <tyronetudehope@gmail.com>
2026-06-24 21:49:35 +0200

    fix: animate gif sprites

--- frontend/apps/crates/components/src/stickers/sprite/actions.rs
+++ frontend/apps/crates/components/src/stickers/sprite/actions.rs
@@ -1,7 +1,15 @@
 use utils::image_effects::ImageEffect;
+use utils::path::module_image_url;
+use utils::prelude::UnwrapJiExt;
 
 use super::state::Sprite;
-use shared::domain::module::body::{_groups::design::SpriteEffect, Image};
+use shared::{
+    domain::{
+        image::ImageFileKind,
+        module::body::{_groups::design::SpriteEffect, Image},
+    },
+    media::PngImageFile,
+};
 
 impl Sprite {
     pub fn remove_white(&self) {
@@ -24,6 +32,15 @@ impl Sprite {
 }
 
 pub async fn load_and_render(image: Image, effects: &[SpriteEffect]) -> (String, f64, f64) {
+    if image.kind == ImageFileKind::Gif && effects.is_empty() {
+        let url = module_image_url(image.lib, image.kind, PngImageFile::Resized, image.id);
+        let img = awsm_web::loaders::image::load(url.clone())
+            .await
+            .unwrap_ji();
+
+        return (url, img.natural_width() as f64, img.natural_height() as f64);
+    }
+
     let mut effect = ImageEffect::new(image, None).await;
 
     for kind in effects.iter() {
